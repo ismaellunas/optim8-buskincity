39897ab5c4eea6685ce65a44b7d5974f67013f97
Bayu <boedy.bayu@gmail.com>
2021-06-15 08:38:30 +0700

    User errorlog

--- config/logging.php
+++ config/logging.php
@@ -42,7 +42,7 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
+            'driver' => 'errorlog', // default: 'single',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
