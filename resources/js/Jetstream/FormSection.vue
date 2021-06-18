3b4c19bfdd475a1b479036c75bc7a1f8a779ba7e
Bayu <boedy.bayu@gmail.com>
2021-06-18 13:29:58 +0700

    Update Profile page to use bulma

--- resources/js/Jetstream/FormSection.vue
+++ resources/js/Jetstream/FormSection.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="md:grid md:grid-cols-3 md:gap-6">
+    <div class="box">
         <jet-section-title>
             <template #title><slot name="title"></slot></template>
             <template #description><slot name="description"></slot></template>
         </jet-section-title>
 
-        <div class="mt-5 md:mt-0 md:col-span-2">
+        <div class="mt-3">
             <form @submit.prevent="$emit('submitted')">
                 <div class="px-4 py-5 bg-white sm:p-6 shadow"
                     :class="hasActions ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md'">
@@ -14,7 +14,7 @@
                     </div>
                 </div>
 
-                <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md" v-if="hasActions">
+                <div class="px-4 py-3" v-if="hasActions">
                     <slot name="actions"></slot>
                 </div>
             </form>
