f44c2fba70c8d03e6c395ab36efeb896a9a3b5fe
Bayu <boedy.bayu@gmail.com>
2021-06-18 13:10:06 +0700

    Add fontawesome js

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,6 +16,7 @@
         <!-- Scripts -->
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="https://kit.fontawesome.com/3004179fa5.js" crossorigin="anonymous"></script>
     </head>
     <body class="font-sans antialiased">
         @inertia
