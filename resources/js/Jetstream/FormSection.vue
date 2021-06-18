afae03b9fe4ef18ec47729165ee436fb1571f9d9
Bayu <boedy.bayu@gmail.com>
2021-06-18 17:53:25 +0700

    Update Jetstream/FormSection to use bulma

--- resources/js/Jetstream/FormSection.vue
+++ resources/js/Jetstream/FormSection.vue
@@ -1,23 +1,26 @@
 <template>
     <div class="box">
-        <jet-section-title>
-            <template #title><slot name="title"></slot></template>
-            <template #description><slot name="description"></slot></template>
-        </jet-section-title>
-
-        <div class="mt-3">
-            <form @submit.prevent="$emit('submitted')">
-                <div class="px-4 py-5 bg-white sm:p-6 shadow"
-                    :class="hasActions ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md'">
-                    <div class="grid grid-cols-6 gap-6">
-                        <slot name="form"></slot>
+        <div class="columns">
+            <div class="column is-one-third">
+                <jet-section-title>
+                    <template #title><slot name="title"></slot></template>
+                    <template #description><slot name="description"></slot></template>
+                </jet-section-title>
+            </div>
+            <div class="column is-two-thirds">
+                <form @submit.prevent="$emit('submitted')">
+                    <div class="px-4 py-4"
+                         :class="hasActions ? '' : ''">
+                        <div class="">
+                            <slot name="form"></slot>
+                        </div>
                     </div>
-                </div>
 
-                <div class="px-4 py-3" v-if="hasActions">
-                    <slot name="actions"></slot>
-                </div>
-            </form>
+                    <div class="px-4 py-3" v-if="hasActions">
+                        <slot name="actions"></slot>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </template>
