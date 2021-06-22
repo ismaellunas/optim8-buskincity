04b82e4b4baaf2699677e2e6475f447dc2d3115d
Bayu <boedy.bayu@gmail.com>
2021-06-22 17:07:23 +0700

    Update welcom jetstream template

--- resources/js/Jetstream/Welcome.vue
+++ resources/js/Jetstream/Welcome.vue
@@ -1,15 +1,23 @@
 <template>
-    <div class="box">
-        Lorem ipsum dolor sit amet, consectetur
-    </div>
+    <jet-action-section>
+        <template #title>
+            Lorem ipsum
+        </template>
+        <template #description>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </template>
+        <template #content>
+            Donec odio enim, tempor sed mi quis, interdum venenatis neque. Nulla tempor ipsum at libero tristique, at ullamcorper sapien ultricies. Mauris facilisis euismod volutpat. Pellentesque nulla risus, ornare tincidunt erat at, posuere viverra dui. Donec pretium, lorem vehicula maximus feugiat, eros tortor suscipit lacus, sagittis feugiat velit nibh nec quam. Morbi ante dolor, pretium vel velit et, auctor finibus felis. Pellentesque eget molestie massa, vel interdum nisl. Duis sem libero, dignissim vel condimentum id, consectetur eu est. Nunc eu mi diam. Nam nec placerat eros. Proin ante arcu, commodo ac vestibulum sit amet, blandit id urna. Integer massa elit, facilisis sit amet dui auctor, vehicula fermentum nisl. Ut et neque eget nunc varius feugiat.
+        </template>
+    </jet-action-section>
 </template>
 
 <script>
-    import JetApplicationLogo from '@/Jetstream/ApplicationLogo'
+    import JetActionSection from '@/Jetstream/ActionSection';
 
     export default {
         components: {
-            JetApplicationLogo,
+            JetActionSection,
         },
     }
 </script>
