11cae52b4eab8330a9365c371c36d640e6080869
Bayu <boedy.bayu@gmail.com>
2021-06-22 17:01:16 +0700

    Add Sdb/Select vue component

---
+++ resources/js/Sdb/Select.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="select">
+        <select
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            ref="input"
+            >
+            <slot></slot>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['modelValue'],
+
+        emits: ['update:modelValue'],
+
+        methods: {
+            focus() {
+                this.$refs.input.focus()
+            }
+        }
+    }
+</script>
