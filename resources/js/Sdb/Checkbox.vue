ef52a07df758157c03b9e1c1c0375d0f7b4e24f7
Bayu <boedy.bayu@gmail.com>
2021-06-22 16:48:34 +0700

    Add Sdb/Checkbox vue component

---
+++ resources/js/Sdb/Checkbox.vue
@@ -0,0 +1,39 @@
+<template>
+    <label class="checkbox">
+        <input
+            type="checkbox"
+            :value="value"
+            v-model="proxyChecked"
+            class=""
+            />
+        <slot></slot>
+    </label>
+</template>
+
+<script>
+export default {
+    emits: ['update:checked'],
+
+    props: {
+        checked: {
+            type: [Array, Boolean],
+            default: false,
+        },
+        value: {
+            default: null,
+        },
+    },
+
+    computed: {
+        proxyChecked: {
+            get() {
+                return this.checked;
+            },
+
+            set(val) {
+                this.$emit("update:checked", val);
+            },
+        },
+    },
+}
+</script>
