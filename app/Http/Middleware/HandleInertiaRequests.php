60ffc5d7d766157909739ed70c26880a55438646
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-06 10:27:25 +0700

    Share data for flash message and error message in inertiajs using share()

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Illuminate\Support\Facades\Session;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -37,7 +38,16 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            //
+            'flash' => [
+                'message' => fn () => $request->session()->get('message')
+            ],
+            'errors' => function () {
+                return (
+                    Session::has('errors')
+                    ? Session::get('errors')->getBag('default')->getMessages()
+                    : (object) []
+                );
+            },
         ]);
     }
 }
