cdf7b12ce06e6f4d6cf9f3f94aac7de87a44bb96
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-08 12:31:59 +0700

    Add Input, Select, Textarea as Sdb/Form vue component

---
+++ resources/js/Sdb/Form/Textarea.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="field">
+        <sdb-label>{{ label }}</sdb-label>
+
+        <div class="control">
+            <sdb-textarea
+                :disabled="disabled"
+                :placeholder="placeholder"
+                :value="modelValue"
+                @input="$emit('update:modelValue', $event.target.value)"
+            />
+        </div>
+
+        <sdb-input-error :message="message"/>
+    </div>
+</template>
+
+<script>
+    import SdbInputError from '@/Sdb/InputError';
+    import SdbLabel from '@/Sdb/Label';
+    import SdbTextarea from '@/Sdb/Textarea';
+
+    export default {
+        components: {
+            SdbInputError,
+            SdbLabel,
+            SdbTextarea,
+        },
+        emits: ['update:modelValue'],
+        props: ['label', 'message', 'modelValue', 'placeholder', 'disabled']
+    }
+</script>
