b9f60e54aa2f73b911fb0a9a9d9b15fe5431c338
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-08 12:12:27 +0700

    Add Sdb/Input vue component

---
+++ resources/js/Sdb/Input.vue
@@ -0,0 +1,25 @@
+<template>
+    <input class="input"
+        :class="{'is-danger' : hasError}"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        ref="input"
+    />
+</template>
+
+<script>
+    export default {
+        props: {
+            modelValue: {},
+            hasError: {default: false}
+        },
+
+        emits: ['update:modelValue'],
+
+        methods: {
+            //focus() {
+            //    this.$refs.input.focus()
+            //}
+        },
+    }
+</script>
