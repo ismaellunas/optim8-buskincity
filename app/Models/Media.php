0bd5f1576991d77b57291a5fc1f96c6a3c901ae6
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-09 09:49:55 +0700

    Add scopeImage into Media model

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -6,5 +6,8 @@ use CloudinaryLabs\CloudinaryLaravel\Model\Media as CloudinaryMedia;
 
 class Media extends CloudinaryMedia
 {
-
+    public function scopeImage($query)
+    {
+        return $query->where('file_type', 'image');
+    }
 }
