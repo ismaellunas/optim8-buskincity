f1480cd1d3d6708b9c144a25b18955b2eb85621a
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-13 15:24:02 +0700

    Add sdb/inputfile vue component

---
+++ resources/js/Sdb/InputFile.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="file is-centered is-boxed has-name">
+        <label class="file-label">
+            <input
+                :accept="accept.join(', ')"
+                :class="class"
+                @change="onFileChange"
+                class="file-input"
+                type="file"
+                />
+            <span class="file-cta">
+                <span class="file-icon">
+                    <i class="fas fa-upload"></i>
+                </span>
+                <span class="file-label">
+                    Choose a file...
+                </span>
+            </span>
+            <span class="file-name">
+                {{ fileName }}
+            </span>
+        </label>
+    </div>
+</template>
+
+<script>
+    import { useModelWrapper, isBlank } from '@/Libs/utils';
+
+    export default {
+        props: {
+            modelValue: {},
+            accept: Array,
+            class: {},
+        },
+        setup(props, { emit }) {
+            return {
+                file: useModelWrapper(props, emit),
+            };
+        },
+        methods: {
+            onFileChange(event) {
+                let files = event.target.files || event.dataTransfer.files;
+                if (!files.length) {
+                    return;
+                }
+                this.file = files[0];
+            }
+        },
+        computed: {
+            fileName() {
+                if (!isBlank(this.file)) {
+                    return this.file.name
+                }
+                return "...";
+            },
+        },
+    }
+</script>
