571c43f0f20afb4d8f6ede594c300bd06902c158
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-13 12:30:05 +0700

    Register medium as model binding for Media model

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,6 +47,8 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+
+        Route::model('medium', \App\Models\Media::class);
     }
 
     /**
