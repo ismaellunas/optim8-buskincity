d406d9e0113efc606a9d46cf8852e81ba448eac7
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-13 16:34:14 +0700

    Add sdb/buttonlink vue component

---
+++ resources/js/Sdb/ButtonLink.vue
@@ -0,0 +1,11 @@
+<template>
+    <inertia-link class="button" :class="class">
+        <slot></slot>
+    </inertia-link>
+</template>
+
+<script>
+    export default {
+        props: ['class'],
+    }
+</script>
