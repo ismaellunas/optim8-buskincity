fb58a862a9a56069ac24689c4b158acdc0d89b57
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-14 12:05:36 +0700

    Update form structure and also replace meta_keywords to meta_title

--- resources/js/Pages/Page/FormDetail.vue
+++ resources/js/Pages/Page/FormDetail.vue
@@ -1,49 +1,60 @@
 <template>
-    <sdb-form-input
-        label="Title"
-        v-model="title"
-        :message="error('title')"
-        placeholder="e.g A Good News"
-        :disabled="disableInput"
-    />
-    <sdb-form-input
-        label="Slug"
-        v-model="slug"
-        :message="error('slug')"
-        placeholder="e.g. a-good-news"
-        :disabled="disableInput"
-    />
+    <div class="columns">
+        <div class="column is-4">
+            <sdb-form-input
+                label="Title"
+                v-model="title"
+                :message="error('title')"
+                placeholder="e.g A Good News"
+                :disabled="disableInput"
+                required
+            />
+        </div>
+        <div class="column is-4">
+            <sdb-form-input
+                label="Slug"
+                v-model="slug"
+                :message="error('slug')"
+                placeholder="e.g. a-good-news"
+                :disabled="disableInput"
+            />
+        </div>
+        <div class="column is-4">
+            <sdb-form-select
+                label="Status"
+                v-model="status"
+                :message="error('status')"
+                :disabled="disableInput"
+                class="is-fullwidth"
+            >
+                <option v-for="option in statusOptions" :value="option.id">
+                    {{ option.value }}
+                </option>
+            </sdb-form-select>
+        </div>
+    </div>
     <sdb-form-textarea
         label="Excerpt"
         v-model="excerpt"
         :message="error('excerpt')"
         placeholder="..."
         :disabled="disableInput"
+        rows="2"
     />
-    <sdb-form-textarea
+    <sdb-form-input
         label="Meta Description"
         v-model="meta_description"
         :message="error('meta_description')"
-        placeholder="..."
+        placeholder="meta description"
         :disabled="disableInput"
     />
-    <sdb-form-textarea
-        label="Meta Keywords"
-        v-model="meta_keywords"
-        :message="error('meta_keywords')"
-        placeholder="Keyword1, Keyword2"
+    <sdb-form-input
+        label="Meta Title"
+        v-model="meta_title"
+        :message="error('meta_title')"
+        placeholder="meta title"
         :disabled="disableInput"
     />
-    <sdb-form-select
-        label="Status"
-        v-model="status"
-        :message="error('status')"
-        :disabled="disableInput"
-    >
-        <option v-for="option in statusOptions" :value="option.id">
-            {{ option.value }}
-        </option>
-    </sdb-form-select>
 </template>
 
 <script>
@@ -65,7 +76,7 @@
             'slug',
             'excerpt',
             'meta_description',
-            'meta_keywords',
+            'meta_title',
             'status',
             'errors',
             'disableInput',
@@ -75,7 +86,7 @@
             return {
                 excerpt: useModelWrapper(props, emit, 'excerpt'),
                 meta_description: useModelWrapper(props, emit, 'meta_description'),
-                meta_keywords: useModelWrapper(props, emit, 'meta_keywords'),
+                meta_title: useModelWrapper(props, emit, 'meta_title'),
                 slug: useModelWrapper(props, emit, 'slug'),
                 status: useModelWrapper(props, emit, 'status'),
                 title: useModelWrapper(props, emit, 'title'),
