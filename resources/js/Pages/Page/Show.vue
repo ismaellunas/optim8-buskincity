f7bb754a3e91ed7f256ba1eac30415d42177f1b5
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-14 18:15:48 +0700

    Update Page/Show to render data of the page

--- resources/js/Pages/Page/Show.vue
+++ resources/js/Pages/Page/Show.vue
@@ -1,27 +1,27 @@
 <template>
+    <page-layout>
+        <template v-for="(block, index) in page.data">
+            <block-columns
+                v-model="page.data[index]"
+                :isEditMode="false"
+                :id="block.id"
+                />
+        </template>
+    </page-layout>
 </template>
 
 <script>
-    import { reactive } from "vue";
+    import BlockColumns from '@/Blocks/Columns';
+    import PageLayout from '@/Layouts/PageLayout';
 
     export default {
         components: {
+            BlockColumns,
+            PageLayout,
         },
         props: {
             page: Object,
             errors: Object,
-        },
-        setup(props) {
-            // state pages
-            const page = reactive({
-                title: props.page.title,
-                slug: props.page.slug,
-                data: props.page.data ?? [],
-            })
-
-            return {
-                page,
-            }
         },
     }
 </script>
