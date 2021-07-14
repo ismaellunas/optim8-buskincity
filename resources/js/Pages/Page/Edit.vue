26289dd25b474325d887290556f1dbafbb81cf31
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-14 18:11:52 +0700

    Add Page/Edit vue component

---
+++ resources/js/Pages/Page/Edit.vue
@@ -0,0 +1,76 @@
+<template>
+    <app-layout>
+        <template #header>
+            Update Page
+        </template>
+
+        <div class="box mb-6">
+            <page-form
+                :form="form"
+                :errors="errors"
+                :isNew="isNew"
+                :isEditMode="isEditMode"
+                :statusOptions="statusOptions"
+                :submit="submit"
+                :tabActive="tabActive"
+            />
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout';
+    import PageForm from '@/Pages/Page/Form';
+    import { Inertia } from "@inertiajs/inertia";
+    import { isBlank } from '@/Libs/utils';
+    import { reactive, ref } from "vue";
+
+    export default {
+        components: {
+            AppLayout,
+            PageForm,
+        },
+        props: {
+            page: Object,
+            entityId: Number,
+            errors: Object,
+            statusOptions: Array,
+            tabActive: String,
+        },
+        setup(props) {
+            const form = reactive({
+                id: props.page.id,
+                title: props.page.title,
+                slug: props.page.slug,
+                excerpt: props.page.excerpt,
+                data: props.page.data ?? [],
+                meta_description: props.page.meta_description,
+                meta_title: props.page.meta_title,
+                status: props.page.status,
+                _method: "PUT",
+            });
+
+            let submitRoute = route('admin.pages.update', {id: props.page.id});
+
+            function submit() {
+                Inertia.put(
+                    submitRoute,
+                    form,
+                    {}
+                );
+            };
+
+            return {
+                form, 
+                submit,
+            };
+        },
+        data() {
+            return {
+                disableInput: false,
+                isEditMode: true,
+                isNew: false,
+            };
+        }
+    }
+</script>
