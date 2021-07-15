c17aef5571a5cb445c8c92de64848d889ce4ccb8
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-15 16:25:16 +0700

    Add Sdb/FlashNotifications vue component

---
+++ resources/js/Sdb/FlashNotifications.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="notification is-info" v-if="flash && isVisible">
+        <button class="delete" @click.prevent="isVisible = false"></button>
+        <div class="alert alert-success">
+            {{ flash.message }}
+        </div>
+    </div>
+</template>
+
+<script>
+    import { isBlank } from '@/Libs/utils';
+
+    export default {
+        props: {
+            flash: Object,
+        },
+        mounted() {             
+            if (!isBlank(this.flash.message)) {
+                this.isVisible = true;
+            }
+        },
+        data() {
+            return {
+                isVisible: false,
+            };
+        },
+        watch: {
+            flash: {
+                deep: true,
+                handler() {
+                    this.isVisible = true;
+                }
+            }
+        }
+    }
+</script>
