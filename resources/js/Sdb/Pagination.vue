83f1c243890bb9392d9f0834e8c48af33589323a
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-15 10:56:06 +0700

    Add sdb/pagination vue component

---
+++ resources/js/Sdb/Pagination.vue
@@ -0,0 +1,58 @@
+<template>
+    <div v-if="links.length > 3">
+        <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+            <a
+                class="pagination-previous"
+                :href="previousLink.url"
+                :disabled="isBlank(previousLink.url) ? 1 : null"
+                v-html="previousLink.label"
+                />
+            <a
+                class="pagination-next"
+                :disabled="isBlank(nextLink.url) ? 1 : null"
+                :href="nextLink.url"
+                v-html="nextLink.label"
+                />
+            <ul class="pagination-list">
+                <li v-for="link in paginationLinks">
+                    <span class="pagination-ellipsis" v-if="isBlank(link.url)">&hellip;</span>
+                    <inertia-link v-else
+                        :href="link.url"
+                        class="pagination-link"
+                        :class="{'is-current': link.active}"
+                        v-html="link.label"
+                        />
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+    import { isBlank } from '@/Libs/utils';
+
+    export default {
+        props: {
+            links: Array,
+        },
+        methods: {
+            isBlank: isBlank,
+        },
+        computed: {
+            previousLink() {
+                return this.links[0];
+            },
+            nextLink() {
+                return this.links[ this.links.length - 1 ];
+            },
+            paginationLinks() {
+                const paginationLinks = this.links.slice();
+
+                paginationLinks.pop();
+                paginationLinks.shift();
+
+                return paginationLinks;
+            },
+        }
+    }
+</script>
