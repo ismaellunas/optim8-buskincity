7472a58efd1836120b608e9301c5babb5f6835dd
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-15 11:55:07 +0700

    Update display Page/Index

--- resources/js/Pages/Page/Index.vue
+++ resources/js/Pages/Page/Index.vue
@@ -1,38 +1,66 @@
 <template>
     <app-layout>
-        <template #header>
-            <h2 class="">Page</h2>
-        </template>
+        <template #header>Pages</template>
 
         <div class="box">
             <div class="columns">
-                <div class="column">
-                    <sdb-button-link :href="route('admin.pages.create')" class="is-primary">
-                        Create
-                    </sdb-button-link>
+                <div class="column is-offset-10">
+                    <div class="is-pulled-right">
+                        <sdb-button-link :href="route('admin.pages.create')" class="is-primary">
+                            <span class="icon is-small">
+                                <i class="fas fa-plus"></i>
+                            </span>
+                            <span>Create New</span>
+                        </sdb-button-link>
+                    </div>
                 </div>
             </div>
 
             <div class="table-container">
-                <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+                <table class="table is-striped is-hoverable is-fullwidth">
                     <thead>
                         <tr>
+                            <th>#</th>
                             <th>Title</th>
                             <th>Slug</th>
-                            <th>Actions</th>
+                            <th>Status</th>
+                            <th>Meta Title</th>
+                            <th>Meta Description</th>
+                            <th>
+                                <div class="level-right">Actions</div>
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="page in pages" :key="page.id">
-                            <th>{{ page.title }}</th>
+                        <tr v-for="page in records.data" :key="page.id">
+                            <th>{{ page.id }}</th>
+                            <td>{{ page.title }}</td>
                             <td>{{ page.slug }}</td>
                             <td>
-                                <sdb-button-link :href="route('admin.pages.edit', {id: page.id})">
-                                    Edit
-                                </sdb-button-link>
-                                <sdb-button class="is-danger ml-2" @click.prevent="deleteRow(page)">
-                                    Delete
+                                <sdb-button :class="statusClass(page.status)" type="button">
+                                    {{ page.statusText }}
                                 </sdb-button>
+                            </td>
+                            <td><i class="far fa-check-circle" v-if="page.hasMetaTitle"></i></td>
+                            <td><i class="far fa-check-circle" v-if="page.hasMetaDescription"></i></td>
+                            <td>
+                                <div class="level-right">
+                                    <sdb-button-link class="is-ghost has-text-black" :href="route('admin.pages.edit', {id: page.id})">
+                                        <span class="icon is-small">
+                                            <i class="far fa-eye"></i>
+                                        </span>
+                                    </sdb-button-link>
+                                    <sdb-button-link class="is-ghost has-text-black" :href="route('admin.pages.edit', {id: page.id})">
+                                        <span class="icon is-small">
+                                            <i class="fas fa-pen"></i>
+                                        </span>
+                                    </sdb-button-link>
+                                    <sdb-button class="is-ghost has-text-black ml-1" @click.prevent="deleteRow(page)">
+                                        <span class="icon is-small">
+                                            <i class="far fa-trash-alt"></i>
+                                        </span>
+                                    </sdb-button>
+                                </div>
                             </td>
                         </tr>
                     </tbody>
@@ -56,15 +84,20 @@
             SdbButtonLink,
             SdbPagination,
         },
-        props: ['pages'],
-        data() {
-            return {
-            };
-        },
+        props: ['records'],
         methods: {
             deleteRow(page) {
                 if (!confirm('Are you sure?')) return;
                 this.$inertia.delete(route('admin.pages.destroy', {id: page.id}));
+            },
+            statusClass(status) {
+                let statusClass = ['is-small', 'is-rounded'];
+                switch(status) {
+                    case 1 : statusClass.push('is-success'); break;
+                    case -1 : statusClass.push('is-danger'); break;
+                    default: statusClass.push('is-primary');
+                };
+                return statusClass;
             }
         },
     }
