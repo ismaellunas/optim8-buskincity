55bdc11e07fb1344a6b57b92505ae3dabb1b4f05
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-16 19:25:16 +0700

    Update to use createInertiaApp when upgrade to inertia-vue3@0.4.1

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,22 +3,21 @@ require('../../node_modules/@fortawesome/fontawesome-free/js/all');
 
 // Import modules...
 import { createApp, h } from 'vue';
-import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress';
 import CKEditor from '@ckeditor/ckeditor5-vue';
 
-const el = document.getElementById('app');
-
-createApp({
-    render: () =>
-        h(InertiaApp, {
-            initialPage: JSON.parse(el.dataset.page),
-            resolveComponent: (name) => require(`./Pages/${name}`).default,
-        }),
+createInertiaApp({
+    title: title => `${title} - My App`,
+    //resolve: (name) => import(`./Pages/${name}`),
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, app, props, plugin }) {
+        createApp({ render: () => h(app, props) })
+            .mixin({ methods: { route } })
+            .use(plugin)
+            .use(CKEditor)
+            .mount(el)
+    },
 })
-    .mixin({ methods: { route } })
-    .use(InertiaPlugin)
-    .use(CKEditor)
-    .mount(el);
 
 InertiaProgress.init({ color: '#4B5563' });
