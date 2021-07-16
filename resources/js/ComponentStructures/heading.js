30f7e519acc1e3aa094adff8db93a84710ff1cbc
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-16 12:23:56 +0700

    Add heading structure

---
+++ resources/js/ComponentStructures/heading.js
@@ -0,0 +1,13 @@
+export default {
+    title: 'Heading',
+    componentName: 'Heading',
+    content: {
+        heading: {
+            html: '',
+            tag: 'h1',
+            attrs: {
+                class: ['title', 'is-1'],
+            },
+        },
+    }
+}
