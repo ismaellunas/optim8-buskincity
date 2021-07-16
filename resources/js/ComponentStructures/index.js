7f89b1e4570737a3b70dac64968bb002c72f3e3e
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-16 12:24:47 +0700

    Disable h1 and h2

--- resources/js/ComponentStructures/index.js
+++ resources/js/ComponentStructures/index.js
@@ -1,13 +1,15 @@
 import card from './card';
 import cardText from './card-text';
-import h1 from './h1';
-import h2 from './h2';
+import heading from './heading';
+//import h1 from './h1';
+//import h2 from './h2';
 import image from './image';
 
 export default {
     card: card,
     cardText: cardText,
-    h1: h1,
-    h2: h2,
+    heading: heading,
+    //h1: h1,
+    //h2: h2,
     image: image,
 }
