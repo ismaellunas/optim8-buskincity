147a711863544d6a9c16baeb84a4228c91034608
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-16 12:43:13 +0700

    Update ckeditorInline to use ckeditor config

--- resources/js/Sdb/CkeditorInline.vue
+++ resources/js/Sdb/CkeditorInline.vue
@@ -9,27 +9,20 @@
 <script>
     import InlineEditor from '@ckeditor/ckeditor5-build-inline';
     import { useModelWrapper } from '@/Libs/utils'
+    import { common } from '@/Libs/ckeditor-configs';
 
     export default {
-        props: ['modelValue', 'disabledPlugins'],
+        props: ['modelValue', 'disabledPlugins', 'config'],
         setup(props, { emit }) {
+            let config = props.config ?? common;
             return {
                 content: useModelWrapper(props, emit),
+                editorConfig: config,
             };
         },
         data() {
             return {
                 editor: InlineEditor,
-                editorConfig: {
-                    toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', ],
-                    heading: {
-                        options: [
-                            { model: 'paragraph', title: 'Paragraph', class: '' },
-                            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'title is-1' },
-                            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'title is-2' }
-                        ]
-                    },
-                },
             }
         },
     }
