1f6f74fbbbea3070d1d77df9dfa0c425edea17fc
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-19 08:43:29 +0700

    Update page Head title and meta description

--- resources/js/Pages/Page/Show.vue
+++ resources/js/Pages/Page/Show.vue
@@ -1,5 +1,9 @@
 <template>
     <page-layout>
+        <Head>
+            <title>{{page.meta_title}}</title>
+             <meta head-key="description" name="description" :content="page.meta_description" />
+        </Head>
         <template v-for="(block, index) in page.data">
             <block-columns
                 v-model="page.data[index]"
@@ -13,11 +17,13 @@
 <script>
     import BlockColumns from '@/Blocks/Columns';
     import PageLayout from '@/Layouts/PageLayout';
+    import { Head } from '@inertiajs/inertia-vue3';
 
     export default {
         components: {
             BlockColumns,
             PageLayout,
+            Head,
         },
         props: {
             page: Object,
