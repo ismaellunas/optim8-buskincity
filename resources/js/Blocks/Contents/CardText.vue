a4d0c8ed85b94cd66dc3963230be7dcfb40c995e
Boedy Bayu <boedy.bayu@gmail.com>
2021-07-21 18:16:08 +0700

    Implement toolbar for cardText component

--- resources/js/Blocks/Contents/CardText.vue
+++ resources/js/Blocks/Contents/CardText.vue
@@ -1,30 +1,31 @@
 <template>
-    <div class="card sdb-card-text" :class="{'edit-mode-content': isEditMode}">
-        <div class="card-content">
-            <div class="content">
-                <template v-if="isEditMode">
-                    <sdb-ckeditor-inline v-model="content.cardContent.content.html"/>
-                </template>
-                <template v-else>
-                    <div v-html="content.cardContent.content.html"></div>
-                </template>
+    <div>
+        <sdb-toolbar-content @delete-content="deleteContent"/>
+
+        <div class="card sdb-card-text">
+            <div class="card-content">
+                <div class="content">
+                    <template v-if="isEditMode">
+                        <sdb-ckeditor-inline v-model="content.cardContent.content.html"/>
+                    </template>
+                    <template v-else>
+                        <div v-html="content.cardContent.content.html"></div>
+                    </template>
+                </div>
             </div>
-        </div>
-        <div class="edit-mode-buttons" v-if="isEditMode">
-            <button class="button is-small" type="button" @click="deleteContent">
-                <i class="fa fa-trash" aria-hidden="true"></i>
-            </button>
         </div>
     </div>
 </template>
 
 <script>
     import SdbCkeditorInline from '@/Sdb/CkeditorInline'
-    import { useModelWrapper, emitModelValue } from '@/Libs/utils'
+    import SdbToolbarContent from '@/Blocks/Contents/ToolbarContent';
+    import { useModelWrapper } from '@/Libs/utils'
 
     export default {
         components: {
             SdbCkeditorInline,
+            SdbToolbarContent,
         },
         props: {
             id: {},
@@ -43,5 +44,3 @@
         }
     }
 </script>
-
-<style scoped src="../../../css/column-content.css"></style>
