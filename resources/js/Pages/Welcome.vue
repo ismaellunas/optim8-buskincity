eaf82946f96fe0510773cecbd24946a3046fdb86
Bayu <boedy@sdbagency.com>
2021-07-28 12:18:55 +0700

    Update Welcome page to use PageLayout

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,40 +1,8 @@
 <template>
-    <nav class="navbar is-dark">
-        <div class="navbar-brand">
-            <a class="navbar-item" href="">
-                <img src="https://dummyimage.com/48x28/e5e5e5/000000.png&text=B+752" alt="" height="28">
-            </a>
-            <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </div>
-
-        <div id="navbarExampleTransparentExample" class="navbar-menu">
-            <div class="navbar-start">
-                <sdb-link href="/" class="navbar-item">Home</sdb-link>
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <sdb-link href="/" class="navbar-link">More</sdb-link>
-                    <div class="navbar-dropdown is-boxed">
-                        <sdb-link href="/" class="navbar-item">Page 1</sdb-link>
-                        <sdb-link href="/" class="navbar-item">Page 2</sdb-link>
-                    </div>
-                </div>
-            </div>
-
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="field">
-                        <div class="control">
-                            <input class="input" type="text" placeholder="Search . . .">
-                        </div>
-                    </div>
-                </div>
-                <sdb-link :href="route('login')" class="navbar-item pr-5">Login</sdb-link>
-            </div>
-        </div>
-    </nav>
+    <Head>
+        <title>Welcome</title>
+        <meta head-key="description" name="description" content="Welcome" />
+    </Head>
 
     <section class="hero">
         <figure class="image">
@@ -44,16 +12,15 @@
 </template>
 
 <script>
-    //import GoogleIcon from '@/Socialstream/ProviderIcons/GoogleIcon';
-    import SdbLink from '@/Sdb/Link';
+    import PageLayout from '@/Layouts/PageLayout';
+    import { Head } from '@inertiajs/inertia-vue3';
 
     export default {
+        layout: PageLayout,
         props: {
         },
-
         components: {
-            //GoogleIcon,
-            SdbLink,
+            Head,
         },
     }
 </script>
