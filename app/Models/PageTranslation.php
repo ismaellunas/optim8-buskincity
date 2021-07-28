840e5720c9c4327c3ed9c2fc111da2fbefe86724
Bayu <boedy@sdbagency.com>
2021-07-28 16:29:11 +0700

    Add PageTranslation model

---
+++ app/Models/PageTranslation.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Page;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\AsCollection;
+use CloudinaryLabs\CloudinaryLaravel\MediaAlly;
+
+class PageTranslation extends Model
+{
+    use HasFactory;
+    use MediaAlly;
+
+    const STATUS_DRAFT = 0;
+    const STATUS_PUBLISH = 1;
+
+    protected $fillable = [
+        'data',
+        'except',
+        'meta_description',
+        'meta_title',
+        'slug',
+        'status',
+        'title',
+    ];
+
+    protected $casts = [
+        'data' => AsCollection::class,
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function page()
+    {
+        return $this->belongsTo(Page::class);
+    }
+}
