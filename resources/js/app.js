00a341535cfecac6176ea40af3d7e4e332b4bd8d
Bayu <boedy@sdbagency.com>
2021-07-28 11:55:10 +0700

    Add vue-sweetalert npm package

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,16 +5,18 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress';
 import CKEditor from '@ckeditor/ckeditor5-vue';
+import VueSweetalert2 from 'vue-sweetalert2';
 
 createInertiaApp({
     title: title => `${title} - My App`,
-    //resolve: (name) => import(`./Pages/${name}`),
-    resolve: name => require(`./Pages/${name}`),
+    resolve: (name) => import(`./Pages/${name}`),
+    //resolve: name => require(`./Pages/${name}`),
     setup({ el, app, props, plugin }) {
         createApp({ render: () => h(app, props) })
             .mixin({ methods: { route } })
             .use(plugin)
             .use(CKEditor)
+            .use(VueSweetalert2)
             .mount(el)
     },
 })
