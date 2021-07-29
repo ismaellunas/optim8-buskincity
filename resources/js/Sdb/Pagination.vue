5a018287d169c90cfdf8d26ebdcd724dbab471b6
Bayu <boedy@sdbagency.com>
2021-07-29 19:06:56 +0700

    Update Pagination component to support ajax when click the pagination button link

--- resources/js/Sdb/Pagination.vue
+++ resources/js/Sdb/Pagination.vue
@@ -1,27 +1,52 @@
 <template>
     <div v-if="links.length > 3">
         <nav class="pagination is-centered" role="navigation" aria-label="pagination">
-            <a
-                class="pagination-previous"
-                :href="previousLink.url"
-                :disabled="isBlank(previousLink.url) ? 1 : null"
-                v-html="previousLink.label"
+            <template v-if="isAjax">
+                <sdb-button
+                    class="pagination-previous"
+                    :disabled="isBlank(previousLink.url) ? 1 : null"
+                    v-html="previousLink.label"
+                    @click.prevent="$emit('on-clicked-pagination', previousLink.url)"
                 />
-            <a
-                class="pagination-next"
-                :disabled="isBlank(nextLink.url) ? 1 : null"
-                :href="nextLink.url"
-                v-html="nextLink.label"
+                <sdb-button
+                    class="pagination-next"
+                    :disabled="isBlank(nextLink.url) ? 1 : null"
+                    v-html="nextLink.label"
+                    @click.prevent="$emit('on-clicked-pagination', nextLink.url)"
                 />
+            </template>
+            <template v-else>
+                <a
+                    class="pagination-previous"
+                    :href="previousLink.url"
+                    :disabled="isBlank(previousLink.url) ? 1 : null"
+                    v-html="previousLink.label"
+                    />
+                <a
+                    class="pagination-next"
+                    :disabled="isBlank(nextLink.url) ? 1 : null"
+                    :href="nextLink.url"
+                    v-html="nextLink.label"
+                    />
+            </template>
             <ul class="pagination-list">
                 <li v-for="link in paginationLinks">
                     <span class="pagination-ellipsis" v-if="isBlank(link.url)">&hellip;</span>
-                    <sdb-link v-else
-                        :href="link.url"
-                        class="pagination-link"
-                        :class="{'is-current': link.active}"
-                        v-html="link.label"
-                        />
+                    <template v-else>
+                        <sdb-button v-if="isAjax"
+                            class="pagination-link"
+                            :class="{'is-current': link.active}"
+                            v-html="link.label"
+                            @click.prevent="$emit('on-clicked-pagination', link.url)"
+                            type="button"
+                            />
+                        <sdb-link v-else
+                            :href="link.url"
+                            class="pagination-link"
+                            :class="{'is-current': link.active}"
+                            v-html="link.label"
+                            />
+                    </template>
                 </li>
             </ul>
         </nav>
@@ -29,15 +54,19 @@
 </template>
 
 <script>
+    import SdbButton from '@/Sdb/Button';
     import SdbLink from '@/Sdb/Link';
     import { isBlank } from '@/Libs/utils';
 
     export default {
         components: {
+            SdbButton,
             SdbLink,
         },
+        emits: ['on-clicked-pagination'],
         props: {
             links: Array,
+            isAjax: { type: Boolean, default: false, },
         },
         methods: {
             isBlank: isBlank,
