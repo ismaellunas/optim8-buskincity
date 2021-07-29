133db1075ddd9ce14d52a3078a7c815d7c2257fa
Bayu <boedy@sdbagency.com>
2021-07-29 18:54:10 +0700

    Add ModalCard vue component

---
+++ resources/js/Sdb/ModalCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-card" :class="contentClass">
+            <header class="modal-card-head">
+                <slot name="header"></slot>
+            </header>
+            <section class="modal-card-body">
+                <slot></slot>
+            </section>
+            <footer class="modal-card-foot">
+                <slot name="footer"></slot>
+            </footer>
+        </div>
+        <button
+            class="modal-close is-large"
+            type="button"
+            @click="$emit('close')"
+            v-show="!isCloseHidden">
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        emits: ['close'],
+        props: {
+            contentClass: [String, Array],
+            isCloseHidden: {type: Boolean, default: false},
+        },
+    }
+</script>
