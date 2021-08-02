6e34a3e6a2526285828eb72a738d6058a69952f6
Bayu <boedy@sdbagency.com>
2021-08-02 17:21:55 +0700

    Update heading structure and add config setting

--- resources/js/ComponentStructures/heading.js
+++ resources/js/ComponentStructures/heading.js
@@ -4,10 +4,43 @@ export default {
     content: {
         heading: {
             html: '',
-            tag: 'h1',
-            attrs: {
-                class: ['title', 'is-1'],
-            },
         },
+    },
+    config: {
+        tag: "h1",
+        size: "is-1",
+        type: "title",
     }
 }
+
+export const config = {
+    tag: {
+        type: "select",
+        label: "Heading",
+        options: [
+            { value: 'h1', name: 'Heading 1' },
+            { value: 'h2', name: 'Heading 2' },
+            { value: 'h3', name: 'Heading 3' },
+        ],
+    },
+    size: {
+        type: "select",
+        label: "Size",
+        options: [
+            { value: 'is-1', name: '1'},
+            { value: 'is-2', name: '2'},
+            { value: 'is-3', name: '3'},
+            { value: 'is-4', name: '4'},
+            { value: 'is-5', name: '5'},
+            { value: 'is-6', name: '6'},
+        ],
+    },
+    type: {
+        type: "select",
+        label: "Type",
+        options: [
+            { value: 'title', name: 'Title'},
+            { value: 'subtitle', name: 'Subtitle'},
+        ],
+    },
+};
