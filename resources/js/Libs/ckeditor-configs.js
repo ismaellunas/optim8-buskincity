5bbf4bf80b795c5e53ddda4162534890c409aa29
Bayu <boedy@sdbagency.com>
2021-08-03 18:31:46 +0700

    Remove class since it's useless and add some commented trial code

--- resources/js/Libs/ckeditor-configs.js
+++ resources/js/Libs/ckeditor-configs.js
@@ -1,10 +1,13 @@
 export let common = {
     toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', ],
+    //autoParagraph: false,
+    //enterMode: 2,
+    //shiftEnterMode: 1,
     heading: {
         options: [
             { model: 'paragraph', title: 'Paragraph', class: '' },
-            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'title is-1' },
-            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'title is-2' }
+            { model: 'heading1', view: 'h1', title: 'Heading 1' },
+            { model: 'heading2', view: 'h2', title: 'Heading 2' }
         ]
     },
 };
