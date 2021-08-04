d7d34cc5d556e4fb3855e345b8fcfee4c6e17e55
Bayu <boedy@sdbagency.com>
2021-08-04 12:47:14 +0700

    Add style options that contains all options of classes

---
+++ resources/js/ComponentStructures/style-options.js
@@ -0,0 +1,15 @@
+export const sizes = [
+    { value: "is-1", name: "1"},
+    { value: "is-2", name: "2"},
+    { value: "is-3", name: "3"},
+    { value: "is-4", name: "4"},
+    { value: "is-5", name: "5"},
+    { value: "is-6", name: "6"},
+];
+
+export const alignments = [
+    { value: "has-text-centered", name: "Centered"},
+    { value: "has-text-justified", name: "Justified"},
+    { value: "has-text-left", name: "Left"},
+    { value: "has-text-right", name: "Right"},
+];
