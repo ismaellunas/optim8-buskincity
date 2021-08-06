a4dfe82075a57779a1d43099a5e18b1412228639
Bayu <boedy@sdbagency.com>
2021-08-06 11:07:00 +0700

    Register Text to list of component that can be used

--- resources/js/ComponentStructures/index.js
+++ resources/js/ComponentStructures/index.js
@@ -1,15 +1,13 @@
 import card from './card';
 import cardText from './card-text';
 import heading from './heading';
-//import h1 from './h1';
-//import h2 from './h2';
 import image from './image';
+import text from './text';
 
 export default {
     card: card,
     cardText: cardText,
     heading: heading,
-    //h1: h1,
-    //h2: h2,
     image: image,
+    text: text,
 }
