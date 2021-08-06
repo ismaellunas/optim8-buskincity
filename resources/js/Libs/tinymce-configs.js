18fe1f557b72017894590cf3c41c8b2319ef03a7
Bayu <boedy@sdbagency.com>
2021-08-06 08:57:48 +0700

    Add Tinymce vue component and config

---
+++ resources/js/Libs/tinymce-configs.js
@@ -0,0 +1,14 @@
+export const textComponent = {
+    //height: 500,
+    //toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | help',
+    inline: true,
+    menubar: false,
+    plugins: [
+        'advlist autolink lists link image charmap',
+        'searchreplace visualblocks code fullscreen',
+        'print preview anchor insertdatetime media',
+        'paste code help wordcount table'
+    ],
+    block_formats: 'Paragraph=p; Header 1=h1; Header 2=h2; Header 3=h3',
+    toolbar: 'formatselect | bold italic link | bullist numlist | table',
+};
---
+++ resources/js/Sdb/EditorTinymce.vue
@@ -0,0 +1,25 @@
+<template>
+    <editor
+        v-model="content"
+        :api-key="apiKey"
+        :init="editorConfig"
+    >
+    </editor>
+</template>
+
+<script>
+  import Editor from '@tinymce/tinymce-vue';
+  import { textComponent as editorConfig } from '@/Libs/tinymce-configs';
+
+  export default {
+    components: {
+      editor: Editor
+    },
+    data() {
+        return {
+            apiKey: "gumlypbnhr1ushb1hnnynxp4n6osqza60b3hn7hdtfa6khjb",
+            editorConfig: editorConfig,
+        };
+    }
+  }
+</script>
