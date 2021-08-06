979bbdce6aba22bdd40bb93e72b663ca59065f52
Bayu <boedy@sdbagency.com>
2021-08-06 09:20:51 +0700

    Add exported variable defaultOption, contentSizes, suffixNumbers, and rename sizes to headingSizes

--- resources/js/ComponentStructures/style-options.js
+++ resources/js/ComponentStructures/style-options.js
@@ -1,4 +1,8 @@
-export const sizes = [
+export const defaultOption = [
+    { value: null, name: "(Default)"},
+];
+
+export const headingSizes = [
     { value: "is-1", name: "1"},
     { value: "is-2", name: "2"},
     { value: "is-3", name: "3"},
@@ -12,4 +16,20 @@ export const alignments = [
     { value: "has-text-justified", name: "Justified"},
     { value: "has-text-left", name: "Left"},
     { value: "has-text-right", name: "Right"},
+];
+
+export const contentSizes = [
+    { value: "is-small", name: "Small" },
+    { value: "is-normal", name: "Normal" },
+    { value: "is-medium", name: "Medium" },
+    { value: "is-large", name: "Large" },
+];
+
+export const suffixNumbers = [
+    { value: "1", name: "1"},
+    { value: "2", name: "2"},
+    { value: "3", name: "3"},
+    { value: "4", name: "4"},
+    { value: "5", name: "5"},
+    { value: "6", name: "6"},
 ];
