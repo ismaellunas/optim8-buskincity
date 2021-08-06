aa23dbb87c167b29c724bf9d7982ea4c9e53696e
Bayu <boedy@sdbagency.com>
2021-08-06 11:15:32 +0700

    Remove unused code since they are covered in loadedmixin

--- resources/js/Mixins/EditModeContent.js
+++ resources/js/Mixins/EditModeContent.js
@@ -2,14 +2,4 @@ import EditModeComponentMixin from './EditModeComponent';
 
 export default {
     mixins: [EditModeComponentMixin],
-    data() {
-        return {
-            editModeWrapperClass: ['edit-mode-content'],
-        };
-    },
-    computed: {
-        canEdit() {
-            return this.isEditMode;
-        }
-    }
 }
