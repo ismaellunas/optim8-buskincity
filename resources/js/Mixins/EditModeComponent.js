eccf5fa8971e7f50643d2ebba84ece73e9064c9b
Bayu <boedy@sdbagency.com>
2021-08-06 11:14:14 +0700

    Remove contentClass and wrapperClass since they are unused

--- resources/js/Mixins/EditModeComponent.js
+++ resources/js/Mixins/EditModeComponent.js
@@ -6,15 +6,12 @@ export default {
     },
     data() {
         return {
-            contentClass: [],
             editModeWrapperClass: [],
         };
     },
     computed: {
         wrapperClass() {
             let wrapperClass = [];
-
-            wrapperClass = wrapperClass.concat(this.contentClass ?? []);
 
             if (this.isEditMode) {
                 wrapperClass = wrapperClass.concat(this.editModeWrapperClass);
