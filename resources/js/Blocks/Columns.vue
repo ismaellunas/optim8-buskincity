d4c29e843a8fb8cdfa0a96f667c344213d3c7017
Bayu <boedy@sdbagency.com>
2021-08-06 11:02:14 +0700

    Remove wrappedClass variable since it is no longer in use

--- resources/js/Blocks/Columns.vue
+++ resources/js/Blocks/Columns.vue
@@ -121,8 +121,6 @@
             wrapperClass() {
                 let wrapperClass = [];
 
-                wrapperClass = wrapperClass.concat(this.contentClass ?? []);
-
                 if (this.isEditMode) {
                     wrapperClass = wrapperClass.concat(
                         this.editModeWrapperClass,
