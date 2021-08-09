db60d4d1f0bc9797131ed914c294d742b5ccf1f3
Bayu <boedy@sdbagency.com>
2021-08-09 17:11:38 +0700

    Add createTrblClasses() , createPaddingClasses(), createMarginClasses()

--- resources/js/Libs/page-builder.js
+++ resources/js/Libs/page-builder.js
@@ -14,3 +14,24 @@ export function createBlock() {
         columns: [],
     }
 }
+
+export function createTrblClasses(trbl, prefix) {
+    const suffix = {top: 't', right: 'r', bottom: 'b', left: 'l'};
+    let classes = [];
+    if (trbl) {
+        for (const [key, value] of Object.entries(trbl)) {
+            if (value) {
+                classes.push(prefix + suffix[key] + '-' + value);
+            }
+        }
+    }
+    return classes;
+};
+
+export function createPaddingClasses(trbl) {
+    return createTrblClasses(trbl, 'p');
+};
+
+export function createMarginClasses(trbl) {
+    return createTrblClasses(trbl, 'm');
+};
