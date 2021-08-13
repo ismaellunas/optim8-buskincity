6ce79ec3976009dca20eaa99854830a12a5b9c2a
Bayu <boedy@sdbagency.com>
2021-08-13 13:03:04 +0700

    Add accessor thumbnailUrl for Media

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -3,11 +3,23 @@
 namespace App\Models;
 
 use CloudinaryLabs\CloudinaryLaravel\Model\Media as CloudinaryMedia;
+use Cloudinary\Transformation\Resize;
 
 class Media extends CloudinaryMedia
 {
     public function scopeImage($query)
     {
         return $query->where('file_type', 'image');
+    }
+
+    // Accessors:
+    public function getThumbnailUrlAttribute()
+    {
+        $result = cloudinary()
+           ->getImageTag($this->file_name)
+           ->resize(Resize::thumbnail()->width(300)->height(300))
+           ->serializeAttributes();
+
+        return strval(str_replace(['src=', '"'], ['', ''], $result));
     }
 }
