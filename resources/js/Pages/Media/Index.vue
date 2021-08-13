6f0aafcc337f9c1c8a03e40fc05cd2fc0e5b1aae
Bayu <boedy@sdbagency.com>
2021-08-13 13:15:35 +0700

    Use MediaLibrary and provide some required methods

--- resources/js/Pages/Media/Index.vue
+++ resources/js/Pages/Media/Index.vue
@@ -5,41 +5,18 @@
     </template>
 
     <div class="box">
-        <div class="columns">
-            <div class="column">
-                <sdb-button-link :href="route(baseRouteName+'.create')" class="is-primary">
-                    Upload
-                </sdb-button-link>
-            </div>
-        </div>
-
-        <div class="table-container">
-            <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-                <thead>
-                    <tr>
-                        <th>Image</th>
-                        <th>Size (bytes)</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="record in records.data" :key="record.id">
-                        <th>
-                            <figure
-                                class="image is-96x96"
-                                v-html="record.tag_url">
-                            </figure>
-                        </th>
-                        <td>{{ record.size }}</td>
-                        <td>
-                            <sdb-button class="is-danger ml-2" @click.prevent="deleteRecord(record)">
-                                Delete
-                            </sdb-button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <sdb-media-library
+            :records="records"
+            :upload-route="route('admin.media.upload-image')"
+            @on-media-upload-success="onMediaUploadSuccess"
+        >
+            <template v-slot:actions="slotProps">
+                <!--
+                <sdb-link class="card-footer-item p-2" @click.prevent="editRecord(slotProps.media)">Edit</sdb-link>
+                -->
+                <sdb-link class="card-footer-item p-2" @click.prevent="deleteRecord(slotProps.media)">Delete</sdb-link>
+            </template>
+        </sdb-media-library>
     </div>
 </app-layout>
 </template>
@@ -48,14 +25,22 @@
     import AppLayout from '@/Layouts/AppLayout';
     import SdbButton from '@/Sdb/Button';
     import SdbButtonLink from '@/Sdb/ButtonLink';
+    import SdbLink from '@/Sdb/Link';
+    import SdbMediaLibrary from '@/Sdb/MediaLibrary';
 
     export default {
         components: {
             AppLayout,
             SdbButton,
             SdbButtonLink,
+            SdbLink,
+            SdbMediaLibrary,
         },
-        props: ['records', 'baseRouteName'],
+        props: {
+            records: {},
+            baseRouteName: {},
+            files: {default: []},
+        },
         methods: {
             getEditRoute(id) {
                 // return this.baseRouteName.replace('.', '/')+'/'+id+'/edit';
@@ -64,9 +49,13 @@
             deleteRecord(record) {
                 if (!confirm('Are you sure?')) return;
                 this.$inertia.delete(route(this.baseRouteName+'.destroy', {id: record.id}));
-            }
+            },
+            editRecord(media) {
+                console.log('mediaId: ' + media.id);
+            },
+            onMediaUploadSuccess(response) {
+                this.$inertia.reload();
+            },
         },
-        computed: {
-        }
     }
 </script>
