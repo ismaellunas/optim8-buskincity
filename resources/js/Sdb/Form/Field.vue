e44f169b26479bb062ac55817a1bec636f99740c
Bayu <boedy@sdbagency.com>
2021-08-18 09:01:00 +0700

    Add Sdb/Form/Field vue component

---
+++ resources/js/Sdb/Form/Field.vue
@@ -0,0 +1,25 @@
+<template>
+    <sdb-field>
+        <sdb-label>
+            <slot name="label"></slot>
+        </sdb-label>
+
+        <div class="control">
+            <slot></slot>
+        </div>
+
+        <slot name="error"></slot>
+    </sdb-field>
+</template>
+
+<script>
+    import SdbField from '@/Sdb/Field';
+    import SdbLabel from '@/Sdb/Label';
+
+    export default {
+        components: {
+            SdbField,
+            SdbLabel,
+        },
+    }
+</script>
