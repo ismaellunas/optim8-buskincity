de363b2c301a405a4bc75fabaca5b5fb7ed6c345
Bayu <boedy@sdbagency.com>
2021-08-18 08:32:32 +0700

    Add Sdb/ButtonIcon vue component

---
+++ resources/js/Sdb/ButtonIcon.vue
@@ -0,0 +1,24 @@
+<template>
+    <sdb-button>
+        <span
+            class="icon"
+            :class="iconClass"
+        >
+            <i :class="icon"></i>
+        </span>
+    </sdb-button>
+</template>
+
+<script>
+    import SdbButton from '@/Sdb/Button';
+
+    export default {
+        components: {
+            SdbButton,
+        },
+        props: [
+            'icon',
+            'iconClass',
+        ],
+    }
+</script>
