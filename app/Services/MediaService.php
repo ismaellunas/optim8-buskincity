08298993b079287f21c2b36269c2042a19b39f1b
Bayu <boedy@sdbagency.com>
2021-08-23 12:09:43 +0700

    Add isFileNameExists() as public static method

--- app/Services/MediaService.php
+++ app/Services/MediaService.php
@@ -7,9 +7,20 @@ use Illuminate\Support\Str;
 
 class MediaService
 {
-    public static function getUniqueFileName(string $fileName): string
+    public static function isFileNameExists(string $fileName, $exceptedIds = []): bool
     {
-        if (Media::where('file_name', $fileName)->exists()) {
+        $queryBuilder = Media::where('file_name', $fileName);
+
+        if (!empty($exceptedIds)) {
+            $queryBuilder->whereIn('id', $exceptedIds);
+        }
+
+        return $queryBuilder->exists();
+    }
+
+    public static function getUniqueFileName(string $fileName, $exceptedIds = []): string
+    {
+        if (self::isFileNameExists($fileName, $exceptedIds)) {
             $fileName .= '_'.Str::lower(Str::random(6));
         }
         return $fileName;
