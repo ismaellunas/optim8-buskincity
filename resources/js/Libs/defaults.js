524ae26152cb6dddc2b85ffe50c6d7af196f4020
Bayu <boedy@sdbagency.com>
2021-08-24 09:42:15 +0700

    Add defaults.js to contains system default values

---
+++ resources/js/Libs/defaults.js
@@ -0,0 +1,21 @@
+export const acceptedFileTypes = [
+    '.doc',
+    '.docx',
+    '.pdf',
+    'application/msword',
+    'application/pdf',
+    'application/vnd.ms-excel',
+    'application/vnd.ms-powerpoint',
+    'application/vnd.oasis.opendocument.text',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'image/jpeg',
+    'image/png',
+    'video/*',
+];
+
+export const acceptedImageTypes = [
+    'image/jpeg',
+    'image/png',
+];
