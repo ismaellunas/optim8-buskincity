0c704acd8adb878eed4ad41de05543b23d06ef28
Bayu <boedy@sdbagency.com>
2021-08-25 15:15:53 +0700

    Add debug as shared data

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,6 +40,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
+            'debug' => config('app.debug'),
             'flash' => [
                 'message' => fn () => $request->session()->get('message')
             ],
