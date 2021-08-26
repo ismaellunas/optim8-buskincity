fca05d26417092552822a177b9c439e45724971c
Bayu <boedy@sdbagency.com>
2021-08-26 13:17:20 +0700

    Update configuration for imageRatios and fixedSquares to use options from style-options

--- resources/js/ComponentStructures/image.js
+++ resources/js/ComponentStructures/image.js
@@ -1,3 +1,5 @@
+import { defaultOption, imageRatios, fixedSquares } from './style-options';
+
 export default {
     title: 'Image',
     componentName: 'Image',
@@ -5,23 +7,17 @@ export default {
         figure: {
             image: {
                 src: null,
-                alt: null,
-                //attrs: {
-                //    class: [],
-                //},
+                mediaId: null,
             },
-            //attrs: {
-            //    class: [],
-            //},
         },
     },
     config: {
         image: {
             // figure
-            fixedSquare: "", // is-16x16, etc..
-            ratio: "is-4by3", // is-square
+            fixedSquare: null,
+            ratio: "is-4by3",
             // img
-            rounded: "", // is-rounded
+            rounded: "",
         }
     }
 }
@@ -33,39 +29,12 @@ export const config = {
             fixedSquare: {
                 type: "select",
                 label: "FixedSquare",
-                options: [
-                    { value: "", name: '- Empty -' },
-                    { value: "is-16x16", name: "16x16 px" },
-                    { value: "is-24x24", name: "24x24 px" },
-                    { value: "is-32x32", name: "32x32 px" },
-                    { value: "is-48x48", name: "48x48 px" },
-                    { value: "is-64x64", name: "64x64 px" },
-                    { value: "is-96x96", name: "96x96 px" },
-                    { value: "is-128x128", name: "128x128 px" },
-                ],
+                options: defaultOption.concat(fixedSquares)
             },
             ratio: {
                 type: "select",
                 label: "Ratio",
-                options: [
-                    { value: "", name: "- Empty -" },
-                    { value: "is-square", name: "Square"},
-                    { value: "is-1by1", name: "1 by 1"},
-                    { value: "is-5by4", name: "5 by 4"},
-                    { value: "is-4by3", name: "4 by 3"},
-                    { value: "is-3by2", name: "3 by 2"},
-                    { value: "is-5by3", name: "5 by 3"},
-                    { value: "is-16by9", name: "16 by 9"},
-                    { value: "is-2by1", name: "2 by 1"},
-                    { value: "is-3by1", name: "3 by 1"},
-                    { value: "is-4by5", name: "4 by 5"},
-                    { value: "is-3by4", name: "3 by 4"},
-                    { value: "is-2by3", name: "2 by 3"},
-                    { value: "is-3by5", name: "3 by 5"},
-                    { value: "is-9by16", name: "9 by 16"},
-                    { value: "is-1by2", name: "1 by 2"},
-                    { value: "is-1by3", name: "1 by 3"},
-                ],
+                options: defaultOption.concat(imageRatios)
             },
             rounded: {
                 type: "select",
