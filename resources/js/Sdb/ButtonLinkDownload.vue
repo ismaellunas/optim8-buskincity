eb35ad377ddd14802167cd8ef66f5bb23b0ea235
Bayu <boedy@sdbagency.com>
2021-08-27 16:29:04 +0700

    Add SdbButtonLinkDownload

---
+++ resources/js/Sdb/ButtonLinkDownload.vue
@@ -0,0 +1,17 @@
+<template>
+    <a
+        class="button"
+        :target="target"
+        download
+    >
+        <slot></slot>
+    </a>
+</template>
+
+<script>
+    export default {
+        props: {
+            target: {type: String, default: '_blank'},
+        }
+    }
+</script>
