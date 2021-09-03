9ac7f34e5aff574206e19936e23d373d36ab35a2
Bayu <boedy@sdbagency.com>
2021-09-03 11:58:46 +0700

    Handle pagination links to consider query params

--- resources/js/Sdb/Pagination.vue
+++ resources/js/Sdb/Pagination.vue
@@ -18,20 +18,25 @@
             <template v-else>
                 <a
                     class="pagination-previous"
-                    :href="previousLink.url"
-                    :disabled="isBlank(previousLink.url) ? 1 : null"
+                    :href="previousLinkUrl"
+                    :disabled="isBlank(previousLinkUrl) ? 1 : null"
                     v-html="previousLink.label"
                     />
                 <a
                     class="pagination-next"
-                    :disabled="isBlank(nextLink.url) ? 1 : null"
-                    :href="nextLink.url"
+                    :disabled="isBlank(nextLinkUrl) ? 1 : null"
+                    :href="nextLinkUrl"
                     v-html="nextLink.label"
                     />
             </template>
             <ul class="pagination-list">
                 <li v-for="link in paginationLinks">
-                    <span class="pagination-ellipsis" v-if="isBlank(link.url)">&hellip;</span>
+                    <span
+                        v-if="isBlank(link.url)"
+                        class="pagination-ellipsis"
+                    >
+                        &hellip;
+                    </span>
                     <template v-else>
                         <sdb-button v-if="isAjax"
                             class="pagination-link"
@@ -41,7 +46,7 @@
                             type="button"
                             />
                         <sdb-link v-else
-                            :href="link.url"
+                            :href="link.url+serializedParams"
                             class="pagination-link"
                             :class="{'is-current': link.active}"
                             v-html="link.label"
@@ -56,7 +61,7 @@
 <script>
     import SdbButton from '@/Sdb/Button';
     import SdbLink from '@/Sdb/Link';
-    import { isBlank } from '@/Libs/utils';
+    import { isBlank, serialize } from '@/Libs/utils';
 
     export default {
         components: {
@@ -67,6 +72,7 @@
         props: {
             links: Array,
             isAjax: { type: Boolean, default: false, },
+            queryParams: { type: Object, default: {} },
         },
         methods: {
             isBlank: isBlank,
@@ -75,8 +81,20 @@
             previousLink() {
                 return this.links[0];
             },
+            previousLinkUrl() {
+                if (!isBlank(this.previousLink.url)) {
+                    return this.previousLink.url + this.serializedParams;
+                }
+                return '';
+            },
             nextLink() {
                 return this.links[ this.links.length - 1 ];
+            },
+            nextLinkUrl() {
+                if (!isBlank(this.nextLink.url)) {
+                    return this.nextLink.url + this.serializedParams;
+                }
+                return '';
             },
             paginationLinks() {
                 const paginationLinks = this.links.slice();
@@ -85,6 +103,12 @@
                 paginationLinks.shift();
 
                 return paginationLinks;
+            },
+            serializedParams() {
+                if (!isBlank(this.queryParams)) {
+                    return '&'+serialize( this.queryParams );
+                }
+                return '';
             },
         }
     }
