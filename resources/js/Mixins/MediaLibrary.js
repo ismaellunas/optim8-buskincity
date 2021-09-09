38d1612347cfc26b6025cc251cb9ce487c8c6437
Bayu <boedy@sdbagency.com>
2021-09-09 18:37:39 +0700

    Add MediaLibrary mixin

---
+++ resources/js/Mixins/MediaLibrary.js
@@ -0,0 +1,41 @@
+import { oops as oopsAlert } from '@/Libs/alert';
+
+export default {
+    name: 'MediaLibraryMixin',
+    data() {
+        return {
+            media: [],
+            imageListQueryParams: {},
+            imageListRouteName: 'admin.media.list.image',
+        };
+    },
+    methods: {
+        getImagesList(url) {
+            const self = this;
+            axios.get(url, {params: this.imageListQueryParams})
+                .then(function (response) {
+                    self.setMedia(response.data);
+                    self.onImageListLoadedSuccess(response);
+                })
+                .catch(function (error) {
+                    oopsAlert();
+                    self.onImageListLoadedFail(error);
+                });
+        },
+        setTerm(term) {
+            this.imageListQueryParams['term'] = term;
+        },
+        setView(view) {
+            this.imageListQueryParams['view'] = view;
+        },
+        search(term) {
+            this.setTerm(term);
+            this.getImagesList(route(this.imageListRouteName));
+        },
+        setMedia(media) {
+            this.media = media;
+        },
+        onImageListLoadedFail() {},
+        onImageListLoadedSuccess() {},
+    },
+}
