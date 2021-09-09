eb3e527f01de99b9c65b3b7685e62bd2a3e6314a
Bayu <boedy@sdbagency.com>
2021-09-09 18:38:22 +0700

    Add Sdb/Form/TextEditor vue component

---
+++ resources/js/Sdb/Form/TextEditor.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="field">
+        <sdb-label>{{ label }}</sdb-label>
+
+        <div class="control">
+            <sdb-text-editor
+                v-model="editorValue"
+                :disabled="disabled"
+                :placeholder="placeholder"
+                :rows="rows"
+                :config="config"
+            />
+        </div>
+
+        <sdb-input-error :message="message"/>
+    </div>
+</template>
+
+<script>
+    import SdbInputError from '@/Sdb/InputError';
+    import SdbLabel from '@/Sdb/Label';
+    import SdbTextEditor from '@/Sdb/EditorTinymce';
+    import { useModelWrapper } from '@/Libs/utils';
+
+    export default {
+        name: 'SdbFormTextEditor',
+        components: {
+            SdbInputError,
+            SdbLabel,
+            SdbTextEditor,
+        },
+        emits: ['update:modelValue'],
+        setup(props, { emit }) {
+            return {
+                editorValue: useModelWrapper(props, emit),
+            };
+        },
+        props: [
+            'config',
+            'disabled',
+            'label',
+            'message',
+            'modelValue',
+            'placeholder',
+            'rows',
+        ],
+    }
+</script>
