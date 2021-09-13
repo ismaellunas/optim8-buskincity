6a86f3add168c3ae48022060ffb2ceb60818fe64
Bayu <boedy@sdbagency.com>
2021-09-13 18:25:08 +0700

    Add Sdb/Form/InputAddons vue component

---
+++ resources/js/Sdb/Form/InputAddons.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <sdb-label>{{ label }}</sdb-label>
+
+        <sdb-field class="has-addons mb-0">
+            <div class="control is-expanded">
+                <sdb-input
+                    :disabled="disabled"
+                    :placeholder="placeholder"
+                    :required="required"
+                    :value="modelValue"
+                    @input="$emit('update:modelValue', $event.target.value)"
+                    @keypress="$emit('on-keypress', $event)"
+                    @blur="$emit('on-blur', $event)"
+                />
+            </div>
+
+            <slot name="afterInput"></slot>
+        </sdb-field>
+
+        <sdb-input-error :message="message"/>
+    </div>
+</template>
+
+<script>
+    import SdbField from '@/Sdb/Field';
+    import SdbInput from '@/Sdb/Input';
+    import SdbInputError from '@/Sdb/InputError';
+    import SdbLabel from '@/Sdb/Label';
+
+    export default {
+        name: 'SdbFormInputAddons',
+        components: {
+            SdbField,
+            SdbInput,
+            SdbInputError,
+            SdbLabel,
+        },
+        emits: [
+            'on-blur',
+            'on-keypress',
+            'update:modelValue',
+        ],
+        props: [
+            'disabled',
+            'label',
+            'message',
+            'modelValue',
+            'placeholder',
+            'required',
+        ],
+    }
+</script>
