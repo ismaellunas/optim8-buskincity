b2beba19b2c2c1c24419e25ab5abf852bdeff058
Bayu <boedy@sdbagency.com>
2021-09-13 18:28:01 +0700

    Add asterisk for label if is-request is true

--- resources/js/Sdb/Label.vue
+++ resources/js/Sdb/Label.vue
@@ -1,11 +1,15 @@
 <template>
-    <label class="label" :class="class"><slot></slot></label>
+    <label class="label" :class="class">
+        <slot></slot>
+        <span v-if="isRequired">*</span>
+    </label>
 </template>
 
 <script>
     export default {
         props: {
-            class: {default: []}
+            class: {default: []},
+            isRequired: Boolean,
         }
     }
 </script>
