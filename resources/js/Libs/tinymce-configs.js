f3522dd05c24ee87df3350bf97491e59fe3a6778
Bayu <boedy@sdbagency.com>
2021-09-13 18:26:07 +0700

    Remove fullEditorConfig as exported variable

--- resources/js/Libs/tinymce-configs.js
+++ resources/js/Libs/tinymce-configs.js
@@ -13,21 +13,3 @@ export const textComponent = {
     block_formats: 'Paragraph=p; Header 1=h1; Header 2=h2; Header 3=h3',
     toolbar: 'formatselect | bold italic link | bullist numlist | table',
 };
-
-export const fullEditorConfig = {
-    //inline: false,
-    height: 300,
-    menubar: false,
-    branding: false,
-    plugins: [
-        'advlist autolink lists link image charmap print preview anchor',
-        'searchreplace visualblocks code fullscreen',
-        'insertdatetime media table paste code help wordcount'
-    ],
-    block_formats: 'Paragraph=p; Header 1=h1; Header 2=h2; Header 3=h3',
-    toolbar: 'undo redo | formatselect | ' +
-    'bold italic backcolor | alignleft aligncenter ' +
-    'alignright alignjustify | bullist numlist outdent indent | ' +
-    'removeformat ',
-    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-};
