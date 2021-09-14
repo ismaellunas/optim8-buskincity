952840ba29fedd6e1c087dce0f65312413ce2146
Bayu <boedy@sdbagency.com>
2021-09-14 19:24:03 +0700

    Add Sdb/DateTime vue component

---
+++ resources/js/Sdb/DateTime.vue
@@ -0,0 +1,35 @@
+<template>
+    <bulma-calendar
+        ref="datetimepicker"
+        :value="modelValue"
+        @input="updateDate"
+        :options="options"
+    />
+</template>
+
+<script>
+    import BulmaCalendar from "bulma-calendar/dist/components/vue/bulma_calendar";
+
+    export default {
+        components: {
+            BulmaCalendar,
+        },
+        props: {
+            modelValue: Date|Array|null,
+        },
+        emits: ['update:modelValue'],
+        data() {
+            return {
+                options: {},
+            };
+        },
+        methods: {
+            focus() {
+                this.$refs.input.focus()
+            },
+            updateDate(date) {
+                this.$emit('update:modelValue', date);
+            }
+        },
+    };
+</script>
