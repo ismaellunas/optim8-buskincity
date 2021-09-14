7950e9e52df11d1598d924e5956eb0d7f342421d
Bayu <boedy@sdbagency.com>
2021-09-14 15:57:28 +0700

    Add Sdb/Tab and Sdb/TabList vue components

---
+++ resources/js/Sdb/Tab.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="tabs">
+        <ul>
+            <slot></slot>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SdbTab',
+    };
+</script>
---
+++ resources/js/Sdb/TabList.vue
@@ -0,0 +1,14 @@
+<template>
+    <li :class="{'is-active': isActive}">
+        <slot></slot>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: 'SdbTabList',
+        props: {
+            isActive: Boolean,
+        },
+    };
+</script>
