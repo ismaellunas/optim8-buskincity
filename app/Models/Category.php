ce5af413215139d479f24502aef6c846a6f18d32
Bayu <boedy@sdbagency.com>
2021-09-15 12:26:13 +0700

    Change parent class from Model to BaseModel

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,9 +5,8 @@ namespace App\Models;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model implements TranslatableContract
+class Category extends BaseModel implements TranslatableContract
 {
     use HasFactory;
     use Translatable;
