5e44290fee01cd7b67c5f933f3cddad88f1d36d7
Bayu <boedy@sdbagency.com>
2021-09-20 16:49:11 +0700

    Add PostObserver and register it in EventServiceProvider

---
+++ app/Observers/PostObserver.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Post;
+use App\Jobs\ProcessPublishScheduledPost;
+
+class PostObserver
+{
+    public function creating(Post $post)
+    {
+        $post->author_id = auth()->id();
+    }
+
+    public function saved(Post $post)
+    {
+        if ($post->isChangedToScheduled) {
+            ProcessPublishScheduledPost::dispatch($post)
+                ->delay($post->scheduled_at);
+        }
+    }
+
+    /**
+     * Handle the Post "created" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function created(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Handle the Post "updated" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function updated(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Handle the Post "deleted" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function deleted(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Handle the Post "restored" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function restored(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Handle the Post "force deleted" event.
+     *
+     * @param  \App\Models\Post  $post
+     * @return void
+     */
+    public function forceDeleted(Post $post)
+    {
+        //
+    }
+}
