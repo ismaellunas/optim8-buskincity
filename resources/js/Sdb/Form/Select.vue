ee8186dc9208d4091f34d0bf609a17c0485b4202
Bayu <boedy@sdbagency.com>
2021-09-21 12:32:49 +0700

    Use SdbFormField and also update props to use Object style

--- resources/js/Sdb/Form/Select.vue
+++ resources/js/Sdb/Form/Select.vue
@@ -1,40 +1,56 @@
 <template>
-    <div class="field">
-        <sdb-label>{{ label }}</sdb-label>
+    <sdb-form-field
+        :is-required="required"
+    >
+        <template v-slot:label>{{ label }}</template>
 
-        <div class="control">
-            <sdb-select
-                v-model="selected"
-                :disabled="disabled"
-                :placeholder="placeholder"
-                :class="class"
-            >
-                <slot></slot>
-            </sdb-select>
-        </div>
+        <sdb-select
+            v-model="selected"
+            :disabled="disabled"
+            :placeholder="placeholder"
+            :class="class"
+        >
+            <slot></slot>
+        </sdb-select>
 
-        <sdb-input-error :message="message"/>
-    </div>
+        <template v-slot:error>
+            <sdb-input-error :message="message"/>
+        </template>
+    </sdb-form-field>
 </template>
 
 <script>
+    import SdbFormField from '@/Sdb/Form/Field';
     import SdbInputError from '@/Sdb/InputError';
-    import SdbLabel from '@/Sdb/Label';
     import SdbSelect from '@/Sdb/Select';
     import { useModelWrapper } from '@/Libs/utils';
 
     export default {
         name: 'SdbFormSelect',
         components: {
+            SdbFormField,
             SdbInputError,
-            SdbLabel,
             SdbSelect,
         },
-        props: ['label', 'message', 'modelValue', 'placeholder', 'disabled', 'class'],
+        props: {
+            class: {},
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            label: String,
+            message: Object|null,
+            modelValue: {},
+            placeholder: String,
+            required: {
+                type: Boolean,
+                default: false
+            },
+        },
         setup(props, { emit }) {
             return {
                 selected: useModelWrapper(props, emit),
             };
         },
-    }
+    };
 </script>
