cfc67d2f03dfdce5c863f3a432a3b08abf06f554
Bayu <boedy@sdbagency.com>
2021-09-22 15:13:52 +0700

    Add CategoryRequest FormRequest and handle populate rule() method

---
+++ app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Astrotomic\Translatable\Validation\RuleFactory;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return RuleFactory::make([
+            '%name%' => 'sometimes|string',
+        ]);
+    }
+}
