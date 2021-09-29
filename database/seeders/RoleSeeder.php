dd8f1aa59a402a91fd11137235d92b61ed9cb031
Bayu <boedy@sdbagency.com>
2021-09-29 10:03:23 +0700

    Add RoleSeeder db seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['name' => 'Super Administrator']);
+        Role::create(['name' => 'Administrator']);
+        Role::create(['name' => 'Author']);
+    }
+}
