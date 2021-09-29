be9c428c129ac7e740e1ccade233308271daf8c4
Bayu <boedy@sdbagency.com>
2021-09-29 09:03:04 +0700

    Add Password Rule that extend Fortify Password Rule

---
+++ app/Rules/Password.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Laravel\Fortify\Rules\Password as FortifyPassword;
+
+class Password extends FortifyPassword implements Rule
+{
+    protected $requireUppercase = true;
+
+    protected $requireNumeric = true;
+
+    protected $requireSpecialCharacter = true;
+}
