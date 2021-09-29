00626f28ee893fedd6cb4aa26a70f4258dd075d1
Bayu <boedy@sdbagency.com>
2021-09-29 10:00:31 +0700

    Add UserPasswordRequest

---
+++ app/Http/Requests/UserPasswordRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Actions\Fortify\PasswordValidationRules;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserPasswordRequest extends FormRequest
+{
+    use PasswordValidationRules;
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'password' => $this->passwordRules(),
+        ];
+    }
+}
