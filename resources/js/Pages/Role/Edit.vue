63fb50cdc04edd33aae98e9ae3ed3562f4689993
Bayu <boedy@sdbagency.com>
2021-09-30 19:22:39 +0700

    Update Role/Edit component

--- resources/js/Pages/Role/Edit.vue
+++ resources/js/Pages/Role/Edit.vue
@@ -1,27 +1,112 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Hi
-            </h2>
-        </template>
+<app-layout>
+    <template #header>Role</template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    Hi
-                </div>
+    <sdb-error-notifications :errors="$page.props.errors"/>
+
+    <div class="mb-6">
+        <form
+            class="columns"
+            method="post"
+            @submit.prevent="onSubmit"
+        >
+            <div class="column">
+                <fieldset
+                    class="box"
+                    :disabled="isProcessing"
+                >
+                    <h3 class="title is-3">Profile</h3>
+                    <hr/>
+
+                    <form-role
+                        v-model="form"
+                        :errors="errors"
+                        :permission-options="permissions"
+                    ></form-role>
+
+                    <div class="field is-grouped is-grouped-right">
+                        <div class="control">
+                            <sdb-button-link
+                                :href="route(baseRouteName+'.index')"
+                                class="is-link is-light">
+                                Cancel
+                            </sdb-button-link>
+                        </div>
+                        <div class="control">
+                            <sdb-button class="is-link">
+                                Update
+                            </sdb-button>
+                        </div>
+                    </div>
+                </fieldset>
             </div>
-        </div>
-    </app-layout>
+        </form>
+    </div>
+
+</app-layout>
 </template>
 
 <script>
-    import AppLayout from '@/Layouts/AppLayout'
+    import AppLayout from '@/Layouts/AppLayout';
+    import FormRole from '@/Pages/Role/Form';
+    import SdbButton from '@/Sdb/Button';
+    import SdbButtonLink from '@/Sdb/ButtonLink';
+    import SdbErrorNotifications from '@/Sdb/ErrorNotifications';
+    import { map } from 'lodash';
+    import { success as successAlert } from '@/Libs/alert';
+    import { useForm } from '@inertiajs/inertia-vue3';
 
     export default {
         components: {
             AppLayout,
+            SdbButton,
+            SdbButtonLink,
+            SdbErrorNotifications,
+            FormRole,
         },
-    }
+        props: {
+            baseRouteName: String,
+            errors: Object,
+            permissions: {},
+            record: Object,
+        },
+        setup(props) {
+            const role = props.record;
+            const form = {
+                name: role.name,
+                permissions: role.permissions ? role.permissions.map(permission => permission.name) : [],
+            };
+
+            return {
+                form: useForm(form),
+            };
+        },
+        data() {
+            return {
+                isProcessing: false,
+                loader: null,
+            };
+        },
+        methods: {
+            onSubmit() {
+                const self = this;
+                const form = self.form;
+                form.put(route(this.baseRouteName+'.update', this.record.id), {
+                    preserveScroll: false,
+                    onStart: () => {
+                        self.loader = self.$loading.show();
+                        self.isProcessing = true;
+                    },
+                    onSuccess: (page) => {
+                        successAlert(page.props.flash.message);
+                        form.isDirty = false;
+                    },
+                    onFinish: () => {
+                        self.loader.hide();
+                        self.isProcessing = false;
+                    }
+                });
+            },
+        },
+    };
 </script>
