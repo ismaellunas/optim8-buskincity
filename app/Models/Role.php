64938b2d5e2a1ba47ecbffebadef2b3f164c7f96
Bayu <boedy@sdbagency.com>
2021-10-05 12:20:56 +0700

    Use HasFactory Trait in Role Model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
 {
+    use HasFactory;
+
     // Scopes:
     public function scopeWithoutSuperAdmin($query)
     {
