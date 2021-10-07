57c737acf89be1ef84a4a648f71ce50b22e434fa
Bayu <boedy@sdbagency.com>
2021-10-07 18:57:21 +0700

    Add getBuilderRecords() and use it into getRecords() and getNoSuperAdministratorRecords()

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -5,15 +5,21 @@ namespace App\Services;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\AbstractPaginator;
 use Illuminate\Support\Facades\Hash;
 
 class UserService
 {
-    public function getRecords(
-        string $term = null,
-        int $perPage = 15
-    ): LengthAwarePaginator {
+    private function getBuilderRecords(
+        string $term = null
+    ): Builder {
         return User::orderBy('id', 'DESC')
+            ->with([
+                'roles' => function ($query) {
+                    $query->select('id', 'name');
+                }
+            ])
             ->when($term, function ($query) use ($term) {
                 $query->search($term);
             })
@@ -21,7 +27,27 @@ class UserService
                 'id',
                 'name',
                 'email',
-            ])
+            ]);
+    }
+
+    public function getRecords(
+        string $term = null,
+        int $perPage = 15
+    ): LengthAwarePaginator {
+        return $this
+            ->getBuilderRecords($term)
+            ->paginate($perPage);
+    }
+
+    public function getNoSuperAdministratorRecords(
+        string $term = null,
+        int $perPage = 15
+    ) {
+        return $this
+            ->getBuilderRecords($term)
+            ->whereDoesntHave('roles', function ($query) {
+                $query->where('name', config('permission.super_admin_role'));
+            })
             ->paginate($perPage);
     }
 
