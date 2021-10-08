1bdd76b28d72a0037825b25226b755c77bf8c3a7
Bayu <boedy@sdbagency.com>
2021-10-08 12:42:48 +0700

    Add AsPageTranslationDataCollection Cast

---
+++ app/Casts/AsPageTranslationDataCollection.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\Castable;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Support\Collection;
+
+class AsPageTranslationDataCollection implements Castable
+{
+    /**
+     * Get the caster class to use when casting from / to this cast target.
+     *
+     * @param  array  $arguments
+     * @return object|string
+     */
+    public static function castUsing(array $arguments)
+    {
+        return new class implements CastsAttributes
+        {
+            public function get($model, $key, $value, $attributes)
+            {
+                $result = isset($attributes[$key]) ? new Collection(json_decode($attributes[$key], true)) : null;
+
+                if (! is_null($result)) {
+                    if (empty($result->get('entities'))) {
+                        $result->put('entities', new \stdClass());
+                    }
+                }
+
+                return $result;
+            }
+
+            public function set($model, $key, $value, $attributes)
+            {
+                return [$key => json_encode($value)];
+            }
+        };
+    }
+}
