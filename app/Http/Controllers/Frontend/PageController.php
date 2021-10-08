674e5ac636d7a513c6ad1b39029f5cedcbffdb15
Bayu <boedy@sdbagency.com>
2021-10-08 14:52:39 +0700

    Add Frontend/PageController

---
+++ app/Http/Controllers/Frontend/PageController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Media;
+use App\Models\PageTranslation;
+use App\Services\TranslationService;
+use Illuminate\Support\Collection;
+use Inertia\Inertia;
+
+class PageController extends Controller
+{
+    private $baseComponentName = 'Page/Frontend';
+    private $baseRouteName = 'frontend.pages';
+
+    private function redirectToPageLocaleOr404(
+        PageTranslation $pageTranslation,
+        string $locale
+    ) {
+        $page = $pageTranslation->page;
+
+        if ($page->hasTranslation($locale)) {
+
+            $pageTranslation = $page->translate($locale);
+
+            return redirect()->route($this->baseRouteName.'.show', [
+                'locale' => $locale,
+                'page_translation' => $pageTranslation->slug
+            ]);
+
+        } else {
+            return redirect()->route('status-code.404');
+        }
+    }
+
+    private function getPageImages(
+        PageTranslation $pageTranslation,
+        string $locale
+    ): Collection {
+        $images = collect([]);
+
+        $locales = array_unique([
+            TranslationService::getDefaultLocale(),
+            $locale,
+        ]);
+
+        if (!empty($pageTranslation->data['media'])) {
+            $mediaIds = collect($pageTranslation->data['media'])->pluck('id');
+
+            $images = Media::whereIn('id', $mediaIds)
+                ->image()
+                ->with([
+                    'translations' => function ($q) use ($locales) {
+                        $q->select(['id', 'locale', 'alt', 'media_id']);
+                        $q->whereIn('locale', $locales);
+                    },
+                ])
+                ->get(['id', 'file_url']);
+        }
+
+        return $images;
+    }
+
+    public function show(string $locale, PageTranslation $pageTranslation)
+    {
+        if ($pageTranslation->locale != $locale) {
+
+            return $this->redirectToPageLocaleOr404($pageTranslation, $locale);
+
+        } else {
+
+            return Inertia::render($this->baseComponentName.'/Show', [
+                'currentLanguage' => TranslationService::currentLanguage(),
+                'images' => $this->getPageImages($pageTranslation, $locale),
+                'page' => $pageTranslation,
+            ]);
+        }
+    }
+}
