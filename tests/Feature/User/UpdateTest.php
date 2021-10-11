7db16af6e780df409d806d3a34e0b44adf0ce8ee
Bayu <boedy@sdbagency.com>
2021-10-11 17:06:49 +0700

    Order imported classes in User/UpdateTest

--- tests/Feature/User/UpdateTest.php
+++ tests/Feature/User/UpdateTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature\User;
 
-use App\Models\User;
 use App\Models\Role;
+use App\Models\User;
 use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
