492a8bcd1c6893cc327de8bf12be6d4929e130a8
DESKTOP-DTNATDG\Ryzen <naufal@gruneasia.info>
2021-10-13 02:12:28 +0700

    [Feature] Add carousel structure to configs and index js

--- resources/js/ComponentStructures/configs.js
+++ resources/js/ComponentStructures/configs.js
@@ -1,5 +1,6 @@
 import { config as card } from './card';
 import { config as cardText } from './card-text';
+import { config as carousel } from './carousel';
 import { config as heading } from './heading';
 import { config as image } from './image';
 import { config as text } from './text';
@@ -7,6 +8,7 @@ import { config as text } from './text';
 export default {
     card: card,
     cardText: cardText,
+    carousel: carousel,
     heading: heading,
     image: image,
     text: text,
