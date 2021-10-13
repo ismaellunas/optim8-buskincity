e17f052454b137d61d25ea4a969455ac44676439
Bayu <boedy@sdbagency.com>
2021-10-13 19:30:34 +0700

    Add file_extensions into constants in config

--- config/constants.php
+++ config/constants.php
@@ -2,4 +2,33 @@
 
 return [
     'one_megabyte' => 1024,
+    'file_extensions' => [
+        'image' => [
+            'jpeg',
+            'jpg',
+            'png',
+            'gif',
+        ],
+        'video' => [
+            'mp4',
+            'mov',
+            'avi',
+            'mpg',
+            'ogv',
+            '3gp',
+        ],
+        'document' => [
+            'pdf',
+            'doc',
+            'docx',
+        ],
+        'spreadsheet' => [
+            'ppt',
+            'pptx',
+        ],
+        'presentation' => [
+            'xls',
+            'xlsx',
+        ],
+    ],
 ];
