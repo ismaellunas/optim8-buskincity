d66b93abb7963b0cec24a2c2204fa6afd1c6dd20
DESKTOP-DTNATDG\Ryzen <naufal@gruneasia.info>
2021-10-15 13:50:56 +0700

    [Feature] Add bulma class on carousel

--- resources/js/Blocks/Contents/Carousel/CarouselMain.vue
+++ resources/js/Blocks/Contents/Carousel/CarouselMain.vue
@@ -1,31 +1,40 @@
 <template>
-    <figure class="carousel image" :class="config.carousel?.ratio">
+    <figure
+        class="carousel is-relative is-clipped has-background-grey-lighter image"
+        :class="config.carousel?.ratio">
         <slot></slot>
 
-        <span
-            class="icon carousel-button has-text-info is-size-3 ml-5 prev"
-            @click="$emit('prev')"
-        >
-            <i class="fa fa-chevron-left"></i>
-        </span>
-        <span
-            class="icon carousel-button has-text-info is-size-3 mr-5 next"
-            @click="$emit('next')"
-        >
-            <i class="fa fa-chevron-right"></i>
-        </span>
-
-        <div class="carousel-indicator">
-            <template
-                v-for="(total, index) in totalImage"
-                :key="index"
-            >
-                <div
-                    class="carousel-indicator-item"
-                    :class="{ active: visibleSlide === index }"
+        <div class="level is-overlay">
+            <div class="level-item is-justify-content-start">
+                <span
+                    class="icon carousel-button has-text-info is-size-3 ml-5"
+                    @click="$emit('prev')"
                 >
+                    <i class="fa fa-chevron-left"></i>
+                </span>
+            </div>
+            <div class="level-item is-align-items-end mb-4" style="height: 100%">
+                <div class="carousel-indicator">
+                    <template
+                        v-for="(total, index) in totalImage"
+                        :key="index"
+                    >
+                        <div
+                            class="carousel-indicator-item has-background-info"
+                            :class="{ active: visibleSlide === index }"
+                        >
+                        </div>
+                    </template>
                 </div>
-            </template>
+            </div>
+            <div class="level-item is-justify-content-end">
+                <span
+                    class="icon carousel-button has-text-info is-size-3 mr-5"
+                    @click="$emit('next')"
+                >
+                    <i class="fa fa-chevron-right"></i>
+                </span>
+            </div>
         </div>
     </figure>
 </template>
