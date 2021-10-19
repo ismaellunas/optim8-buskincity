8673afb6328277f9380b63a7a035d9661d014a0b
DESKTOP-DTNATDG\Ryzen <naufal@gruneasia.info>
2021-10-19 14:16:16 +0700

    [Feature] Make Card Text for Frontend Component

---
+++ resources/js/Blocks/Frontend/Contents/CardText.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="card sdb-card-text">
+            <div class="card-content">
+                <div class="content" :class="cardContentClass">
+                    <div v-html="entity.content.cardContent.content.html"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { useModelWrapper } from '@/Libs/utils'
+
+    export default {
+        props: {
+            id: String,
+            modelValue: {type: Object, required: true},
+        },
+
+        setup(props, { emit }) {
+            return {
+                config: props.modelValue.config,
+                entity: useModelWrapper(props, emit),
+            };
+        },
+
+        computed: {
+            cardContentClass() {
+                let classes = [];
+                classes.push(this.config.content?.size ?? 'is-normal');
+                return classes;
+            },
+        }
+    }
+</script>
