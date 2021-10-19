022cea48ea663e5783da6883aeb04b610b7e9e81
Bayu <boedy@sdbagency.com>
2021-10-19 15:03:54 +0700

    Move admin routes to routes/admin and register it on RouteServiceProvider

---
+++ routes/admin.php
@@ -0,0 +1,75 @@
+<?php
+
+use App\Http\Controllers\{
+    CategoryController,
+    MediaController,
+    PageController,
+    PermissionController,
+    PostController,
+    RoleController,
+    UserController,
+    UserRoleController
+};
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::resource('/pages', PageController::class)
+        ->except(['show']);
+
+    Route::resource('/media', MediaController::class)
+        ->except(['edit', 'show']);
+
+    Route::post('/media/update-image/{medium}', [MediaController::class, 'updateImage'])
+        ->name('media.update-image');
+    Route::post('/media/save-as-image/{medium}', [MediaController::class, 'saveAsImage'])
+        ->name('media.save-as-image');
+    Route::get('/media-list/image', [MediaController::class, 'listImages'])
+        ->name('media.list.image');
+
+    Route::resource('/categories', CategoryController::class)
+        ->except(['show']);
+
+    Route::resource('/posts', PostController::class)
+        ->except(['show']);
+
+    Route::resource('/users', UserController::class)
+        ->except(['show']);
+    Route::put('/users/{user}/password', [UserController::class, 'updatePassword'])
+        ->name('users.password');
+
+    Route::resource('/roles', RoleController::class);
+
+    Route::resource('/permissions', PermissionController::class);
+
+    Route::resource('/user-roles', UserRoleController::class);
+
+    Route::get('dashboard', function () {
+        return Inertia::render('Auth/Admin/Dashboard');
+    });
+});
+
+Route::name('api.')->prefix('api')->middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::post('/media', [MediaController::class, 'apiStore'])
+        ->name('media.store');
+});
+
+Route::middleware(['guest:'.config('fortify.guard')])->group(function () {
+    $limiter = config('fortify.limiters.login');
+
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+        ->middleware(array_filter([
+            $limiter ? 'throttle:'.$limiter : null,
+        ]))
+        ->name('login.attempt');
+});
