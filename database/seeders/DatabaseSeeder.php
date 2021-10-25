aff6515ec877f92b675801a228b4fb0504e3de89
Bayu <boedy@sdbagency.com>
2021-10-25 19:52:24 +0700

    Populate color setting into setting table from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use App\Models\Permission;
 use App\Models\Post;
 use App\Models\Role;
 use App\Models\User;
+use App\Models\Setting;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
@@ -70,5 +71,48 @@ class DatabaseSeeder extends Seeder
         Page::factory()
             ->hasTranslations(1)
             ->create();
+
+        $settings = [
+            [
+                "key" => "color_primary",
+                "display_name" => "Primary Color",
+                "value" => "#00d1b2",
+                "group" => "theme_color"
+            ],
+            [
+                "key" => "color_link",
+                "display_name" => "Link Color",
+                "value" => "#485fc7",
+                "group" => "theme_color"
+            ],
+            [
+                "key" => "color_info",
+                "display_name" => "Info Color",
+                "value" => "#3e8ed0",
+                "group" => "theme_color"
+            ],
+            [
+                "key" => "color_success",
+                "display_name" => "Success Color",
+                "value" => "#48c78e",
+                "group" => "theme_color"
+            ],
+            [
+                "key" => "color_warning",
+                "display_name" => "Warning Color",
+                "value" => "#ffe08a",
+                "group" => "theme_color"
+            ],
+            [
+                "key" => "color_danger",
+                "display_name" => "Danger Color",
+                "value" => "#f14668",
+                "group" => "theme_color"
+            ]
+        ];
+
+        foreach ($settings as $setting) {
+            Setting::factory()->create($setting);
+        }
     }
 }
