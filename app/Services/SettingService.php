ea8e7230340a0d7fea1dfa087721ec32b6d3e3cf
Bayu <boedy@sdbagency.com>
2021-10-27 21:17:03 +0700

    Add getFrontendCssUrl() method into SettingService

--- app/Services/SettingService.php
+++ app/Services/SettingService.php
@@ -2,6 +2,14 @@
 
 namespace App\Services;
 
+use App\Models\Setting;
+
 class SettingService
 {
+    public static function getFrontendCssUrl(): string
+    {
+        $urlCss = Setting::where('key', 'url_css')->first(['key', 'value']);
+
+        return $urlCss->value ?? mix('css/app.css')->toHtml();
+    }
 }
