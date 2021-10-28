e5ab737c692a6cbf83e74565c344ddd109a200bd
Bayu <boedy@sdbagency.com>
2021-10-28 15:10:07 +0700

    Add generateVariableSass() method into SettingService

--- app/Services/SettingService.php
+++ app/Services/SettingService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Setting;
+use Illuminate\Support\Facades\Storage;
 
 class SettingService
 {
@@ -11,5 +12,25 @@ class SettingService
         $urlCss = Setting::where('key', 'url_css')->first(['key', 'value']);
 
         return $urlCss->value ?? mix('css/app.css')->toHtml();
+    }
+
+    public function generateVariablesSass()
+    {
+        $colors = Setting::where('group', 'theme_color')
+            ->get(['key', 'value'])
+            ->pluck('value', 'key')
+            ->all();
+
+        $variablesSass = view('theme_options.colors_sass', array_merge(
+            config('constants.theme_colors'),
+            $colors
+        ));
+
+        $disk = Storage::build([
+            'driver' => 'local',
+            'root' => storage_path('theme/sass'),
+        ]);
+
+        $disk->put('sdb_variables.sass', $variablesSass);
     }
 }
