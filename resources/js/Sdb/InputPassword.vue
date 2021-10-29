093696bf5265a38417f279d843f69bdc079244cb
DESKTOP-DTNATDG\Ryzen <naufal@gruneasia.info>
2021-10-29 13:59:47 +0700

    [Feature] Add component input password

---
+++ resources/js/Sdb/InputPassword.vue
@@ -0,0 +1,30 @@
+<template>
+    <input
+        ref="input"
+        class="input"
+        :type="type"
+        :class="{'is-danger' : hasError}"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    />
+</template>
+
+<script>
+    export default {
+        name: 'SdbInputPassword',
+
+        props: {
+            hasError: {type: Boolean, default: false},
+            modelValue: {},
+            type: {type: String, default: 'password'},
+        },
+
+        emits: ['update:modelValue'],
+
+        methods: {
+            focus() {
+                this.$refs.input.focus()
+            }
+        },
+    }
+</script>
