bcf1eb3fd1d01dd27e0ad589c64f4f0c8b60d7a4
Bayu <boedy@sdbagency.com>
2021-10-29 12:56:41 +0700

    Add ThemeFontSizeEdit and handler edit() and update()

--- app/Http/Controllers/ThemeFontSizeController.php
+++ app/Http/Controllers/ThemeFontSizeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
 use App\Services\SettingService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Inertia\Inertia;
 
 class ThemeFontSizeController extends CrudController
 {
@@ -14,5 +18,47 @@ class ThemeFontSizeController extends CrudController
     public function __construct(SettingService $settingService)
     {
         $this->settingService = $settingService;
+    }
+
+    public function edit()
+    {
+        $defaultFontSizes = config('constants.theme_font_sizes');
+
+        return Inertia::render(
+            'ThemeFontSize/Edit',
+            $this->getData([
+                'defaultFontSizes' => $defaultFontSizes,
+                'fontSizes' => $this->settingService->getFontSizes(),
+            ])
+        );
+    }
+
+    public function update(Request $request)
+    {
+        $colors = $request->all();
+
+        foreach ($colors as $key => $color) {
+            $setting = Setting::firstOrNew(['key' => $key]);
+            $setting->value = $color;
+            $setting->save();
+        }
+
+        $this->settingService->generateVariablesSass();
+
+        $this->settingService->generateThemeCss();
+
+        $asset = $this->settingService->uploadThemeCssToCloudStorage(
+            !App::environment('production')
+            ? config('app.env')
+            : null
+        );
+
+        $this->settingService->saveCssUrl($asset->fileUrl);
+
+        $this->settingService->clearStorageTheme();
+
+        $this->generateFlashMessage('Font Sizes updated successfully!');
+
+        return redirect()->route($this->baseRouteName.'.edit');
     }
 }
