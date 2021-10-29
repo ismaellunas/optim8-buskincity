07713c8b9137888c7683c41ec00651ded8ab19df
Bayu <boedy@sdbagency.com>
2021-10-29 12:41:01 +0700

    Add fontSizes default data in SettingSeeder

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -62,5 +62,68 @@ class SettingSeeder extends Seeder
         foreach ($settings as $setting) {
             Setting::factory()->create($setting);
         }
+
+        $fontSizes = [
+            [
+                "key" => "font_size_heading_1",
+                "display_name" => "Heading 1",
+                "value" => 2,
+                "group" => "font_size",
+                "order" => "1"
+            ],
+            [
+                "key" => "font_size_heading_2",
+                "display_name" => "Heading 2",
+                "value" => 1.75,
+                "group" => "font_size",
+                "order" => "2"
+            ],
+            [
+                "key" => "font_size_heading_3",
+                "display_name" => "Heading 3",
+                "value" => 1.5,
+                "group" => "font_size",
+                "order" => "3"
+            ],
+            [
+                "key" => "font_size_heading_4",
+                "display_name" => "Heading 4",
+                "value" => 1.25,
+                "group" => "font_size",
+                "order" => "4"
+            ],
+            [
+                "key" => "font_size_heading_5",
+                "display_name" => "Heading 5",
+                "value" => 1.125,
+                "group" => "font_size",
+                "order" => "5"
+            ],
+            [
+                "key" => "font_size_heading_6",
+                "display_name" => "Heading 6",
+                "value" => 1,
+                "group" => "font_size",
+                "order" => "6"
+            ],
+            [
+                "key" => "font_size_text",
+                "display_name" => "Text",
+                "value" => null,
+                "group" => "font_size",
+                "order" => "7"
+            ],
+            [
+                "key" => "font_size_small",
+                "display_name" => "Small",
+                "value" => null,
+                "group" => "font_size",
+                "order" => "8"
+            ]
+        ];
+
+        foreach ($fontSizes as $fontSize) {
+            Setting::factory()->create($fontSize);
+        }
     }
 }
