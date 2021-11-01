2881b29ffb124d37086652b2f91fd22ffe9d1209
Bayu <boedy.bayu@gmail.com>
2021-11-01 14:00:58 +0700

    Add ThemeFontSizeRequest and add FlashNotifiable Trait

---
+++ app/Http/Requests/ThemeFontSizeRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Services\SettingService;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ThemeFontSizeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            '*' => 'numeric',
+        ];
+    }
+
+    public function attributes()
+    {
+        $attributes = [];
+
+        $fontSizes = (new SettingService())->getFontSizes();
+
+        foreach ($fontSizes as $key => $fontSize) {
+            $attributes[$key] = $fontSize['display_name'];
+        }
+
+        return $attributes;
+    }
+}
