62ecb0aa74f2a2d256c29af3a0c690ceb4e7b7b7
DESKTOP-DTNATDG\Ryzen <naufal@gruneasia.info>
2021-11-02 15:57:47 +0700

    [Feature] Add navbar layout for frontend

--- resources/js/Layouts/PageLayout.vue
+++ resources/js/Layouts/PageLayout.vue
@@ -7,111 +7,14 @@
             >
         </Head>
 
-    <nav class="navbar is-primary">
-        <div class="navbar-brand">
-            <sdb-link
-                class="navbar-item"
-                href="/"
-            >
-                <img
-                    :src="logoUrl" 
-                    alt=""
-                    height="28"
-                >
-            </sdb-link>
-
-            <div
-                class="navbar-burger burger"
-                data-target="navbarExampleTransparentExample"
-            >
-                <span />
-                <span />
-                <span />
-            </div>
-        </div>
-
-        <div
-            id="navbarExampleTransparentExample"
-            class="navbar-menu"
+        <component
+            :is="navbarLayoutName"
+            :available-languages="availableLanguages"
+            :current-language="currentLanguage"
+            :logo-url="logoUrl"
+            :menus="menus"
         >
-            <div class="navbar-start">
-                <sdb-link
-                    href="/"
-                    class="navbar-item"
-                >
-                    Home
-                </sdb-link>
-
-                <template
-                    v-for="(menu, index) in menus"
-                    :key="index"
-                >
-                    <template v-if="menu.children.length > 0">
-                        <div class="navbar-item has-dropdown is-hoverable">
-                            <a
-                                class="navbar-link"
-                            >
-                                {{ menu.title }}
-                            </a>
-                            <div class="navbar-dropdown">
-                                <template
-                                    v-for="childMenu in menu.children"
-                                >
-                                    <sdb-link
-                                        class="navbar-item"
-                                        :href="childMenu.link"
-                                    >
-                                        {{ childMenu.title }}
-                                    </sdb-link>
-                                </template>
-                            </div>
-                        </div>
-                    </template>
-                    <template v-else>
-                        <sdb-link
-                            class="navbar-item"
-                            :href="menu.link"
-                        >
-                            {{ menu.title }}
-                        </sdb-link>
-                    </template>
-                </template>
-            </div>
-
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="field">
-                        <div class="control">
-                            <input
-                                class="input"
-                                type="text"
-                                placeholder="Search . . ."
-                            >
-                        </div>
-                    </div>
-                </div>
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <span class="navbar-link">{{ currentLanguage.toUpperCase() }}</span>
-                    <div class="navbar-dropdown is-boxed">
-                        <sdb-link
-                            v-for="language in availableLanguages"
-                            :key="language.id"
-                            class="navbar-item"
-                            :href="route('language.change', [language.id])"
-                        >
-                            {{ language.id.toUpperCase() }}
-                        </sdb-link>
-                    </div>
-                </div>
-                <sdb-link
-                    :href="route('login')"
-                    class="navbar-item pr-5"
-                >
-                    Login
-                </sdb-link>
-            </div>
-        </div>
-    </nav>
+        </component>
 
         <slot />
     </div>
@@ -119,7 +22,9 @@
 </template>
 
 <script>
-    import SdbLink from '@/Sdb/Link';
+    import SdbNavbarLayoutOne from '@/Sdb/Navbar/NavbarLayoutOne';
+    import SdbNavbarLayoutTwo from '@/Sdb/Navbar/NavbarLayoutTwo';
+    import SdbNavbarLayoutThree from '@/Sdb/Navbar/NavbarLayoutThree';
     import { Head, usePage } from '@inertiajs/inertia-vue3';
 
     export default {
@@ -127,7 +32,9 @@
 
         components: {
             Head,
-            SdbLink,
+            SdbNavbarLayoutOne,
+            SdbNavbarLayoutTwo,
+            SdbNavbarLayoutThree,
         },
 
         props: [
@@ -152,6 +59,27 @@
 
             logoUrl() {
                 return this.menuSettings.header_logo_url.value ?? "https://dummyimage.com/48x28/e5e5e5/000000.png&text=B+752";
+            },
+
+            navbarLayoutName() {
+                const layout = parseInt(this.menuSettings.header_layout.value);
+                switch (layout) {
+                    case 1:
+                        return "SdbNavbarLayoutOne";
+                        break;
+
+                    case 2:
+                        return "SdbNavbarLayoutTwo";
+                        break;
+
+                    case 3:
+                        return "SdbNavbarLayoutThree";
+                        break;
+
+                    default:
+                        return "SdbNavbarLayoutOne";
+                        break;
+                }
             },
         }
     };
