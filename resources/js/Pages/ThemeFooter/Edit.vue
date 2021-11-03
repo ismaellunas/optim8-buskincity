f348dbb3aee29ffee81751db1d8e7893faa009d2
DESKTOP-DTNATDG\Ryzen <naufal@gruneasia.info>
2021-11-03 16:15:28 +0700

    [Feature] Add vue file for handle theme option of footer

---
+++ resources/js/Pages/ThemeFooter/Edit.vue
@@ -0,0 +1,111 @@
+<template>
+    <app-layout>
+        <template #header>
+            {{ title }}
+        </template>
+
+        <div class="box mb-6">
+            <sdb-tab>
+                <ul>
+                    <sdb-tab-list
+                        v-for="(tab, index) in tabs"
+                        :key="index"
+                        :is-active="isTabActive(index)"
+                    >
+                        <a @click.prevent="setActiveTab(index)">
+                            {{ tab.title }}
+                        </a>
+                    </sdb-tab-list>
+                </ul>
+
+                <sdb-button
+                    type="button"
+                    class="is-primary"
+                    @click="onSave(activeTab)"
+                >
+                    <span>Save</span>
+                </sdb-button>
+            </sdb-tab>
+
+            <footer-layout
+                v-if="isLayout"
+                ref="footerLayout"
+                :setting="settings.footer_layout"
+            />
+
+            <footer-navigation
+                v-if="isNavigation"
+                ref="footerNavigation"
+            />
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout';
+    import FooterLayout from './FooterLayout';
+    import FooterNavigation from './FooterNavigation';
+    import MixinHasTab from '@/Mixins/HasTab';
+    import SdbButton from '@/Sdb/Button';
+    import SdbTab from '@/Sdb/Tab';
+    import SdbTabList from '@/Sdb/TabList';
+
+    export default {
+        components: {
+            AppLayout,
+            FooterLayout,
+            FooterNavigation,
+            SdbButton,
+            SdbTab,
+            SdbTabList,
+        },
+        mixins: [
+            MixinHasTab,
+        ],
+        props: {
+            baseRouteName: {
+                type: String,
+                required: true
+            },
+            settings: {
+                type: Object,
+                required: true
+            },
+            title: {
+                type: String,
+                default: "-"
+            },
+        },
+        setup() {
+            return {
+                tabs: {
+                    layout: {title: 'Layout'},
+                    navigation: {title: 'Navigation'},
+                },
+            }
+        },
+        data() {
+            return {
+                activeTab: 'layout',
+            };
+        },
+        computed: {
+            isLayout() {
+                return this.activeTab == 'layout';
+            },
+            isNavigation() {
+                return this.activeTab == 'navigation';
+            },
+        },
+        methods: {
+            onTabSelected(tab) {
+                this.activeTab = tab;
+            },
+            onSave(tab) {
+                if (tab == 'layout') {
+                    this.$refs.footerLayout.saveLayout();
+                }
+            },
+        }
+    }
+</script>
---
+++ resources/js/Pages/ThemeFooter/FooterNavigation.vue
@@ -0,0 +1,46 @@
+<template>
+    <main>
+        <div class="columns">
+            <div class="column">
+                <div class="is-pulled-left">
+                    <b>Menu Navigation</b><br>
+                    Last Saved: -
+                </div>
+            </div>
+        </div>
+        <div class="columns">
+            <div class="column">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis iste accusamus cumque deserunt! Labore, placeat temporibus exercitationem quod recusandae perferendis velit suscipit vero, officia magnam ab minus, odit cupiditate porro?</p>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    import { success as successAlert  } from '@/Libs/alert';
+    import { usePage } from '@inertiajs/inertia-vue3';
+
+    export default {
+        name: 'FooterNavigation',
+
+        props: {
+            //
+        },
+
+        setup() {
+            return {
+                baseRouteName: usePage().props.value.baseRouteName ?? null,
+            };
+        },
+
+        data() {
+            return {
+                //
+            };
+        },
+
+        methods: {
+            //
+        },
+    }
+</script>
