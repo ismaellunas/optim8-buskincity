7449cda57a645ae8dbe6ad8542434b38f3bdcdea
DESKTOP-DTNATDG\Ryzen <naufal@gruneasia.info>
2021-11-04 16:02:01 +0700

    [Feature] Remove some function on menu interface

--- app/Contracts/MenuInterface.php
+++ app/Contracts/MenuInterface.php
@@ -3,7 +3,5 @@
 namespace App\Contracts;
 
 interface MenuInterface {
-    public function getTranslation(): object;
-    public function getTitle(): string;
     public function getUrl(): string;
 }
