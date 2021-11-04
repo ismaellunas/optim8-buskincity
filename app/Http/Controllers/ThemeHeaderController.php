033a036a89887f384ee54778ddba040da13d565e
DESKTOP-DTNATDG\Ryzen <naufal@gruneasia.info>
2021-11-04 11:26:43 +0700

    [Feature] Add function to handle navigation feature

--- app/Http/Controllers/ThemeHeaderController.php
+++ app/Http/Controllers/ThemeHeaderController.php
@@ -2,39 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MenuItemRequest;
 use App\Http\Requests\ThemeHeaderLogoRequest as LogoRequest;
-use App\Models\Setting;
-use App\Services\SettingService;
+use App\Models\{
+    Menu,
+    MenuItem,
+    Setting,
+};
+use App\Services\{
+    MenuService,
+    SettingService,
+};
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ThemeHeaderController extends CrudController
 {
+    private $menuService;
     private $settingService;
+    private $modelMenu = Menu::class;
+    private $modelMenuItem = MenuItem::class;
+
     protected $baseRouteName = 'admin.theme.header';
-    protected $componentName = 'ThemeHeader/Header/';
+    protected $componentName = 'ThemeHeader/';
     protected $title = "Header";
 
-    public function __construct(SettingService $settingService)
-    {
+    public function __construct(
+        MenuService $menuService,
+        SettingService $settingService
+    ) {
+        $this->menuService = $menuService;
         $this->settingService = $settingService;
     }
 
-    public function index()
+    public function edit()
     {
         return Inertia::render(
-            $this->componentName.'Index',
+            $this->componentName.'Edit',
             $this->getData([
-                'headerLayoutLastSaved' => $this->settingService
-                    ->getHeaderLayoutLastSaved(),
-                'headerLogoUrlLastSaved' => $this->settingService
-                    ->getHeaderLogoUrlLastSaved(),
-                'settings' => $this->settingService
-                    ->getHeader(),
+                'categories' => $this->menuService->getRecordCategories(),
+                'menu' => $this->modelMenu::header()->first(),
+                'menuItemLastSaved' => $this->menuService->getMenuItemLastSaved("header"),
+                'menuItems' => $this->menuService->generateMenus(),
+                'pages' => $this->menuService->getRecordPages(),
+                'posts' => $this->menuService->getRecordPosts(),
+                'settings' => $this->settingService->getHeader(),
+                'types' => $this->modelMenuItem::TYPES,
             ]),
         );
     }
 
+    // Header handle
     public function updateLayout(Request $request)
     {
         $layout = $request->layout;
@@ -45,7 +63,7 @@ class ThemeHeaderController extends CrudController
 
         $this->generateFlashMessage('Header layout updated successfully!');
 
-        return redirect()->route($this->baseRouteName.'.index');
+        return redirect()->route($this->baseRouteName.'.edit');
     }
 
     public function updateLogo(LogoRequest $request)
@@ -61,6 +79,105 @@ class ThemeHeaderController extends CrudController
 
         $this->generateFlashMessage('Header logo upload successfully!');
 
-        return redirect()->route($this->baseRouteName.'.index');
+        return redirect()->route($this->baseRouteName.'.edit');
+    }
+
+    // Navigation handle
+    public function store(MenuItemRequest $request)
+    {
+        $inputs = $this->generateCustomValues($request->all());
+
+        $menuItem = new MenuItem();
+        $menuItem->saveFromInputs($inputs);
+
+        $this->generateFlashMessage('Menu item created successfully!');
+
+        return redirect()->route($this->baseRouteName.'.edit');
+    }
+
+    public function update(
+        MenuItemRequest $request,
+        MenuItem $menuItem
+    ) {
+        $inputs = $this->generateCustomValues($request->all());
+
+        $menuItem->saveFromInputs($inputs);
+
+        $menuItem->syncTranslations(array_keys($inputs));
+
+        $this->generateFlashMessage('Menu item updated successfully!');
+
+        return redirect()->route($this->baseRouteName.'.edit');
+    }
+
+    public function updateFormat(Request $request)
+    {
+        $menuItems = new $this->modelMenuItem;
+
+        $menuItems->updateFormatMenuItems($request->all());
+
+        $this->generateFlashMessage('Menu navigation successfully Saved!');
+
+        return redirect()->route($this->baseRouteName.'.edit');
+    }
+
+    public function destroy(MenuItem $menuItem)
+    {
+        $this->updateParentId($menuItem);
+
+        $menuItem->delete();
+
+        $this->generateFlashMessage('Menu navigation deleted successfully!');
+
+        return redirect()->route($this->baseRouteName.'.edit');
+    }
+
+    private function updateParentId($menuItem)
+    {
+        $childs = $this->modelMenuItem::where('parent_id', $menuItem->id)->get();
+
+        foreach ($childs as $child) {
+            $child->parent_id = $menuItem->parent_id;
+            $child->save();
+        }
+    }
+
+    private function generateCustomValues($inputs)
+    {
+        $lastMenuItem = $this->modelMenuItem::orderBy('order', 'DESC')
+            ->where('menu_id', $inputs['menu_id'])
+            ->first();
+
+        if ($lastMenuItem && $inputs['id'] === null) {
+            $inputs['order'] = $lastMenuItem->order + 1;
+        } else if (!$lastMenuItem && $inputs['id'] === null) {
+            $inputs['order'] = 1;
+        }
+
+        if ($inputs['type'] == $this->modelMenuItem::TYPE_URL) {
+            $inputs['page_id'] = null;
+            $inputs['post_id'] = null;
+            $inputs['category_id'] = null;
+        }
+
+        if ($inputs['type'] == $this->modelMenuItem::TYPE_PAGE) {
+            $inputs['url'] = null;
+            $inputs['post_id'] = null;
+            $inputs['category_id'] = null;
+        }
+
+        if ($inputs['type'] == $this->modelMenuItem::TYPE_POST) {
+            $inputs['url'] = null;
+            $inputs['page_id'] = null;
+            $inputs['category_id'] = null;
+        }
+
+        if ($inputs['type'] == $this->modelMenuItem::TYPE_CATEGORY) {
+            $inputs['url'] = null;
+            $inputs['page_id'] = null;
+            $inputs['post_id'] = null;
+        }
+
+        return $inputs;
     }
 }
