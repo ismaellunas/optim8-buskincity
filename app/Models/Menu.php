16390295972e742ded414abcb15676f41e487d3b
Naufal Rivaldi <naufal@sdbagency.com>
2021-11-10 16:53:59 +0700

    Add generate menu items on menu model

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -11,6 +11,7 @@ class Menu extends Model
 
     protected $fillable = [
         'type',
+        'locale',
     ];
 
     const TYPE_HEADER = 1;
@@ -20,6 +21,62 @@ class Menu extends Model
     {
         $this->fill($inputs);
         $this->save();
+    }
+
+    public static function generateMenuItems(
+        string $locale = 'en',
+        int $type = self::TYPE_HEADER
+    ): array {
+        $menu = self::getMenu($locale, $type);
+
+        return $menu ? self::createArrayMenuItems($menu) : [];
+    }
+
+    private static function getMenu(
+        string $locale,
+        int $type
+    ) {
+        return self::with([
+                'menuItems' => function ($query) {
+                    $query->orderBy('order', 'ASC')
+                        ->orderBy('parent_id', 'ASC');
+                }
+            ])
+            ->where('locale', $locale)
+            ->where(function ($query) use ($type) {
+                if ($type == self::TYPE_HEADER) {
+                    $query->header();
+                } else {
+                    $query->footer();
+                }
+            })
+            ->first();
+    }
+
+    private static function createArrayMenuItems(
+        object $menu,
+        ?int $parentId = null
+    ): array {
+        $menus = [];
+        foreach ($menu->menuItems as $menuItem) {
+            if ($menuItem['parent_id'] == $parentId) {
+                $children = self::createArrayMenuItems($menu, $menuItem['id']);
+
+                if ($children) {
+                    $menuItem['children'] = $children;
+                } else {
+                    $menuItem['children'] = [];
+                }
+
+                $className = "\App\Entities\Menus\\".MenuItem::TYPE_VALUES[$menuItem['type']]."Menu";
+                $typeMenu = new $className($menuItem['id']);
+                $menuItem['link'] = $typeMenu->getUrl();
+
+                $menus[] = $menuItem;
+            }
+        }
+
+        return $menus;
     }
 
     // Scope
