32318f9184d64457dcab37a74f608d11fe9f7dce
Naufal Rivaldi <naufal@sdbagency.com>
2021-11-12 13:11:34 +0700

    change blog/category to category

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::group([
     Route::get('/blog', [PostController::class, 'index'])
         ->name('blog.index');
 
-    Route::get('/blog/category/{id}', [PostCategoryController::class, 'index'])
+    Route::get('/category/{id}', [PostCategoryController::class, 'index'])
         ->name('blog.category.index');
 
     Route::get('blog/{slug}', [PostController::class, 'show'])
