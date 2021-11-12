bd141cc4815c8607fc121dc3ae275ea012f3280e
Bayu <boedy@sdbagency.com>
2021-11-12 12:59:06 +0700

    Add Sdb/NavbarItem vue component

---
+++ resources/js/Sdb/NavbarItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <sdb-link
+        v-if="isInternalLink"
+        class="navbar-item"
+        :href="url"
+    >
+        <slot />
+    </sdb-link>
+    <a
+        v-else
+        class="navbar-item"
+        :href="url"
+    >
+        <slot />
+    </a>
+</template>
+
+<script>
+    import SdbLink from '@/Sdb/Link';
+
+    export default {
+        name: 'SdbNavbarItem',
+
+        components: {
+            SdbLink,
+        },
+
+        props: {
+            isInternalLink: {
+                type: Boolean,
+                default: false,
+            },
+            url: {
+                type: String,
+                required: true
+            },
+        },
+    };
+</script>
