ca3be97d60fa831c43a1cf06cf32f9283bd45d92
Bayu <boedy@sdbagency.com>
2021-11-15 15:16:24 +0700

    Add new MenuItemRequest

---
+++ app/Http/Requests/MenuItemRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\MenuItem;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class MenuItemRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'title' => [
+                'sometimes',
+                'required',
+                'max:255',
+            ],
+            'type' => [
+                'sometimes',
+                'required',
+                'integer',
+                Rule::in(array_keys(MenuItem::TYPE_VALUES)),
+            ],
+            'url' => [
+                'nullable',
+                'url',
+            ],
+            'page_id' => [
+                'nullable',
+                'integer',
+                'exists:pages,id'
+            ],
+            'post_id' => [
+                'nullable',
+                'integer',
+                'exists:posts,id'
+            ],
+            'category_id' => [
+                'nullable',
+                'integer',
+                'exists:categories,id'
+            ],
+            'menu_id' => [
+                'required',
+                'integer',
+                'exists:menus,id'
+            ],
+        ];
+    }
+}
