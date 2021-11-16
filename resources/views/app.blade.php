6ca563a1d987c847dba98f29efc58a57d0645789
Naufal Rivaldi <naufal@sdbagency.com>
2021-11-16 23:20:26 +0700

    Remove script fontawesome on production

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,11 +18,6 @@
         @env ('production')
             <!-- Styles -->
             <link href="https://cdn.jsdelivr.net/npm/vue-loading-overlay@4/dist/vue-loading.css" rel="stylesheet">
-            <!-- Scripts -->
-            <script defer src="https://use.fontawesome.com/releases/v5.15.3/js/solid.js" integrity="sha384-9xA4r2/2cctd+IZZKLvI1hmeHZ5Yp8xXkS6J8inDtdyZCqhEHVcTGmSUCbNED5Ae" crossorigin="anonymous"></script>
-            <script defer src="https://use.fontawesome.com/releases/v5.15.3/js/regular.js" integrity="sha384-bPKzNk+f6IzEi89cU+jf3bwWzJQqo+U1/QYUijuD7XD9WO3MSrrAVVEglIOCo6VD" crossorigin="anonymous"></script>
-            <script defer src="https://use.fontawesome.com/releases/v5.15.3/js/brands.js" integrity="sha384-oEE/PrsvhwsuT1MjC4sgnz39CQ84HoPt8jwH0RLyJDdDOKulN+UEbm9IgJW0aTu5" crossorigin="anonymous"></script>
-            <script defer src="https://use.fontawesome.com/releases/v5.15.3/js/fontawesome.js" integrity="sha384-hD97VKS04Rv8VYShf782apVZOVP6bVH/ubzrWXIIbKOwnD6gsDIcB29K03FL1A9J" crossorigin="anonymous"></script>
         @endenv
 
         @env ('local')
@@ -32,6 +27,7 @@
             <script src="{{ mix('js/local.js') }}" defer></script>
         @endenv
 
+        <script src="https://kit.fontawesome.com/632bc9cc22.js" crossorigin="anonymous"></script>
     </head>
     <body class="font-sans antialiased">
         @inertia
