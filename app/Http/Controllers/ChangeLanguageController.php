30d4c8e4e8b29d4e7f42ae2a6390e590c6402472
Naufal Rivaldi <naufal@sdbagency.com>
2021-11-19 19:43:57 +0700

    Change redirect logic if user change locale

--- app/Http/Controllers/ChangeLanguageController.php
+++ app/Http/Controllers/ChangeLanguageController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Services\TranslationService as TranslationSv;
+use Illuminate\Support\Str;
+use LaravelLocalization;
 
 class ChangeLanguageController extends Controller
 {
@@ -13,24 +15,60 @@ class ChangeLanguageController extends Controller
      */
     public function __invoke($newLocale)
     {
-        TranslationSv::setLanguageAndAppLocale($newLocale);
-
         $url = url()->previous();
+        $defaultLocale = TranslationSv::getDefaultLocale();
+
+        if ($newLocale === $defaultLocale) {
+            $newLocale = "";
+        }
 
         if (!empty($url)) {
+            $url = $this->removeLocaleFromUrl($url);
             $route = app('router')->getRoutes($url)->match(app('request')->create($url));
             $prevRouteName = $route->getName();
 
             if (empty($prevRouteName)) {
-                return redirect('/');
+                return redirect('/'.$newLocale);
             }
 
             $prevParams = $route->parameters;
-            $prevParams['locale'] = $newLocale;
+            $url = $this->appendLocaleToUrl(
+                $newLocale,
+                route($prevRouteName, $prevParams)
+            );
 
-            return redirect()->route($prevRouteName, $prevParams);
-        } else {
-            return redirect('/');
+            return redirect($url);
         }
+
+        return redirect('/'.$newLocale);
+    }
+
+    private function appendLocaleToUrl(
+        ?string $locale,
+        string $url
+    ):string {
+        $segments = explode(config('app.url'), $url);
+
+        if ($locale == "") {
+            return $url;
+        }
+
+        return config('app.url')."/".$locale.$segments[1];
+    }
+
+    private function removeLocaleFromUrl(string $url)
+    {
+        $uriPath = parse_url($url, PHP_URL_PATH);
+        $uriSegments = explode('/', $uriPath);
+        $locales = LaravelLocalization::getSupportedLanguagesKeys();
+
+        if (Str::startsWith($uriSegments[1], $locales)) {
+            $segments = explode(config('app.url'), $url);
+            $segments[1] = Str::replaceFirst('/'.$uriSegments[1], '', $segments[1]);
+
+            $url = config('app.url').$segments[1];
+        }
+
+        return $url;
     }
 }
