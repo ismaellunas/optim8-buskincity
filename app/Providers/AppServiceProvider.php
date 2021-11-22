dedbc4730ff838296b19255230bf79eb77f13c94
Bayu <boedy@sdbagency.com>
2021-11-22 11:18:52 +0700

    Add PageBuilderService and register as singleton in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,13 @@ namespace App\Providers;
 
 use App\Entities\Caches\MenuCache;
 use App\Entities\Caches\SettingCache;
-use App\Services\SettingService;
+use App\Services\{
+    MenuService,
+    PageBuilderService,
+    PageService,
+    SettingService,
+    TranslationService,
+};
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,7 +18,12 @@ class AppServiceProvider extends ServiceProvider
     public $singletons = [
         MenuCache::class => MenuCache::class,
         SettingCache::class => SettingCache::class,
+
+        MenuService::class => MenuService::class,
+        PageBuilderService::class => PageBuilderService::class,
+        PageService::class => PageService::class,
         SettingService::class => SettingService::class,
+        TranslationService::class => TranslationService::class,
     ];
 
     /**
