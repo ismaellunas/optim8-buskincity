bd8b1f60e06776fd05421de575a6c18417f98e54
Naufal Rivaldi <naufal@sdbagency.com>
2021-12-08 16:35:01 +0700

    Add component dropwdown scroll

---
+++ resources/js/Sdb/DropdownScroll.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="dropdown-scroll">
+        <slot />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SdbDropdownScroll"
+    }
+</script>
+
+<style scoped>
+    #dropdown-scroll {
+        max-height: 300px;
+        overflow-x: auto;
+    }
+</style>
