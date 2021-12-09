0b9aa096c12666baf34c3ed5ef28536e512b487e
Bayu <boedy@sdbagency.com>
2021-12-09 16:36:18 +0700

    Refactor SdbInputFile vue component

--- resources/js/Sdb/InputFile.vue
+++ resources/js/Sdb/InputFile.vue
@@ -35,43 +35,63 @@
 </template>
 
 <script>
-    import { isEmpty, concat } from 'lodash';
-    import { useModelWrapper } from '@/Libs/utils';
+    import { concat } from 'lodash';
+    import { isBlank, useModelWrapper } from '@/Libs/utils';
 
     export default {
+        name: 'SdbInputFile',
+
         props: {
-            accept: {type: Array, default: []},
-            isNameDisplayed: {type: Boolean, default: true},
-            modelValue: {},
+            accept: {
+                type: Array,
+                default: () => []
+            },
+            isNameDisplayed: {
+                type: Boolean,
+                default: true
+            },
+            modelValue: {
+                type: [File, null],
+                required: true,
+            },
         },
+
+        emits: ['on-file-picked'],
+
         setup(props, { emit }) {
             return {
                 file: useModelWrapper(props, emit),
             };
         },
+
         computed: {
             hasFile() {
-                return !isEmpty(this.file);
+                return !isBlank(this.file);
             },
+
             fileName() {
                 if (this.hasFile) {
                     return this.file.name
                 }
                 return "...";
             },
+
             wrapperClass() {
                 return concat(
                     this.isNameDisplayed ? 'has-name': null,
                 ).filter(Boolean);
             },
         },
+
         methods: {
             resetFile() {
                 this.$refs.fileInput.value = null;
             },
+
             pickFile() {
                 let input = this.$refs.fileInput
                 let file = input.files
+
                 if (file && file[0]) {
                     let reader = new FileReader
                     reader.onload = (event) => {
@@ -82,5 +102,5 @@
                 }
             },
         },
-    }
+    };
 </script>
