2be5ce49c987ca03e75bf2daac4f971e6cf74f26
Refo Junior <refo@sdbagency.com>
2021-12-10 09:44:11 +0800

    add social media on the menu 3

--- resources/views/components/navbar/navbar_layout_three.blade.php
+++ resources/views/components/navbar/navbar_layout_three.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar is-primary">
+<nav class="navbar is-primary is-justify-content-space-between">
     <div class="navbar-brand">
         <a
             class="navbar-item"
@@ -20,57 +20,74 @@
             <span />
         </div>
     </div>
-
-    <div
-        id="navbarExampleTransparentExample"
-        class="navbar-menu"
-    >
-        <div class="navbar-end">
-            @foreach ($menus as $menu)
-                @if ($menu->children)
-                    <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link">
-                            {{ $menu->title }}
-                        </a>
-                        <div class="navbar-dropdown">
-                            @foreach ($menu->children as $childMenu)
-                                <a
-                                    class="navbar-item"
-                                    href="{{ $childMenu->link }}"
-                                >
-                                    {{ $childMenu->title }}
-                                </a>
-                            @endforeach
+    <div>
+        <div
+            id="navbarExampleTransparentExample"
+            class="navbar-menu"
+        >
+            <div class="navbar-end">
+                @foreach ($menus as $menu)
+                    @if ($menu->children)
+                        <div class="navbar-item has-dropdown is-hoverable">
+                            <a class="navbar-link">
+                                {{ $menu->title }}
+                            </a>
+                            <div class="navbar-dropdown">
+                                @foreach ($menu->children as $childMenu)
+                                    <a
+                                        class="navbar-item"
+                                        href="{{ $childMenu->link }}"
+                                    >
+                                        {{ $childMenu->title }}
+                                    </a>
+                                @endforeach
+                            </div>
                         </div>
-                    </div>
-                @else
-                    <a
-                        class="navbar-item"
-                        href="{{ $menu->link }}"
-                    >
-                        {{ $menu->title }}
-                    </a>
-                @endif
-            @endforeach
-            <div class="navbar-item has-dropdown is-hoverable">
-                <span class="navbar-link">{{ strtoupper($currentLanguage) }}</span>
-                <div class="navbar-dropdown is-boxed">
-                    @foreach ($languageOptions as $language)
+                    @else
                         <a
                             class="navbar-item"
-                            href="{{ route('language.change', $language['id']) }}"
+                            href="{{ $menu->link }}"
                         >
-                            {{ strtoupper($language['id']) }}
+                            {{ $menu->title }}
                         </a>
-                    @endforeach
+                    @endif
+                @endforeach
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <span class="navbar-link">{{ strtoupper($currentLanguage) }}</span>
+                    <div class="navbar-dropdown is-boxed">
+                        @foreach ($languageOptions as $language)
+                            <a
+                                class="navbar-item"
+                                href="{{ route('language.change', $language['id']) }}"
+                            >
+                                {{ strtoupper($language['id']) }}
+                            </a>
+                        @endforeach
+                    </div>
                 </div>
+                <a
+                    href="{{ route('login') }}"
+                    class="navbar-item pr-5"
+                >
+                    Login
+                </a>
             </div>
-            <a
-                href="{{ route('login') }}"
-                class="navbar-item pr-5"
-            >
-                Login
-            </a>
+        </div>
+        <div class="navbar-menu">
+            <div class="navbar-end pr-3">
+                <a href="" class="navbar-item">
+                    <i class="fab fa-facebook-square fa-2x"></i>
+                </a>
+                <a href="" class="navbar-item">
+                    <i class="fab fa-twitter-square fa-2x"></i>
+                </a>
+                <a href="" class="navbar-item">
+                    <i class="fab fa-instagram fa-2x"></i>
+                </a>
+                <a href="" class="navbar-item">
+                    <i class="fab fa-snapchat fa-2x"></i>
+                </a>
+            </div>
         </div>
     </div>
 </nav>
