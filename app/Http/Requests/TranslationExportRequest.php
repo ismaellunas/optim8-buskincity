fab227127891d949b3e63d78911553865323020f
Bayu <boedy@sdbagency.com>
2021-12-14 12:32:36 +0700

    Add TranslationManagerRequest and change TranslationExportRequest to extend TranslationManagerRequest

--- app/Http/Requests/TranslationExportRequest.php
+++ app/Http/Requests/TranslationExportRequest.php
@@ -3,42 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Validation\Rule;
 
-class TranslationExportRequest extends FormRequest
+class TranslationExportRequest extends TranslationManagerRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        $groups = config('constants.translations.groups');
-
-        return [
-            'groups' => [
-                'nullable',
-                'array',
-            ],
-            'groups.*' => [
-                Rule::in($groups),
-            ],
-        ];
-    }
-
     protected function failedValidation(Validator $validator)
     {
         try {
