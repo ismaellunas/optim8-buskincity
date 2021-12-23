97f1399b96fb9f962741b2b08f952ed3d1946604
Bayu <boedy@sdbagency.com>
2021-12-23 18:09:37 +0700

    Authorize user when suspend and unsuspend in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -214,14 +214,22 @@ class UserController extends CrudController
 
     public function suspend(User $user)
     {
+        $this->authorize('suspend', $user);
+
         $user->suspend();
+
+        $this->generateFlashMessage('User suspend successfully!');
 
         return back();
     }
 
     public function unsuspend(User $user)
     {
+        $this->authorize('unsuspend', $user);
+
         $user->unsuspend();
+
+        $this->generateFlashMessage('User unsuspend successfully!');
 
         return back();
     }
