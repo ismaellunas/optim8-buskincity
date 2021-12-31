8481d6719e79d49f8c3c8ac9cc617993aff900aa
Naufal Rivaldi <naufal@sdbagency.com>
2021-12-31 13:24:28 +0700

    Move import line to import block

--- resources/js/frontend.js
+++ resources/js/frontend.js
@@ -3,9 +3,9 @@ const app = createApp({});
 
 // Components
 import Carousel from '@/Components/Builder/Content/Carousel';
-app.component('Carousel', Carousel);
-
 import Tabs from '@/Components/Builder/Content/Tabs';
+
+app.component('Carousel', Carousel);
 app.component('Tabs', Tabs);
 
 // Mount
