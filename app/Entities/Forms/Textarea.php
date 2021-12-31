cdeda0d111b80955d48e5f7386d7142bd0caa2e5
Bayu <boedy.bayu@gmail.com>
2021-12-31 19:40:31 +0700

    Add Forms/Text and Forms/Textarea entity

---
+++ app/Entities/Forms/Textarea.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entities\Forms;
+
+class Textarea extends Text
+{
+    protected $type = "Textarea";
+
+    public $rows;
+
+    public function __construct(string $name, array $data = [])
+    {
+        parent::__construct($name, $data);
+
+        $this->rows = $data['rows'] ?? null;
+    }
+
+    public function schema(): array
+    {
+        $schema = parent::schema();
+
+        $schema['rows'] = $this->rows;
+
+        return $schema;
+    }
+}
