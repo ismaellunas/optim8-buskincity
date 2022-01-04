21888cae68153e7fa3d4249ba79e06f6dc19d806
Bayu <boedy@sdbagency.com>
2022-01-04 15:28:06 +0700

    Remove unused code in BiodataForm

--- resources/js/Pages/Profile/BiodataForm.vue
+++ resources/js/Pages/Profile/BiodataForm.vue
@@ -13,28 +13,11 @@
                 :name="formName"
             />
         </template>
-
-        <template #actions>
-            <!--
-            <jet-action-message :on="form.recentlySuccessful" class="mr-3">
-                Saved.
-            </jet-action-message>
-
-            <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Save
-            </jet-button>
-            -->
-        </template>
     </jet-form-section>
 </template>
 
 <script>
-    import JetActionMessage from '@/Jetstream/ActionMessage'
-    import JetButton from '@/Jetstream/Button'
     import JetFormSection from '@/Jetstream/FormSection'
-    import JetInput from '@/Jetstream/Input'
-    import JetInputError from '@/Jetstream/InputError'
-    import JetLabel from '@/Jetstream/Label'
     import FormBiodata from '@/Form/FormBuilder';
 
     export default {
@@ -42,12 +25,7 @@
 
         components: {
             FormBiodata,
-            //JetActionMessage,
-            //JetButton,
             JetFormSection,
-            //JetInput,
-            //JetInputError,
-            //JetLabel,
         },
 
         data() {
@@ -55,7 +33,5 @@
                 formName: 'biodata',
             };
         },
-
-        methods: {},
     }
 </script>
