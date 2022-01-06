5580f28dab6a863f77c53f7636c59a6a84fce88e
Naufal Rivaldi <naufal@sdbagency.com>
2022-01-06 16:19:00 +0700

    Adding form section and section title component

---
+++ resources/js/Sdb/FormSection.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-one-third">
+                <sdb-section-title>
+                    <template #title>
+                        <slot name="title" />
+                    </template>
+                    <template #description>
+                        <slot name="description" />
+                    </template>
+                </sdb-section-title>
+            </div>
+            <div class="column is-two-thirds">
+                <form @submit.prevent="$emit('submitted')">
+                    <div
+                        class="px-4 py-4"
+                        :class="hasActions ? '' : ''"
+                    >
+                        <div class="">
+                            <slot name="form" />
+                        </div>
+                    </div>
+
+                    <div
+                        v-if="hasActions"
+                        class="px-4 py-3"
+                    >
+                        <slot name="actions" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import SdbSectionTitle from './SectionTitle'
+
+    export default {
+        name: 'SdbFormSection',
+
+        components: {
+            SdbSectionTitle,
+        },
+
+        emits: ['submitted'],
+
+        computed: {
+            hasActions() {
+                return !! this.$slots.actions
+            }
+        }
+    }
+</script>
