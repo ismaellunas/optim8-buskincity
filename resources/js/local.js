f768843be0ea72cde48090e3827072cc94bab554
Naufal Rivaldi <naufal@sdbagency.com>
2022-01-24 16:35:36 +0700

    import for local tinymce

--- resources/js/local.js
+++ resources/js/local.js
@@ -1,3 +1,25 @@
-require('@fortawesome/fontawesome-free/js/fontawesome.min');
-require('@fortawesome/fontawesome-free/js/regular.min');
-require('@fortawesome/fontawesome-free/js/solid.min');
+// Tiny MCE
+import 'tinymce/tinymce'
+import 'tinymce/themes/silver/theme'
+import 'tinymce/icons/default'
+// Plugins
+import 'tinymce/plugins/fullscreen'
+import 'tinymce/plugins/paste'
+import 'tinymce/plugins/autoresize'
+import 'tinymce/plugins/advlist'
+import 'tinymce/plugins/autolink'
+import 'tinymce/plugins/lists'
+import 'tinymce/plugins/link'
+import 'tinymce/plugins/image'
+import 'tinymce/plugins/charmap'
+import 'tinymce/plugins/print'
+import 'tinymce/plugins/preview'
+import 'tinymce/plugins/anchor'
+import 'tinymce/plugins/searchreplace'
+import 'tinymce/plugins/visualblocks'
+import 'tinymce/plugins/code'
+import 'tinymce/plugins/media'
+import 'tinymce/plugins/table'
+import 'tinymce/plugins/wordcount'
+import 'tinymce/plugins/hr'
+import 'tinymce/plugins/insertdatetime'
