8686973625398550594c7be4419c0e90113471c7
Bayu <boedy@sdbagency.com>
2022-01-25 16:51:17 +0700

    Create getSchema() method and call it on mounted hook and after succesfully submitting the form

--- resources/js/Form/Builder.vue
+++ resources/js/Form/Builder.vue
@@ -74,34 +74,38 @@
         },
 
         mounted() {
-            const self = this;
-
-            axios.get(
-                route(self.routeGetSchemas),
-                {
-                    params: {
-                        id: self.entityId,
-                        route_name: self.routeName
-                    }
-                }
-
-            ).then((response) => {
-                self.fieldGroups = response.data;
-
-                self.form = self.createForm(self.fieldGroups);
-
-                self.$emit('loaded-successfully', response.data);
-
-            }).catch((error) => {
-                if (error.response) {
-                    if (error.response.status == 403) {
-                        self.$emit('loaded-forbidden', error.response);
-                    }
-                }
-            });
+            this.getSchemas();
         },
 
         methods: {
+            getSchemas() {
+                const self = this;
+
+                return axios.get(
+                    route(self.routeGetSchemas),
+                    {
+                        params: {
+                            id: self.entityId,
+                            route_name: self.routeName
+                        }
+                    }
+
+                ).then((response) => {
+                    self.fieldGroups = response.data;
+
+                    self.form = self.createForm(self.fieldGroups);
+
+                    self.$emit('loaded-successfully', response.data);
+
+                }).catch((error) => {
+                    if (error.response) {
+                        if (error.response.status == 403) {
+                            self.$emit('loaded-forbidden', error.response);
+                        }
+                    }
+                });
+            },
+
             createForm(groupFields) {
                 const form = {
                     id: this.entityId
@@ -124,26 +128,6 @@
                 return useForm(form);
             },
 
-            /*
-            createForm(fields) {
-                const form = {
-                    id: this.entityId
-                };
-
-                if (!isEmpty(fields)) {
-                    for (const [key, field] of Object.entries(fields)) {
-                        if (typeof field.value === 'undefined') {
-                            form[ key ] = undefined;
-                        } else {
-                            form[ key ] = field.value;
-                        }
-                    }
-                }
-
-                return useForm(form);
-            },
-            */
-
             submit() {
                 const self = this;
 
@@ -160,8 +144,10 @@
                             onStart: () => {
                                 self.loader = self.$loading.show();
                             },
-                            onSuccess: (page) => {
+                            onSuccess: async (page) => {
                                 successAlert(page.props.flash.message);
+
+                                await self.getSchemas();
 
                                 self.resetFields();
 
