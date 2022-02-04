719d9e81a8242e55a556c4232d15f058ff8d622d
Naufal Rivaldi <naufal@sdbagency.com>
2022-02-04 14:53:36 +0700

    Add class break long text to avoid long text

--- resources/views/components/builder/column.blade.php
+++ resources/views/components/builder/column.blade.php
@@ -1,6 +1,6 @@
 @aware(['entities' => [], 'locale' => '', 'images' => []])
 
-<div class="column">
+<div class="column break-long-text">
     @foreach ($components as $pageComponent)
         <x-dynamic-component
             :component="$componentName($pageComponent['componentName'])"
