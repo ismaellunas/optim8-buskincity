f232216a3edb466501d74b58b906e30aac077bc8
Naufal Rivaldi <naufal@sdbagency.com>
2022-02-21 10:53:05 +0700

    Add GeoIP and IP Registry

---
+++ app/Entities/GeoIP/IPRegistry.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entities\GeoIP;
+
+use Exception;
+use App\Entities\GeoIP\Support\HttpClient;
+use Torann\GeoIP\Services\AbstractService;
+
+class IPRegistry extends AbstractService
+{
+    protected $client;
+
+    public function boot()
+    {
+        $this->client = new HttpClient([
+            'base_uri' => 'https://api.ipregistry.co/',
+            'query' => [
+                'key' => $this->config('key'),
+            ],
+        ]);
+    }
+
+    public function locate($ip)
+    {
+        // Get data from client
+        $data = $this->client->get($ip);
+
+        // Verify server response
+        if ($this->client->getErrors() !== null) {
+            throw new Exception('Request failed (' . $this->client->getErrors() . ')');
+        }
+
+        // Parse body content
+        $json = json_decode($data[0], true);
+
+        return $this->hydrate($json);
+    }
+}
