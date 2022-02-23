36687c7420836e65373b560a6a85f59c5139847f
Naufal Rivaldi <naufal@sdbagency.com>
2022-02-23 11:48:24 +0700

    Add code to get IP Address with CloudFlare's

--- app/Services/IPService.php
+++ app/Services/IPService.php
@@ -4,33 +4,34 @@ namespace App\Services;
 
 class IPService
 {
-    private function getUserIp()
+    private function getClientIp()
     {
-        // It will get IP version 4 or 6
-        // https://stackoverflow.com/questions/51245053/how-can-i-get-both-ipv4-and-ipv6-address-using-php-code
-        if (isset($_SERVER["HTTP_CLIENT_IP"])) {
-            $ip = $_SERVER["HTTP_CLIENT_IP"];
-        } elseif (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
-            $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
-        } elseif (isset($_SERVER["HTTP_X_FORWARDED"])) {
-            $ip = $_SERVER["HTTP_X_FORWARDED"];
-        } elseif (isset($_SERVER["HTTP_FORWARDED_FOR"])) {
-            $ip = $_SERVER["HTTP_FORWARDED_FOR"];
-        } elseif (isset($_SERVER["HTTP_FORWARDED"])) {
-            $ip = $_SERVER["HTTP_FORWARDED"];
+        $ipaddress = '';
+
+        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+            $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
         } else {
-            $ip = $_SERVER["REMOTE_ADDR"];
+            if (isset($_SERVER['HTTP_CLIENT_IP']))
+                $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+            else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+                $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            else if(isset($_SERVER['HTTP_X_FORWARDED']))
+                $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+            else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+                $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+            else if(isset($_SERVER['HTTP_FORWARDED']))
+                $ipaddress = $_SERVER['HTTP_FORWARDED'];
+            else if(isset($_SERVER['REMOTE_ADDR']))
+                $ipaddress = $_SERVER['REMOTE_ADDR'];
+            else
+                $ipaddress = 'UNKNOWN';
         }
-
-        if (strpos($ip, ',') > 0) {
-            $ip = substr($ip, 0, strpos($ip, ','));
-        }
-
-        return $ip;
+        return $ipaddress;
     }
 
     public function getUserData()
     {
-        return geoip($this->getUserIp());
+        return geoip($this->getClientIp());
     }
 }
