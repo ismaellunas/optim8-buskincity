fbd3e58c4068484a1169a5d54d6e28e12f572b83
Naufal Rivaldi <naufal@sdbagency.com>
2022-03-02 14:54:42 +0700

    Fix class text and textarea

--- app/View/Components/Form/Fields/Text.php
+++ app/View/Components/Form/Fields/Text.php
@@ -4,10 +4,15 @@ namespace App\View\Components\Form\Fields;
 
 class Text extends BaseField
 {
-    public function __construct($label, $value, $translate, $userLocale)
+    public function __construct($label, $value, $isTranslated, $userLocale)
     {
-        parent::__construct($label, $value, $translate, $userLocale);
+        parent::__construct($label, $value, $isTranslated, $userLocale);
 
         $this->value = $this->setValueTranslation($value);
+    }
+
+    protected function getViewName(): string
+    {
+        return "base-field";
     }
 }
