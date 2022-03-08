0da0ff5c3cc2257a81b8551103369f10696aeb5e
Naufal Rivaldi <naufal@sdbagency.com>
2022-03-08 12:15:26 +0700

    Add all seeder to database seeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\{
-    Category,
-    Language,
-    Menu,
-    MenuItem,
-    Page,
-    Permission,
-    Post,
-    Role,
-    User,
-};
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
-use App\Services\LanguageService;
 
 class DatabaseSeeder extends Seeder
 {
@@ -34,102 +21,12 @@ class DatabaseSeeder extends Seeder
             LanguageSeeder::class,
             TranslationSeeder::class,
             FormSeeder::class,
+            CategorySeeder::class,
+            UserAndPermissionSeeder::class,
+            LanguageSettingSeeder::class,
+            PageSeeder::class,
+            PostSeeder::class,
+            MenuSeeder::class,
         ]);
-
-        // Wildcard permissions
-        $wildcardPermissions = Permission::where('name', 'LIKE', '%.*')->get();
-
-        $administratorRole = Role::whereName('Administrator')->first();
-        $administratorRole->syncPermissions($wildcardPermissions);
-
-        // System Permissions
-        $systemPermissions = Permission::where('name', 'LIKE', 'system.%')->get();
-        foreach ($systemPermissions as $permission) {
-            $administratorRole->givePermissionTo($permission);
-        }
-
-        $superAdminUser = User::factory()->create([
-            'first_name' => 'Super',
-            'last_name' => 'Administrator',
-            'email' => 'super.administrator@biz.com',
-        ]);
-
-        $superAdminUser->assignRole(config('permission.super_admin_role'));
-
-        $adminUser = User::factory()
-            ->create([
-                'first_name' => 'Admin',
-                'last_name' => 'Administrator',
-                'email' => 'admin@biz.com',
-            ]);
-
-        $adminUser->assignRole('Administrator');
-
-        $performer = User::factory()->create([
-            'first_name' => 'Dan',
-            'last_name' => 'Rice',
-            'email' => 'dan.rice@biz.com',
-        ]);
-
-        $performerRole = Role::whereName('Performer')->first();
-        $performerRole->syncPermissions(['payment.management']);
-
-        $performer->assignRole('Performer');
-
-        $category = Category::factory()
-            ->hasTranslations(1, ['name' => 'News'])
-            ->create();
-
-        Post::factory()
-            ->count(2)
-            ->for($adminUser, 'author')
-            ->state(new Sequence(
-                ['status' => Post::STATUS_DRAFT],
-                ['status' => Post::STATUS_PUBLISHED],
-            ))
-            ->hasAttached($category)
-            ->fakeContent()
-            ->create();
-
-        Page::factory()
-            ->hasTranslations(1)
-            ->create();
-
-        Menu::factory()
-            ->count(3)
-            ->has(
-                MenuItem::factory()
-            )
-            ->state(new Sequence(
-                ['type' => Menu::TYPE_HEADER],
-                ['type' => Menu::TYPE_FOOTER],
-                ['type' => Menu::TYPE_SOCIAL_MEDIA],
-            ))
-            ->create([
-                'locale' => config('app.fallback_locale')
-            ]);
-
-        $this->setActiveLanguages();
-        $this->setDefaultLanguage();
-    }
-
-    private function setActiveLanguages()
-    {
-        $activeLanguages = [
-            'en',
-            'pt',
-            'sv',
-            'de',
-        ];
-
-        Language::whereIn('code', $activeLanguages)
-            ->update(['is_active' => true]);
-    }
-
-    private function setDefaultLanguage()
-    {
-        $englishId = Language::where('code', 'en')->value('id');
-
-        app(LanguageService::class)->setDefault($englishId);
     }
 }
