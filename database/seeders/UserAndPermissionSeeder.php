59c33eae943b5f01d2871e3afba8f57ea19f9f12
Naufal Rivaldi <naufal@sdbagency.com>
2022-03-08 12:15:04 +0700

    Set seeder for startup data

---
+++ database/seeders/UserAndPermissionSeeder.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\{
+    Permission,
+    Role,
+    User,
+};
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+
+class UserAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->setPermissionToRoles();
+
+        $superAdminUser = User::factory()->create([
+            'first_name' => 'Super',
+            'last_name' => 'Administrator',
+            'email' => 'super.administrator@biz.com',
+        ]);
+
+        $superAdminUser->assignRole(config('permission.super_admin_role'));
+
+        $adminUser = User::factory()
+            ->create([
+                'first_name' => 'Admin',
+                'last_name' => 'Administrator',
+                'email' => 'admin@biz.com',
+            ]);
+
+        $adminUser->assignRole('Administrator');
+
+        $performers = User::factory()
+            ->count(2)
+            ->state(new Sequence(
+                [
+                    'first_name' => 'Dan',
+                    'last_name' => 'Rice',
+                    'email' => 'dan.rice@biz.com',
+                ],
+                [
+                    'first_name' => 'John',
+                    'last_name' => 'Doe',
+                    'email' => 'john.doe@biz.com',
+                ],
+            ))
+            ->create();
+
+        foreach ($performers as $performer) {
+            $performer->assignRole('Performer');
+        }
+    }
+
+    private function setPermissionToRoles(): void
+    {
+        // Wildcard permissions
+        $wildcardPermissions = Permission::where('name', 'LIKE', '%.*')->get();
+
+        $administratorRole = Role::whereName('Administrator')->first();
+        $administratorRole->syncPermissions($wildcardPermissions);
+
+        // System Permissions
+        $systemPermissions = Permission::where('name', 'LIKE', 'system.%')->get();
+
+        foreach ($systemPermissions as $permission) {
+            $administratorRole->givePermissionTo($permission);
+        }
+
+        // Payment management Permissions
+        $performerRole = Role::whereName('Performer')->first();
+
+        $performerRole->syncPermissions(['payment.management']);
+    }
+}
