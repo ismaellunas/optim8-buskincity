216eba945f2d38930a096a0dc4d2404735926136
Bayu <boedy@sdbagency.com>
2022-03-17 10:26:29 +0700

    Handle get and save using stripeSettingService

---
+++ app/Services/StripeSettingService.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Setting;
+use Illuminate\Support\Collection;
+
+class StripeSettingService
+{
+    private $casts = [
+        'stripe_amount_options' => 'object',
+        'stripe_application_fee_percentage' => 'float',
+        'stripe_default_country' => 'string',
+        'stripe_is_enabled' => 'bool',
+        'stripe_minimal_amounts' => 'array',
+        'stripe_payment_currencies' => 'array',
+    ];
+
+    public function getKeys(): array
+    {
+        return array_keys($this->casts);
+    }
+
+    public function save(string $key, mixed $value): Setting
+    {
+        $dataType = $this->casts[$key] ?? 'string';
+
+        switch ($dataType) {
+            case 'array': $convertedValue = json_encode($value); break;
+            case 'bool': $convertedValue = (bool)$value; break;
+            case 'float': $convertedValue = (float)$value; break;
+            case 'int': $convertedValue = (int)$value; break;
+            case 'object': $convertedValue = json_encode($value); break;
+            default: $convertedValue = $value;
+        }
+
+        return Setting::updateOrCreate(
+            ['key' => $key],
+            ['value' => $convertedValue]
+        );
+    }
+
+    public function getAll(): Collection
+    {
+        $settings = Setting::whereIn('key', $this->getKeys())
+            ->select(['key', 'value'])
+            ->get();
+
+        return $settings->mapWithKeys(function ($setting) {
+
+            $dataType = $this->casts[$setting->key] ?? 'string';
+
+            $value = null;
+
+            switch ($dataType) {
+                case 'array': $value = json_decode($setting->value); break;
+                case 'bool': $value = (bool)$setting->value; break;
+                case 'float': $value = (float)$setting->value; break;
+                case 'int': $value = (int)$setting->value; break;
+                case 'object': $value = (object)json_decode($setting->value); break;
+                default: $value = $setting->value;
+            }
+
+            return [$setting->key => $value];
+        });
+    }
+}
