267c727d7fbffc9a095078adac5f7f24b681be7a
Naufal Rivaldi <naufal@sdbagency.com>
2022-03-22 16:26:09 +0700

    Set url on email based on theme logo

--- themes/biz/views/vendor/mail/html/header.blade.php
+++ themes/biz/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === config('app.name'))
-<img src="{{ url('assets/img/biz752-logo.png') }}" class="logo" alt="{{ config('app.name') }}">
+<img src="{{ ThemeHelper::getLogoUrl() }}" class="logo" alt="{{ config('app.name') }}">
 @else
 {{ $slot }}
 @endif
