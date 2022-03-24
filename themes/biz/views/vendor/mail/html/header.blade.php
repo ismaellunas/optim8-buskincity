3858df8beb380bfc55fd218ed5fa7b633e308139
Bayu <boedy@sdbagency.com>
2022-03-24 10:22:07 +0700

    Display application name if the logo is empty.

--- themes/biz/views/vendor/mail/html/header.blade.php
+++ themes/biz/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === config('app.name'))
+@if (trim($slot) === config('app.name') && !empty(ThemeHelper::getLogoUrl()))
 <img src="{{ ThemeHelper::getLogoUrl() }}" class="logo" alt="{{ config('app.name') }}">
 @else
 {{ $slot }}
