ca68edc880922f50058a09e109f676d626be7059
Bayu <boedy@sdbagency.com>
2022-04-05 13:05:41 +0700

    Remove triggering job to update stripe logo in ThemeHeaderController

--- app/Http/Controllers/ThemeHeaderController.php
+++ app/Http/Controllers/ThemeHeaderController.php
@@ -4,11 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Entities\CloudinaryStorage;
 use App\Http\Requests\ThemeHeaderLayoutRequest;
-use App\Jobs\UpdateStripeConnectedAccountBrandingLogo;
 use App\Models\{
     Media,
     Menu,
-    MenuItem,
     Setting,
 };
 use App\Services\{
@@ -17,7 +15,6 @@ use App\Services\{
     SettingService,
     TranslationService,
 };
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Str;
 use Inertia\Inertia;
@@ -28,7 +25,6 @@ class ThemeHeaderController extends ThemeOptionController
     private $menuService;
     private $settingService;
     private $modelMenu = Menu::class;
-    private $modelMenuItem = MenuItem::class;
 
     protected $baseRouteName = 'admin.theme.header';
     protected $componentName = 'ThemeHeader/';
@@ -88,11 +84,6 @@ class ThemeHeaderController extends ThemeOptionController
             ]);
             $setting->value = $media->id;
             $setting->save();
-
-            $job = new UpdateStripeConnectedAccountbrandingLogo($media);
-            $job->delay(now()->addMinutes(1));
-
-            dispatch($job);
         }
 
         $this->generateFlashMessage('Header layout updated successfully!');
