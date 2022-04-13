5f28e6fe21371c55c938fffedd8fd96b2784efc2
Bayu <boedy.bayu@gmail.com>
2022-04-13 15:54:15 +0700

    Update store to provide options and parameters for performer application form

--- app/Http/Controllers/Frontend/PerformerApplicationController.php
+++ app/Http/Controllers/Frontend/PerformerApplicationController.php
@@ -3,18 +3,43 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-//use Inertia\Inertia;
+use App\Services\CountryService;
+use Inertia\Inertia;
 
 class PerformerApplicationController extends Controller
 {
     public function create()
     {
+        $user = auth()->user();
 
+        $userMetas = $user->getMetas(['country']);
+
+        return Inertia::render('ApplicationPerformer', [
+            'disciplineOptions' => $this->getDisciplineOptions(),
+            'countryOptions' => app(CountryService::class)->getCountryOptions(),
+            'defaultCountry' => $userMetas->get('country'),
+            'firstName' => $user->first_name,
+            'lastName' => $user->last_name,
+            'email' => $user->email,
+        ]);
     }
 
     public function store()
     {
 
+    }
+
+    private function getDisciplineOptions(): array
+    {
+        return collect(config('buskincity.disciplines'))
+            ->values()
+            ->map(function ($discipline) {
+                return [
+                    'id' => $discipline,
+                    'value' => $discipline,
+                ];
+            })
+            ->all();
     }
 
     private function sendEmail(array $data)
