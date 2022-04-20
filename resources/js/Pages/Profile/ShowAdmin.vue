0dc82abaf276ae60bc7e5418506ae30439e2904c
Naufal Rivaldi <naufal@sdbagency.com>
2022-04-20 15:52:01 +0700

    Add qrcode on user profile

--- resources/js/Pages/Profile/ShowAdmin.vue
+++ resources/js/Pages/Profile/ShowAdmin.vue
@@ -22,18 +22,28 @@
 
         provide() {
             return {
+                can: this.can,
                 countryOptions: this.countryOptions,
                 errors: this.errors,
+                profilePageUrl: this.profilePageUrl,
                 sessions: this.sessions,
                 supportedLanguageOptions: this.supportedLanguageOptions,
+                qrCodeIsDisplayed: this.qrCodeIsDisplayed,
+                qrCodeLogo: this.qrCodeLogo,
+                qrCodeLogoName: this.qrCodeLogoName,
             }
         },
 
         props: {
+            can: { type: Object, required: true },
             countryOptions: { type: Array, default: () => [] },
             errors: {type: Object, default: () => {}},
+            profilePageUrl: { type: String, default: null },
             sessions: { type: Array, default:() => [] },
             supportedLanguageOptions: { type: Array, default: () => [] },
+            qrCodeIsDisplayed: { type: Boolean, default: false },
+            qrCodeLogo: { type: String, default: null },
+            qrCodeLogoName: { type: String, default: null },
         },
     };
 </script>
