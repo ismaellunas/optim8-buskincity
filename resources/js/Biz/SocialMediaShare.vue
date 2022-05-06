0c80d903acf161e0cd7c8ac065d1bde732daca4a
Naufal Rivaldi <naufal@sdbagency.com>
2022-05-06 17:58:22 +0700

    Make component for vue social sharing

---
+++ resources/js/Biz/SocialMediaShare.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <template
+            v-for="(socialmedia, network) in data"
+            :key="network"
+        >
+            <ShareNetwork
+                :class="`button as-text-black ml-1 ` + socialmedia.class"
+                :network="network"
+                :url="socialmedia.url"
+                :title="socialmedia.title"
+                :description="socialmedia.description"
+                :hashtags="socialmedia.hashtags"
+            >
+                <span class="icon">
+                    <i :class="socialmedia.icon" />
+                </span>
+                &nbsp; {{ titleCase(network) }}
+            </ShareNetwork>
+        </template>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SocialMediaShare',
+
+        props: {
+            data: {type: Object, default:() => {}},
+        },
+
+        methods: {
+            titleCase(string) {
+                return string[0].toUpperCase() + string.slice(1).toLowerCase();
+            }
+        },
+    }
+</script>
