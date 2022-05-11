685d98e030f563d5cb3b6329899b6b655e95974d
Bayu <boedy@sdbagency.com>
2022-05-11 12:44:24 +0700

    Handle header title to load application name for inertia page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 require('./bootstrap');
 
 // Import modules...
+import { appName } from '@/Libs/defaults';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress';
@@ -9,7 +10,7 @@ import VueLoading from 'vue-loading-overlay';
 import VueSocialSharing from 'vue-social-sharing'
 
 createInertiaApp({
-    title: title => `${title} - My App`,
+    title: title => `${title} | ${appName}`,
     resolve: (name) => import(`./Pages/${name}`),
     //resolve: name => require(`./Pages/${name}`),
     setup({ el, app, props, plugin }) {
