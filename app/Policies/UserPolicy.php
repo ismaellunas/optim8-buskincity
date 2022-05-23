112a993d44e474d39ff7104944c388757f073dd3
Naufal Rivaldi <naufal@sdbagency.com>
2022-05-23 23:52:04 +0700

    Delete 2 return

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -83,7 +83,6 @@ class UserPolicy extends BasePermissionPolicy
 
     public function setPassword(User $user, Model $selectedUser)
     {
-        return !$user->isConnectedAccount;
         return (
             parent::update($user, $selectedUser)
             || !$selectedUser->isConnectedAccount
