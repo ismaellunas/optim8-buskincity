ad9fce84065463e867cc2ccc00f1a517f26bbe9f
Naufal Rivaldi <naufal@sdbagency.com>
2022-05-24 10:11:58 +0700

    Remove slug regex on route

--- routes/web.php
+++ routes/web.php
@@ -151,7 +151,6 @@ Route::group([
         ->name('blog.index');
 
     Route::get('/category/{category_translation}', [PostCategoryController::class, 'index'])
-        ->where('slug', '[\w\d\-\_]+')
         ->name('blog.category.index');
 
     Route::get('blog/{slug}', [PostController::class, 'show'])
