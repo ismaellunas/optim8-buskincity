defd8b044518c2a2ab6235de0a860ef98ca020af
Naufal Rivaldi <naufal@sdbagency.com>
2022-05-30 10:17:17 +0700

    Update controller to provide dynamic data

--- app/Http/Controllers/Frontend/ProfileController.php
+++ app/Http/Controllers/Frontend/ProfileController.php
@@ -7,7 +7,8 @@ use App\Models\User;
 use App\Services\{
     FormService,
     SettingService,
-    TranslationService
+    TranslationService,
+    UserProfileService,
 };
 use Illuminate\Support\Str;
 
@@ -15,13 +16,16 @@ class ProfileController extends Controller
 {
     private $formService;
     private $settingService;
+    private $userProfileService;
 
     public function __construct(
         FormService $formService,
         SettingService $settingService,
+        UserProfileService $userProfileService,
     ) {
         $this->formService = $formService;
         $this->settingService = $settingService;
+        $this->userProfileService = $userProfileService;
     }
 
     public function show(User $user)
@@ -52,10 +56,27 @@ class ProfileController extends Controller
             );
         }
 
+        $this->userData($data, $user);
+
         if (view()->exists($viewName)) {
             return view($viewName, $data);
         }
 
         return view('profile', $data);
+    }
+
+    private function userData(&$data, User $user): void
+    {
+        $topBackgroundPicture = $this->userProfileService
+            ->getMedias('top_background_picture')
+            ->first();
+
+        $data = array_merge(
+            $data,
+            [
+                'profileBackgroundUrl' => $topBackgroundPicture->file_url ?? null,
+                'profilePhotoUrl' => $user->profilePhotoUrl ?? url('/images/profile-picture-default.png'),
+            ]
+        );
     }
 }
