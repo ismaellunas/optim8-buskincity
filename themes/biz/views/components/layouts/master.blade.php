e0a680b04b5b165755d4a8c850de0fca9a12da59
Naufal Rivaldi <naufal@sdbagency.com>
2022-06-09 15:02:08 +0700

    Fix meta description on public profile page

--- themes/biz/views/components/layouts/master.blade.php
+++ themes/biz/views/components/layouts/master.blade.php
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        @if ($metaDescription)
+        @if (isset($metaDescription))
             <meta name="description" content="{{ $metaDescription }}">
         @endif
 
