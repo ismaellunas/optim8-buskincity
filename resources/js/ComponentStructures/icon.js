2295e82cbc617f03e7a94913c4a3c83ef1a73987
Naufal Rivaldi <naufal@sdbagency.com>
2022-06-22 17:43:46 +0700

    Add isNumber on setting input addons

--- resources/js/ComponentStructures/icon.js
+++ resources/js/ComponentStructures/icon.js
@@ -43,6 +43,7 @@ export const config = {
                 component: "InputAddons",
                 settings: {
                     addons: 'px',
+                    isNumber: true,
                 },
             },
         }
