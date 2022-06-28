9c0d81937798efcc7d9db54c43ba9a8a2ad18d9c
Naufal Rivaldi <naufal@sdbagency.com>
2022-06-28 16:51:19 +0700

    set id directly to the carousel

--- themes/biz/views/components/builder/content/carousel.blade.php
+++ themes/biz/views/components/builder/content/carousel.blade.php
@@ -1,7 +1,6 @@
-<div @class($entity['id'])>
-    <carousel
-        :config="{{ Illuminate\Support\Js::from($config) }}"
-        :entity-images="{{ Illuminate\Support\Js::from($carouselImages) }}"
-        :slide-speed="{{ $slideSpeed }}"
-    />
-</div>
+<carousel
+    @class($entity['id'])
+    :config="{{ Illuminate\Support\Js::from($config) }}"
+    :entity-images="{{ Illuminate\Support\Js::from($carouselImages) }}"
+    :slide-speed="{{ $slideSpeed }}"
+/>
