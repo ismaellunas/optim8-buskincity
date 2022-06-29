04e15081f06872d1c8d25eeff9fded2f0723d1df
Naufal Rivaldi <naufal@sdbagency.com>
2022-06-29 16:42:12 +0700

    move unique class to outer div

--- themes/biz/views/components/builder/content/tabs.blade.php
+++ themes/biz/views/components/builder/content/tabs.blade.php
@@ -1,6 +1,5 @@
-<div class="container">
+<div @class($uniqueClass)>
     <tabs
-        @class($uniqueClass)
         :classes="{{ Illuminate\Support\Js::from($classes) }}"
         :content="{{ Illuminate\Support\Js::from($tabsContent) }}"
     />
