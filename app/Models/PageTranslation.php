d5c5e6db00d1113338d334f284af1af48bb9ffc7
Naufal Rivaldi <naufal@sdbagency.com>
2022-07-06 06:16:37 +0700

    Move method to model

--- app/Models/PageTranslation.php
+++ app/Models/PageTranslation.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use App\Casts\AsPageTranslationDataCollection;
+use App\Contracts\HasStyleInterface;
 use App\Contracts\PublishableInterface;
+use App\Helpers\MinifyCss;
+use App\Helpers\Url;
 use App\Models\Page;
+use App\Services\PageService;
 use App\Traits\HasLocale;
-use App\Traits\HasStyle;
 use CloudinaryLabs\CloudinaryLaravel\MediaAlly;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -16,7 +19,6 @@ class PageTranslation extends Model implements PublishableInterface
     use HasFactory;
     use HasLocale;
     use MediaAlly;
-    use HasStyle;
 
     private $uniqueKeyLength = 6;
 
@@ -70,5 +72,58 @@ class PageTranslation extends Model implements PublishableInterface
             "id" => $id,
             "uniqueKey" => $uniqueKey
         ];
+    }
+
+    public function generatePageStyle(): void
+    {
+        $css = "";
+
+        $styledComponents = $this->getStyledComponents(
+            $this->data->get('entities')
+        );
+
+        foreach ($styledComponents as $styleBlocks) {
+            foreach ($styleBlocks as $styleBlock) {
+                $css .= $styleBlock->toText();
+            }
+        }
+
+        $this->generated_style = MinifyCss::minify($css);
+        $this->save();
+    }
+
+    public function getStyleUrlAttribute()
+    {
+        $uid = $this->id.$this->unique_key;
+
+        return route('page.css', $uid);
+    }
+
+    private function getStyledComponents($entities): array
+    {
+        return collect($entities)
+            ->filter(function ($entity) {
+                $className = PageService::getEntityClassName($entity['componentName']);
+
+                if (class_exists($className)) {
+                    return in_array(
+                        HasStyleInterface::class,
+                        class_implements($className)
+                    );
+                }
+
+                return false;
+            })
+            ->map(function ($entity) {
+                $className = PageService::getEntityClassName($entity['componentName']);
+
+                $entity = new $className($entity);
+
+                return collect($entity->getStyleBlocks())
+                    ->filter(function ($styleBlock) {
+                        return !$styleBlock->isEmpty();
+                    });
+            })
+            ->all();
     }
 }
