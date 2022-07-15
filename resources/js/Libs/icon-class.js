28bd9d3932c7da6db5c4e86bc0d74da6a1326ed2
Bayu <boedy@sdbagency.com>
2022-07-15 18:30:31 +0700

    Update fontawesome package to 6 and implement icon-class library

---
+++ resources/js/Libs/icon-class.js
@@ -0,0 +1,17 @@
+export const add = 'fa-solid fa-eye';
+export const close = 'fa-solid fa-xmark';
+export const download = 'fa-solid fa-download';
+export const edit = 'fa-solid fa-pen';
+export const expand = 'fa-solid fa-expand';
+export const remove = 'fa-solid fa-trash-can';
+export const upload = 'fa-solid fa-upload';
+
+export default {
+    add,
+    close,
+    download,
+    edit,
+    expand,
+    remove,
+    upload,
+}
