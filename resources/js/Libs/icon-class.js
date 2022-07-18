0be0fbbf83eeacdff70e3df65be0d9cdffe0186f
Naufal Rivaldi <naufal@sdbagency.com>
2022-07-18 12:14:12 +0700

    Add another icon on icon class js

--- resources/js/Libs/icon-class.js
+++ resources/js/Libs/icon-class.js
@@ -1,17 +1,51 @@
-export const add = 'fa-solid fa-eye';
-export const close = 'fa-solid fa-xmark';
-export const download = 'fa-solid fa-download';
-export const edit = 'fa-solid fa-pen';
-export const expand = 'fa-solid fa-expand';
-export const remove = 'fa-solid fa-trash-can';
-export const upload = 'fa-solid fa-upload';
+export const iconType = 'fa-light';
+
+export const add = iconType + ' fa-plus';
+export const angleDown = iconType + ' fa-angle-down';
+export const back = iconType + ' fa-arrow-left';
+export const checkCircle = iconType + ' fa-check-circle';
+export const clear = iconType + ' fa-times';
+export const close = iconType + ' fa-xmark';
+export const copy = iconType + ' fa-copy';
+export const download = iconType + ' fa-download';
+export const edit = iconType + ' fa-pen';
+export const ellipsis = iconType + ' fa-ellipsis-h';
+export const eraser = iconType + ' fa-eraser';
+export const expand = iconType + ' fa-expand';
+export const idCard = iconType + ' fa-id-card';
+export const move = iconType + ' fa-arrows-alt';
+export const preview = iconType + ' fa-arrow-up-right-from-square';
+export const remove = iconType + ' fa-trash-can';
+export const show = iconType + ' fa-eye';
+export const signIn = iconType + ' fa-sign-in-alt';
+export const suspend = iconType + ' fa-ban';
+export const table = iconType + ' fa-th';
+export const tableList = iconType + ' fa-th-list';
+export const unsuspend = iconType + ' fa-hands-helping';
+export const upload = iconType + ' fa-upload';
 
 export default {
     add,
+    angleDown,
+    back,
+    checkCircle,
+    clear,
     close,
+    copy,
     download,
     edit,
+    ellipsis,
+    eraser,
     expand,
+    idCard,
+    move,
+    preview,
     remove,
+    show,
+    signIn,
+    suspend,
+    table,
+    tableList,
+    unsuspend,
     upload,
 }
