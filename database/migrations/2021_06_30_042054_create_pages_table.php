0ecbb93d5c9c1ea593ce0c72839b9c3e1dd3eb20
Bayu <boedy@sdbagency.com>
2022-07-20 12:31:03 +0700

    Add Page type and add Page entity in Space module

--- database/migrations/2021_06_30_042054_create_pages_table.php
+++ database/migrations/2021_06_30_042054_create_pages_table.php
@@ -15,6 +15,7 @@ class CreatePagesTable extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
+            $table->string('type')->nullable();
             $table
                 ->foreignId('author_id')
                 ->nullable()
