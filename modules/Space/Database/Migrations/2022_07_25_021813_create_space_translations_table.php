6c493361ab3eb899ee139c3a19fd9f11a9f449d9
Bayu <boedy@sdbagency.com>
2022-07-26 12:42:41 +0700

    Add locale column into space_translations

--- modules/Space/Database/Migrations/2022_07_25_021813_create_space_translations_table.php
+++ modules/Space/Database/Migrations/2022_07_25_021813_create_space_translations_table.php
@@ -15,14 +15,17 @@ class CreateSpaceTranslationsTable extends Migration
     {
         Schema::create('space_translations', function (Blueprint $table) {
             $table->id();
-            $table->text('excerpt');
-            $table->text('description');
+            $table->text('excerpt')->nullable();
+            $table->text('description')->nullable();
             $table->string('surface', 512)->nullable();
             $table->string('condition', 512)->nullable();
+            $table->string('locale')->index();
+
             $table->foreignId('space_id')
                 ->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
