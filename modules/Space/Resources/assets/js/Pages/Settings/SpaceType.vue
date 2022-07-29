c69c06d6755d336822feb96ee78dba88a6452337
Naufal Rivaldi <naufal@sdbagency.com>
2022-07-29 09:35:51 +0700

    Change space types setting to fullwidth

--- modules/Space/Resources/assets/js/Pages/Settings/SpaceType.vue
+++ modules/Space/Resources/assets/js/Pages/Settings/SpaceType.vue
@@ -1,87 +1,89 @@
 <template>
-    <div class="columns">
-        <div class="column">
-            <b>Space Types</b>
-        </div>
+    <div>
+        <form-section @submitted="updateProfileInformation">
+            <template #title>
+                Space Types
+            </template>
 
-        <div class="column">
-            <div class="columns">
-                <div class="column">
-                    <div class="is-pulled-left">
-                        <biz-filter-search
-                            v-model="term"
-                            @search="search"
-                        />
+            <template #form>
+                <div class="columns">
+                    <div class="column">
+                        <div class="is-pulled-left">
+                            <biz-filter-search
+                                v-model="term"
+                                @search="search"
+                            />
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="is-pulled-right">
+                            <biz-button
+                                type="button"
+                                class="is-primary"
+                                @click="openModalCreate()"
+                            >
+                                <span class="icon is-small">
+                                    <i :class="icon.add" />
+                                </span>
+                                <span>Add New</span>
+                            </biz-button>
+                        </div>
                     </div>
                 </div>
-                <div class="column">
-                    <div class="is-pulled-right">
-                        <biz-button
-                            type="button"
-                            class="is-primary"
-                            @click="openModalCreate()"
-                        >
-                            <span class="icon is-small">
-                                <i :class="icon.add" />
-                            </span>
-                            <span>Add New</span>
-                        </biz-button>
-                    </div>
+
+                <div class="table-container">
+                    <biz-table class="is-fullwidth">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>
+                                    <div class="level-right">
+                                        Actions
+                                    </div>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                                v-for="record in types.data"
+                                :key="record.id"
+                            >
+                                <td>{{ record.name }}</td>
+                                <td>
+                                    <div class="level-right">
+                                        <biz-button
+                                            type="button"
+                                            class="is-ghost has-text-black"
+                                            @click="openModalEdit(record)"
+                                        >
+                                            <span class="icon is-small">
+                                                <i :class="icon.edit" />
+                                            </span>
+                                        </biz-button>
+                                        <biz-button
+                                            type="button"
+                                            class="is-ghost has-text-black ml-1"
+                                            @click="onDelete(record)"
+                                        >
+                                            <span class="icon is-small">
+                                                <i :class="icon.remove" />
+                                            </span>
+                                        </biz-button>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </biz-table>
                 </div>
-            </div>
 
-            <div class="table-container">
-                <biz-table class="is-fullwidth">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>
-                                <div class="level-right">
-                                    Actions
-                                </div>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr
-                            v-for="record in types.data"
-                            :key="record.id"
-                        >
-                            <td>{{ record.name }}</td>
-                            <td>
-                                <div class="level-right">
-                                    <biz-button
-                                        type="button"
-                                        class="is-ghost has-text-black"
-                                        @click="openModalEdit(record)"
-                                    >
-                                        <span class="icon is-small">
-                                            <i :class="icon.edit" />
-                                        </span>
-                                    </biz-button>
-                                    <biz-button
-                                        type="button"
-                                        class="is-ghost has-text-black ml-1"
-                                        @click="onDelete(record)"
-                                    >
-                                        <span class="icon is-small">
-                                            <i :class="icon.remove" />
-                                        </span>
-                                    </biz-button>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </biz-table>
-            </div>
-
-            <biz-pagination
-                :is-ajax="true"
-                :links="types.links"
-                :query-params="queryParams"
-                @on-clicked-pagination="getSpaceTypes"
-            />
-        </div>
+                <biz-pagination
+                    :is-ajax="true"
+                    :links="types.links"
+                    :query-params="queryParams"
+                    @on-clicked-pagination="getSpaceTypes"
+                />
+            </template>
+        </form-section>
 
         <space-type-form-modal
             v-if="isModalOpen"
@@ -99,6 +101,7 @@
     import BizFilterSearch from '@/Biz/Filter/Search';
     import BizPagination from '@/Biz/Pagination';
     import BizTable from '@/Biz/Table';
+    import FormSection from '@/Frontend/FormSection';
     import icon from '@/Libs/icon-class';
     import SpaceTypeFormModal from './SpaceTypeFormModal';
     import { confirmDelete, oops as oopsAlert, success as successAlert } from '@/Libs/alert';
@@ -112,6 +115,7 @@
             BizFilterSearch,
             BizPagination,
             BizTable,
+            FormSection,
             SpaceTypeFormModal,
         },
 
