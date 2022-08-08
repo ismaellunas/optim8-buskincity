597a05ad5fbaa3e2916039cf129101844614888d
naufal <naufal@sdbagency.com>
2022-08-08 09:52:17 +0700

    Change key to type

--- modules/Space/Services/SpaceService.php
+++ modules/Space/Services/SpaceService.php
@@ -152,7 +152,7 @@ class SpaceService
 
     public function types(): Collection
     {
-        return app(GlobalOptionService::class)->getOptionByKey(
+        return app(GlobalOptionService::class)->getOptionByType(
             config('space.type_option')
         );
     }
