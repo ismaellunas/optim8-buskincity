0b8fd55621d91b1efd025719d1f401e8d1c15d98
naufal <naufal@sdbagency.com>
2022-08-18 11:21:00 +0700

    Add permission form builder

--- modules/FormBuilder/Database/Seeders/FormBuilderDatabaseSeeder.php
+++ modules/FormBuilder/Database/Seeders/FormBuilderDatabaseSeeder.php
@@ -16,6 +16,6 @@ class FormBuilderDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $this->call("Modules\FormBuilder\Database\Seeders\PermissionSeeder");
     }
 }
