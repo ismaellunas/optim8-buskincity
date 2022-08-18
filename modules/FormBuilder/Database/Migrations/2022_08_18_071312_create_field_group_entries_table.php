83151e7fd1bb8858b77acce4e91ad630f51ae33a
naufal <naufal@sdbagency.com>
2022-08-18 14:32:27 +0700

    Add migration for form builder table

---
+++ modules/FormBuilder/Database/Migrations/2022_08_18_071312_create_field_group_entries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('field_group_entries', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('field_group_id')
+                ->constrained('field_groups')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('field_group_entries');
+    }
+};
