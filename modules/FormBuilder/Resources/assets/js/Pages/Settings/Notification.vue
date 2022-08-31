41e7152bea37be35de5f4e95bda631415d5b8e90
naufal <naufal@sdbagency.com>
2022-08-31 09:06:10 +0700

    Create input lists component

--- modules/FormBuilder/Resources/assets/js/Pages/Settings/Notification.vue
+++ modules/FormBuilder/Resources/assets/js/Pages/Settings/Notification.vue
@@ -1,90 +1,86 @@
 <template>
-    <div>
-        <form-section>
-            <template #title>
-                Notification
-            </template>
+    <biz-input-list>
+        <template #title>
+            Notification
+        </template>
 
-            <template #form>
-                <div class="columns">
-                    <div class="column">
-                        <div class="is-pulled-left">
-                            <biz-filter-search
-                                v-model="term"
-                                @search="search"
-                            />
-                        </div>
-                    </div>
-                    <div class="column">
-                        <div class="is-pulled-right">
-                            <biz-button-link
-                                :href="route(baseRouteName + '.create', formBuilder.id)"
-                                class="is-primary"
-                            >
-                                <span class="icon is-small">
-                                    <i :class="icon.add" />
-                                </span>
-                                <span>Add New</span>
-                            </biz-button-link>
-                        </div>
-                    </div>
+        <div class="columns">
+            <div class="column">
+                <div class="is-pulled-left">
+                    <biz-filter-search
+                        v-model="term"
+                        @search="search"
+                    />
                 </div>
-
-                <div class="table-container">
-                    <biz-table class="is-fullwidth">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Subject</th>
-                                <th>
-                                    <div class="level-right">
-                                        Actions
-                                    </div>
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr
-                                v-for="record in records.data"
-                                :key="record.id"
-                            >
-                                <td>{{ record.name }}</td>
-                                <td>{{ record.subject }}</td>
-                                <td>
-                                    <div class="level-right">
-                                        <biz-button-link
-                                            href="#"
-                                            class="is-ghost has-text-black"
-                                        >
-                                            <span class="icon is-small">
-                                                <i :class="icon.edit" />
-                                            </span>
-                                        </biz-button-link>
-                                        <biz-button
-                                            type="button"
-                                            class="is-ghost has-text-black ml-1"
-                                            @click="onDelete(record)"
-                                        >
-                                            <span class="icon is-small">
-                                                <i :class="icon.remove" />
-                                            </span>
-                                        </biz-button>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </biz-table>
+            </div>
+            <div class="column">
+                <div class="is-pulled-right">
+                    <biz-button-link
+                        :href="route(baseRouteName + '.create', formBuilder.id)"
+                        class="is-primary"
+                    >
+                        <span class="icon is-small">
+                            <i :class="icon.add" />
+                        </span>
+                        <span>Add New</span>
+                    </biz-button-link>
                 </div>
+            </div>
+        </div>
 
-                <biz-pagination
-                    :is-ajax="true"
-                    :links="records.links"
-                    :query-params="queryParams"
-                    @on-clicked-pagination="getSettingNotifications"
-                />
-            </template>
-        </form-section>
-    </div>
+        <div class="table-container">
+            <biz-table class="is-fullwidth">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Subject</th>
+                        <th>
+                            <div class="level-right">
+                                Actions
+                            </div>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        v-for="record in records.data"
+                        :key="record.id"
+                    >
+                        <td>{{ record.name }}</td>
+                        <td>{{ record.subject }}</td>
+                        <td>
+                            <div class="level-right">
+                                <biz-button-link
+                                    href="#"
+                                    class="is-ghost has-text-black"
+                                >
+                                    <span class="icon is-small">
+                                        <i :class="icon.edit" />
+                                    </span>
+                                </biz-button-link>
+                                <biz-button
+                                    type="button"
+                                    class="is-ghost has-text-black ml-1"
+                                    @click="onDelete(record)"
+                                >
+                                    <span class="icon is-small">
+                                        <i :class="icon.remove" />
+                                    </span>
+                                </biz-button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </biz-table>
+        </div>
+
+        <biz-pagination
+            :is-ajax="true"
+            :links="records.links"
+            :query-params="queryParams"
+            @on-clicked-pagination="getSettingNotifications"
+        />
+    </biz-input-list>
 </template>
 
 <script>
@@ -92,9 +88,9 @@
     import BizButton from '@/Biz/Button';
     import BizButtonLink from '@/Biz/ButtonLink';
     import BizFilterSearch from '@/Biz/Filter/Search';
+    import BizInputList from '@/Biz/InputList';
     import BizPagination from '@/Biz/Pagination';
     import BizTable from '@/Biz/Table';
-    import FormSection from '@/Frontend/FormSection';
     import icon from '@/Libs/icon-class';
     import { confirmDelete, oops as oopsAlert, success as successAlert } from '@/Libs/alert';
     import { usePage } from '@inertiajs/inertia-vue3';
@@ -106,9 +102,9 @@
             BizButton,
             BizButtonLink,
             BizFilterSearch,
+            BizInputList,
             BizPagination,
             BizTable,
-            FormSection,
         },
 
         mixins: [
