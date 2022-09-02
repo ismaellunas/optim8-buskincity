7da8879defbaf04b1d92e46bdebdd428efb09a33
naufal <naufal@sdbagency.com>
2022-09-02 21:39:17 +0700

    Use meta on field group entry table

--- modules/FormBuilder/Entities/FieldGroupEntry.php
+++ modules/FormBuilder/Entities/FieldGroupEntry.php
@@ -4,8 +4,10 @@ namespace Modules\FormBuilder\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Kodeine\Metable\Metable;
 
 class FieldGroupEntry extends Model
 {
     use HasFactory;
+    use Metable;
 }
