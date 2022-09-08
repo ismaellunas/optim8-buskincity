0a7aee3c7a124bd0e3f40a05d67f131f3398baee
Bayu <boedy@sdbagency.com>
2022-09-08 16:48:04 +0700

    Secure and validate booking event using EventBookRequest

--- modules/Ecommerce/Http/Controllers/Frontend/OrderController.php
+++ modules/Ecommerce/Http/Controllers/Frontend/OrderController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Modules\Ecommerce\Entities\Order;
 use Modules\Ecommerce\Entities\Product;
+use Modules\Ecommerce\Http\Requests\EventBookRequest;
 use Modules\Ecommerce\Http\Requests\OrderRescheduleRequest;
 use Modules\Ecommerce\Services\EventService;
 use Modules\Ecommerce\Services\OrderService;
@@ -67,7 +68,7 @@ class OrderController extends CrudController
     {
     }
 
-    public function bookEvent(Request $request, Product $product)
+    public function bookEvent(EventBookRequest $request, Product $product)
     {
         $inputs = $request->all();
 
