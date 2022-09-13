40460789eed8abf4fb0896f8cd221f6075b60fa3
Bayu <boedy@sdbagency.com>
2022-09-13 16:42:04 +0700

    Test worker for schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,6 +29,8 @@ class Kernel extends ConsoleKernel
         if (config('telescope.enabled')) {
             $schedule->command('telescope:prune --hours=48')->daily();
         }
+
+        $schedule->command('command:print-test-info')->everyMinute();
     }
 
     /**
