96318532fe60e590049bb48479027d3ae15a0164
Bayu <boedy@sdbagency.com>
2022-10-04 11:20:32 +0700

    Assign space wildcard permissions into Administrator

--- modules/Space/Database/Seeders/PermissionSeeder.php
+++ modules/Space/Database/Seeders/PermissionSeeder.php
@@ -3,8 +3,10 @@
 namespace Modules\Space\Database\Seeders;
 
 use App\Models\Permission;
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 use Modules\Space\ModuleService;
 
 class PermissionSeeder extends Seeder
@@ -18,11 +20,20 @@ class PermissionSeeder extends Seeder
     {
         Model::unguard();
 
-        foreach (ModuleService::permissions() as $permission) {
+        $permissions = ModuleService::permissions();
+
+        foreach ($permissions as $permission) {
             Permission::create([
                 'name' => $permission,
                 'guard_name' => 'web',
             ]);
         }
+
+        $wildcardPermissions = $permissions
+            ->filter(fn ($permission) => Str::endsWith($permission, '.*'))
+            ->all();
+
+        $administratorRole = Role::findByName(config('permission.role_names.admin'));
+        $administratorRole->givePermissionTo($wildcardPermissions);
     }
 }
