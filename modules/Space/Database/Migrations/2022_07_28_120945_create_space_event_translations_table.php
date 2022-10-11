90fdabad5742f6c078803eb56ccb32b9c0b357ed
Bayu <boedy@sdbagency.com>
2022-10-11 13:44:50 +0700

    Remove Event module and migrate the entities, factory, migrations to Space module

--- modules/Space/Database/Migrations/2022_07_28_120945_create_space_event_translations_table.php
+++ modules/Space/Database/Migrations/2022_07_28_120945_create_space_event_translations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEventTranslationsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateEventTranslationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('event_translations', function (Blueprint $table) {
+        Schema::create('space_event_translations', function (Blueprint $table) {
             $table->id();
             $table->text('excerpt')->nullable();
             $table->text('description')->nullable();
             $table->string('locale', 15)->index();
 
-            $table->foreignId('event_id')
+            $table->foreignId('space_event_id')
                 ->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
@@ -37,4 +37,4 @@ class CreateEventTranslationsTable extends Migration
     {
         Schema::dropIfExists('event_translations');
     }
-}
+};
