6584dbc6c7eb9a90255a744ba4484ab5e4a0cc3b
naufal <naufal@sdbagency.com>
2022-10-19 10:21:51 +0700

    Fix condition to set unique class and background class when using section or columns

--- themes/biz/views/components/builder/columns.blade.php
+++ themes/biz/views/components/builder/columns.blade.php
@@ -1,10 +1,21 @@
-<div @class($uniqueClass)>
-    <div class="columns">
-        @foreach ($columns as $column)
-            <x-builder.column
-                :uid="$column['id']"
-                :components="$column['components']"
-            />
-        @endforeach
-    </div>
+@aware([
+    'backgroundColor' => '',
+    'isSectionIncluded',
+    'uniqueClass',
+    'hasBackgroundImage',
+])
+
+<div @class([
+        'columns',
+        'pb-background-image' => ($hasBackgroundImage && !$isSectionIncluded),
+        $uniqueClass,
+        $uniqueClass.'-background' => ($hasBackgroundImage && !$isSectionIncluded),
+        $backgroundColor => !$isSectionIncluded
+    ])>
+    @foreach ($columns as $column)
+        <x-builder.column
+            :uid="$column['id']"
+            :components="$column['components']"
+        />
+    @endforeach
 </div>
