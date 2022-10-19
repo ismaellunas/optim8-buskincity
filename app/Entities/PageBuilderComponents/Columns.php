7eefc1288467cf0f0f951d0ce8acdac86f4874af
naufal <naufal@sdbagency.com>
2022-10-19 10:21:08 +0700

    Update generate style for columns component to compose background image style

--- app/Entities/PageBuilderComponents/Columns.php
+++ app/Entities/PageBuilderComponents/Columns.php
@@ -3,6 +3,8 @@
 namespace App\Entities\PageBuilderComponents;
 
 use App\Contracts\PageBuilderDimensionInterface;
+use App\Entities\StyleBlock;
+use App\Models\Media;
 use App\Traits\PageBuilderDimension;
 
 class Columns extends BaseComponent implements
@@ -12,10 +14,43 @@ class Columns extends BaseComponent implements
 
     protected function composeStyleBlocks(): void
     {
+        $styleBlock = null;
+
         if ($this->doesConfigHaveDimension()) {
             $this->styleBlocks[] = $this->getDimensionStyleBlock(
                 $this->getSelector()
             );
         }
+
+        if ($this->doesWrapperHaveBackgroundImage()) {
+            $this->styleBlocks[] = $this->getBackgroundStyleBlock(
+                $this->getSelector().'-background'
+            );
+        }
+    }
+
+    private function doesWrapperHaveBackgroundImage(): bool
+    {
+        return !!$this->getConfig()['wrapper']['backgroundImage'] ?? false;
+    }
+
+    private function getBackgroundStyleBlock(string $rootSelector): StyleBlock
+    {
+        $styleBlock = new StyleBlock($rootSelector);
+
+        $backgroundUrl = $this->getMediaUrl(
+            $this->getConfig()['wrapper']['backgroundImage']
+        );
+
+        if ($backgroundUrl) {
+            $styleBlock->addStyle('background-image', 'url('.$backgroundUrl.')');
+        }
+
+        return $styleBlock;
+    }
+
+    private function getMediaUrl(int $mediaId): ?string
+    {
+        return Media::find($mediaId)->file_url ?? null;
     }
 }
