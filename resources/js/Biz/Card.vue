c1112a6289cea523ac88427d4c4fdf86746cab62
naufal <naufal@sdbagency.com>
2022-10-31 10:05:47 +0700

    Update card component can collapse

--- resources/js/Biz/Card.vue
+++ resources/js/Biz/Card.vue
@@ -4,7 +4,11 @@
             v-if="$slots.headerTitle || $slots.headerButton"
             class="card-header"
         >
-            <p class="card-header-title">
+            <p
+                class="card-header-title"
+                :style="cardHeaderStyle"
+                @click="onClickHeaderButton()"
+            >
                 <slot name="headerTitle" />
             </p>
 
@@ -13,20 +17,97 @@
                 class="card-header-icon"
                 aria-label="more options"
                 type="button"
+                @click="onClickHeaderButton()"
             >
                 <slot name="headerButton" />
             </button>
+
+            <button
+                v-else-if="isCollapses"
+                class="card-header-icon"
+                aria-label="more options"
+                type="button"
+                @click="onClickHeaderButton()"
+            >
+                <biz-icon :icon="iconCollapse" />
+            </button>
         </header>
 
-        <div class="card-content">
+        <div
+            v-if="isContentShown"
+            class="card-content"
+        >
             <slot />
         </div>
 
         <footer
-            v-if="$slots.footer"
+            v-if="$slots.footer && isContentShown"
             class="card-footer"
         >
             <slot name="footer" />
         </footer>
     </div>
 </template>
+
+<script>
+    import BizIcon from '@/Biz/Icon';
+    import icon from '@/Libs/icon-class';
+    import { ref } from 'vue';
+
+    export default {
+        name: 'BizCard',
+
+        components: {
+            BizIcon,
+        },
+
+        props: {
+            iconCloseCollapse: { type: String, default: null },
+            iconOpenCollapse: { type: String, default: null },
+            isCollapses: { type: Boolean, default: false },
+            openCollapseOnLoad: { type: Boolean, default: false },
+        },
+
+        setup(props) {
+            let isContentShown = props.isCollapses ? false : true;
+
+            if (!isContentShown && props.openCollapseOnLoad) {
+                isContentShown = true;
+            }
+
+            return {
+                isContentShown: ref(isContentShown),
+            };
+        },
+
+        data () {
+            return {
+                icon,
+            };
+        },
+
+        computed: {
+            iconCollapse() {
+                if (!this.isContentShown) {
+                    return this.iconOpenCollapse ?? icon.angleDown;
+                }
+
+                return this.iconCloseCollapse ?? icon.angleUp;
+            },
+
+            cardHeaderStyle() {
+                return {
+                    cursor: this.isCollapses ? 'pointer' : 'auto',
+                };
+            },
+        },
+
+        methods: {
+            onClickHeaderButton() {
+                if (this.isCollapses) {
+                    this.isContentShown = !this.isContentShown
+                }
+            },
+        },
+    }
+</script>
