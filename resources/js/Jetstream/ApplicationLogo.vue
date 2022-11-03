9597082f8509d4a18b40df91614cecf6b3eb7f13
naufal <naufal@sdbagency.com>
2022-11-03 11:22:09 +0700

    Remove unused component

--- resources/js/Jetstream/ApplicationLogo.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <img
-        src=""
-        alt=""
-    >
-</template>
--- resources/js/Jetstream/ApplicationMark.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <img
-        src=""
-        alt=""
-        height="28"
-    >
-</template>
