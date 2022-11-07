2783a77c3b6b52862970ae961a43bc156a2c8507
naufal <naufal@sdbagency.com>
2022-11-07 09:49:12 +0700

    Add custom id on frontend

--- app/View/Components/Builder/Row.php
+++ app/View/Components/Builder/Row.php
@@ -10,6 +10,7 @@ class Row extends Component
     public $uid;
     public $backgroundColor;
     public $columns;
+    public $customId;
     public $entities;
     public $hasBackgroundImage;
     public $images;
@@ -37,9 +38,10 @@ class Row extends Component
 
         $configWrapper = $this->config['wrapper'] ?? null;
         if ($configWrapper) {
-            $this->backgroundColor = $this->config['wrapper']['backgroundColor'] ?? '';
-            $this->isFullwidth = $this->config['wrapper']['isFullwidth'] ?? false;
-            $this->rounded = $this->config['wrapper']['rounded'] ?? null;
+            $this->customId = $configWrapper['customId'] ?? null;
+            $this->backgroundColor = $configWrapper['backgroundColor'] ?? '';
+            $this->isFullwidth = $configWrapper['isFullwidth'] ?? false;
+            $this->rounded = $configWrapper['rounded'] ?? null;
         }
 
         if ($this->isSectionIncluded) {
