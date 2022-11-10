d9819a29ba948da56efd9680b8e3d821ac3b563d
naufal <naufal@sdbagency.com>
2022-11-10 21:22:39 +0700

    Add biz slider and biz form slider component

---
+++ resources/js/Biz/Slider.vue
@@ -0,0 +1,31 @@
+<template>
+    <vue-slider
+        v-model="computedValue"
+        :disabled="disabled"
+    />
+</template>
+
+<script>
+    import VueSlider from 'vue-slider-component';
+    import 'vue-slider-component/theme/default.css';
+    import { useModelWrapper } from '@/Libs/utils';
+
+    export default {
+        name: 'BizSlider',
+
+        components: {
+            VueSlider
+        },
+
+        props: {
+            modelValue: { type: [Number, String, null], required: true },
+            disabled: { type: Boolean, default: false },
+        },
+
+        setup(props, {emit}) {
+            return {
+                computedValue: useModelWrapper(props, emit),
+            };
+        },
+    }
+</script>
