b03f3522753cd09966015c94cb7f554cc710c762
Naufal <naufal@sdbagency.com>
2022-11-14 13:46:05 +0700

    Move trait code to base component class and fix calculation for mobile spacing style

--- app/Entities/PageBuilderComponents/BaseComponent.php
+++ app/Entities/PageBuilderComponents/BaseComponent.php
@@ -3,14 +3,20 @@
 namespace App\Entities\PageBuilderComponents;
 
 use App\Contracts\HasStyleInterface;
+use App\Contracts\PageBuilderDimensionInterface;
 use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
+use App\Entities\StyleBlock;
 
-abstract class BaseComponent implements HasStyleInterface
+abstract class BaseComponent implements
+    HasStyleInterface,
+    PageBuilderDimensionInterface
 {
     protected $data;
     protected $selector;
     protected $styleBlocks = [];
     protected $mobileStyleBlocks = [];
+    protected $defaultDimensionValue = 12;
 
     public function __construct($data)
     {
@@ -33,10 +39,22 @@ abstract class BaseComponent implements HasStyleInterface
     }
 
     protected function composeStyleBlocks(): void
-    {}
+    {
+        if ($this->doesConfigHaveDimension()) {
+            $this->styleBlocks[] = $this->getDimensionStyleBlock(
+                $this->getSelector()
+            );
+        }
+    }
 
     protected function composeMobileStyleBlocks(): void
-    {}
+    {
+        if ($this->doesConfigHaveDimension()) {
+            $this->mobileStyleBlocks[] = $this->getMobileDimensionStyleBlock(
+                $this->getSelector()
+            );
+        }
+    }
 
     public function getStyleBlocks(): array
     {
@@ -50,5 +68,94 @@ abstract class BaseComponent implements HasStyleInterface
         $this->composeMobileStyleBlocks();
 
         return $this->mobileStyleBlocks;
+    }
+
+    protected function doesConfigHaveDimension(): bool
+    {
+        return !empty($this->getConfig()['dimension']);
+    }
+
+    protected function getDimensionConfig(): array
+    {
+        return $this->getConfig()['dimension'];
+    }
+
+    public function getDimensionStyleBlock(
+        string $rootSelector
+    ): StyleBlock {
+        $styleBlock = new StyleBlock($rootSelector);
+
+        $this->getSpaceDimensionConfig('margin')
+            ->each(function ($value, $key) use ($styleBlock) {
+                $unit = $this->getUnitSpaceDimension('margin');
+
+                $styleBlock->addStyle('margin-'.$key, $value.$unit);
+            });
+
+        $this->getSpaceDimensionConfig('padding')
+            ->each(function ($value, $key) use ($styleBlock) {
+                $unit = $this->getUnitSpaceDimension('padding');
+
+                $styleBlock->addStyle('margin-'.$key, $value.$unit);
+            });
+
+        return $styleBlock;
+    }
+
+    public function getMobileDimensionStyleBlock(
+        string $rootSelector
+    ): StyleBlock {
+        $styleBlock = new StyleBlock($rootSelector);
+
+        $this->getSpaceDimensionConfig('margin')
+            ->each(function ($value, $key) use ($styleBlock) {
+                $unit = $this->getUnitSpaceDimension('margin');
+                $value = $this->calculateDimensionValue($key, $value);
+
+                $styleBlock->addStyle('margin-'.$key, $value.$unit);
+            });
+
+        $this->getSpaceDimensionConfig('padding')
+            ->each(function ($value, $key) use ($styleBlock) {
+                $unit = $this->getUnitSpaceDimension('padding');
+                $value = $this->calculateDimensionValue($key, $value);
+
+                $styleBlock->addStyle('margin-'.$key, $value.$unit);
+            });
+
+        return $styleBlock;
+    }
+
+    protected function calculateDimensionValue(string $key, mixed $value = null)
+    {
+        if (($key == 'top' || $key == 'bottom') && $value) {
+            $value = (int)$value / 2;
+
+            return ($value >= 12) ? $value : $this->defaultDimensionValue;
+        }
+
+        return $this->defaultDimensionValue;
+    }
+
+    private function getSpaceDimensionConfig(string $spaceType): Collection
+    {
+        $styleConfig = $this->getDimensionConfig();
+
+        if (!empty($styleConfig['style.'.$spaceType])) {
+
+            return collect($styleConfig['style.'.$spaceType])
+                ->except(['unit'])
+                ->filter(fn($value) => ($value == "0" || !empty($value)));
+        }
+
+        return collect();
+    }
+
+    private function getUnitSpaceDimension(string $spaceType): string
+    {
+        $styleConfig = $this->getDimensionConfig();
+
+        return $styleConfig['style.'.$spaceType]['unit']
+                ?? 'px';
     }
 }
