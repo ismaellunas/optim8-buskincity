7be039a6650331db0643eec9ec98ec4eb04ad5a3
Bayu <boedy@sdbagency.com>
2022-11-22 19:02:59 +0700

    Override viewAny() and view() in OrderPolicy

--- modules/Ecommerce/Policies/OrderPolicy.php
+++ modules/Ecommerce/Policies/OrderPolicy.php
@@ -2,12 +2,46 @@
 
 namespace Modules\Ecommerce\Policies;
 
+use App\Models\User;
 use App\Policies\BasePermissionPolicy;
+use App\Services\ModuleService;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Traits\Macroable;
 
 class OrderPolicy extends BasePermissionPolicy
 {
     use Macroable;
 
+    private bool $isModuleSpaceActive;
+
     protected $basePermission = 'order';
+
+    public function __construct()
+    {
+        $this->isEcommerceModuleActive = app(ModuleService::class)->isModuleActive('ecommerce');
+    }
+
+    public function viewAny(User $user)
+    {
+        if (!$this->isEcommerceModuleActive) {
+            return false;
+        }
+
+        return (
+            parent::viewAny($user)
+            || auth()->check()
+        );
+    }
+
+    public function view(User $user, Model $order)
+    {
+        if (!$this->isEcommerceModuleActive) {
+            return false;
+        }
+
+        return (
+            parent::view($user, $order)
+            || $order->isPlacedByUser($user)
+        );
+    }
 }
