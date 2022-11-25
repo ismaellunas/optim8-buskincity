df3a210af2c408398a70a450dbbc2020f657c023
Naufal <naufal@sdbagency.com>
2022-11-25 15:00:56 +0700

    Update logic to show driver login

--- themes/buskincity/views/auth/login.blade.php
+++ themes/buskincity/views/auth/login.blade.php
@@ -35,20 +35,26 @@
                     <h1 class="title is-2 mb-4">Log In</h1>
                     <p>Please log in to continue.</p>
 
-                    @foreach ($loginService->getAvailableSocialiteDrivers() as $driver)
-                        <a href="{{ route('oauth.redirect', $driver) }}" @class([
-                            "button is-medium is-light is-fullwidth",
-                            "mt-6" => $loop->first,
-                            "mt-4" => !$loop->first,
-                        ])>
-                            <span class="icon is-small">
-                                <i class="fa-brands fa-{{ $driver }}"></i>
-                            </span>
-                            <span>Continue with <span class="has-text-weight-bold">{{ Str::title($driver) }}</span></span>
-                        </a>
-                    @endforeach
+                    @php
+                        $availableSocialiteDrivers = $loginService->getAvailableSocialiteDrivers();
+                    @endphp
 
-                    <div class="is-divider mt-6 mb-6 ml-5 mr-6" data-content="OR"></div>
+                    @if (!empty($availableSocialiteDrivers))
+                        @foreach ($availableSocialiteDrivers as $driver)
+                            <a href="{{ route('oauth.redirect', $driver) }}" @class([
+                                "button is-medium is-light is-fullwidth",
+                                "mt-6" => $loop->first,
+                                "mt-4" => !$loop->first,
+                            ])>
+                                <span class="icon is-small">
+                                    <i class="fa-brands fa-{{ $driver }}"></i>
+                                </span>
+                                <span>Continue with <span class="has-text-weight-bold">{{ Str::title($driver) }}</span></span>
+                            </a>
+                        @endforeach
+
+                        <div class="is-divider mt-6 mb-6 ml-5 mr-6" data-content="OR"></div>
+                    @endif
 
                     <a class="button is-medium is-light is-fullwidth mt-4" onclick="showForm()">
                         <span class="icon is-small">
