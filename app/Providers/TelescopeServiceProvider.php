57359112c29284cec819eb820fc10c9e7abc7228
Bayu <bayu@biz752.com>
2022-12-12 17:07:34 +0700

    Install Laravel telescope for non local environment

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Str;
 use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
@@ -29,8 +30,19 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
                    $entry->isFailedRequest() ||
                    $entry->isFailedJob() ||
                    $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag();
+                   $entry->hasMonitoredTag() ||
+                   $this->isBackendRequest($entry);
         });
+    }
+
+    private function isBackendRequest(IncomingEntry $entry): bool
+    {
+        return (
+            $entry->type == 'request' &&
+            Str::startsWith($entry->content['uri'], '/admin/') &&
+            !Str::endsWith($entry->content['uri'], '.js.map') &&
+            !Str::startsWith($entry->content['uri'], '/admin/system-log')
+        );
     }
 
     /**
@@ -63,9 +75,11 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            if ($this->app->environment('local')) {
+                return true;
+            }
+
+            return $user->can('system.log');
         });
     }
 }
