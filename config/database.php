f070cfd48ab363c5e40716452d327f52bb84c7ff
Bayu <bayu@biz752.com>
2022-12-13 17:34:33 +0700

    Update redis.options.context.stream[verify_peer and verify_peer_name]

--- config/database.php
+++ config/database.php
@@ -139,6 +139,12 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'context' => [
+                'stream' => [
+                    'verify_peer' => 0,
+                    'verify_peer_name' => 0,
+                ]
+            ]
         ],
 
         'default' => [
