982a196286d7e31c023c160e32f3396f1b93b87a
Naufal <naufal@sdbagency.com>
2022-12-20 17:31:50 +0700

    Add error message for recaptcha

--- themes/buskincity/views/auth/forgot_password.blade.php
+++ themes/buskincity/views/auth/forgot_password.blade.php
@@ -69,8 +69,16 @@
                                     class="g-recaptcha"
                                     data-sitekey="{{ $recaptchaSiteKey }}"
                                     data-size="invisible"
+                                    data-error-callback="recaptchaError"
                                 ></div>
                             @endif
+
+                            <span
+                                id="recaptcha-error-message"
+                                class="help has-text-danger is-hidden"
+                            >
+                                Please check the reCAPTCHA!
+                            </span>
 
                             <button type="submit" class="button is-medium is-primary is-fullwidth">
                                 <span class="has-text-weight-bold">{{ __('Send Reset Link')}}</span>
