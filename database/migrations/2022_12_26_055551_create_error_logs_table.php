0daebd26f9534c5ff5cd9d4e71645227dba071bc
Naufal <naufal@sdbagency.com>
2023-01-02 15:07:53 +0700

    Add error log table and model

---
+++ database/migrations/2022_12_26_055551_create_error_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('error_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->string('file')->nullable();
+            $table->integer('line')->nullable();
+            $table->text('message')->nullable();
+            $table->text('trace')->nullable();
+            $table->integer('total_hit')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('error_logs');
+    }
+};
