75e8aff7a175345f63009189cdf176d70cbaacfb
Bayu <bayu@biz752.com>
2023-01-05 14:39:05 +0700

    Provide Events Calendar component on frontend

---
+++ modules/Booking/Resources/assets/js/PageBuilderComponents/EventsCalendar.vue
@@ -0,0 +1,373 @@
+<template>
+    <div class="events-calendar">
+        <div class="columns">
+            <div class="column">
+                <div class="columns is-multiline">
+                    <div class="column">
+                        <div class="control has-icons-left">
+                            <biz-select
+                                v-model="selectedLocation"
+                                placeholder="Any"
+                            >
+                                <option
+                                    v-for="location in locationOptions"
+                                    :key="location.id"
+                                    :value="location.id"
+                                >
+                                    {{ location.value }}
+                                </option>
+                            </biz-select>
+
+                            <biz-icon
+                                class="is-small is-left"
+                                :icon="icon.globe"
+                            />
+                        </div>
+                    </div>
+
+                    <div class="column is-5">
+                        <biz-filter-date-range
+                            v-model="queryParams.dates"
+                            input-class-name="input"
+                            max-range="7"
+                            :max-date="maxDate"
+                            :min-date="minDate"
+                            :year-range="yearRange"
+                            :clearable="false"
+                        />
+                    </div>
+
+                    <div class="column is-3">
+                        <biz-button
+                            class="is-primary"
+                            type="button"
+                            @click.prevent="getEvents(null, $event)"
+                        >
+                            Search Events
+                        </biz-button>
+                    </div>
+                </div>
+
+                <div
+                    v-if="events.data"
+                    class="block"
+                >
+                    <article
+                        v-for="record in events.data"
+                        :key="record.id"
+                        class="media"
+                    >
+                        <figure class="media-left">
+                            <p class="image is-64x64">
+                                <img :src="record.user?.profile_photo_url ?? userImage">
+                            </p>
+                        </figure>
+                        <div class="media-content">
+                            <div class="content">
+                                <p class="has-text-justified">
+                                    <template v-if="record.user.stage_name">
+                                        <strong>{{ record.user.stage_name }}</strong> - <small>{{ record.user.name }}</small>
+                                    </template>
+                                    <template v-else>
+                                        <strong>{{ record.user.name }}</strong>
+                                    </template>
+                                    <br>
+                                    <biz-icon :icon="icon.locationMark" />
+                                    {{ record.location.address }}
+                                    <br>
+                                    <biz-icon :icon="bookingIcon.calendar" />
+                                    {{ record.event.date }}, {{ record.event.start_end_time }}, {{ record.event.timezone }}
+                                    <br>
+                                    <biz-icon :icon="bookingIcon.duration" />
+                                    {{ record.event.duration }}
+                                </p>
+                            </div>
+                            <div class="buttons">
+                                <a
+                                    v-if="record.user.profile_page_url"
+                                    class="button my-0"
+                                    target="_blank"
+                                    :href="record.direction_url"
+                                >
+                                    Direction
+                                </a>
+
+                                <a
+                                    v-if="record.user.profile_page_url"
+                                    class="button is-primary my-0"
+                                    target="_blank"
+                                    :href="record.user.profile_page_url"
+                                >
+                                    Performer Detail
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+
+                <div v-else>
+                    No Event
+                </div>
+
+                <biz-pagination
+                    class="mt-6"
+                    :is-ajax="true"
+                    :links="events.links"
+                    :query-params="queryParams"
+                    @on-clicked-pagination="getEvents"
+                />
+            </div>
+
+            <div class="column">
+                <div
+                    ref="mapDiv"
+                    :style="mapStyle"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import MixinHasLoader from '@/Mixins/HasLoader';
+    import BizButton from '@/Biz/Button';
+    import BizFilterDateRange from '@/Biz/Filter/DateRange';
+    import BizIcon from '@/Biz/Icon';
+    import BizPagination from '@/Biz/Pagination';
+    import BizSelect from '@/Biz/Select';
+    import bookingIcon from '@mod/Booking/Resources/assets/js/Libs/booking-icon';
+    import icon from '@/Libs/icon-class';
+    import moment from 'moment';
+    import { Loader } from '@googlemaps/js-api-loader';
+    import { MarkerClusterer } from "@googlemaps/markerclusterer";
+    import { clone, each, find, keys, get, merge } from 'lodash';
+    import { computed, onMounted, onUnmounted, reactive, ref, toRaw } from 'vue';
+    import { useGeolocation, mapStyle as drawMapStyle } from '@/Libs/map';
+    import { useModelWrapper, isBlank } from '@/Libs/utils';
+    import { userImage } from '@/Libs/defaults';
+
+    export default {
+        name: 'EventsCalendar',
+
+        components: {
+            BizButton,
+            BizIcon,
+            BizSelect,
+            BizPagination,
+            BizFilterDateRange,
+        },
+
+        mixins: [
+            MixinHasLoader,
+        ],
+
+        props: {
+            apiKey: { type: String, default: null },
+            initPosition: { type: Object, default: null },
+            isDraggable: { type: Boolean, default: true },
+            mapStyle: { type: [String, Array, Object], default: () => ["width: 100%", "height: 95vh"] },
+            maxDate: { type: String, required: true },
+            minDate: { type: String, required: true },
+            pageQueryParams: { type: Object, default: () => {} },
+            urls : { type: Object, required: true },
+            userCity: { type: String, required: true },
+            userCountryCode: { type: String, required: true },
+            yearRange: { type: Array, required: true },
+        },
+
+        setup(props) {
+            let availableLocations = ref({});
+            let events = ref({});
+            let initPos = null;
+            let map = ref(null);
+            let mapData = ref(null);
+            let mapDiv = ref(null);
+            let selectedLocation = ref(null);
+
+            if (!isBlank(props.initPosition)) {
+
+                initPos = {
+                    lat: props.initPosition.latitude,
+                    lng: props.initPosition.longitude,
+                };
+
+            } else {
+
+                const { coords } = useGeolocation();
+
+                initPos = {
+                    lat: coords.value.latitude,
+                    lng: coords.value.longitude,
+                };
+            }
+
+            const queryParams = merge(
+                {dates: [
+                    moment().format('YYYY-MM-DD'),
+                    moment().add(6, 'day').format('YYYY-MM-DD'),
+                ]},
+                props.pageQueryParams
+            );
+
+            return {
+                availableLocations,
+                bookingIcon,
+                dateRange: clone(queryParams.dates),
+                events,
+                icon,
+                mapData,
+                mapDiv,
+                queryParams: ref(queryParams),
+                selectedLocation,
+                userImage,
+            };
+        },
+
+        data() {
+            return {
+                isShown: true,
+                mapLoader: null,
+                markerClusterer: null,
+                map: null,
+                markers: [],
+            };
+        },
+
+        computed: {
+            locationOptions() {
+                const options = [];
+
+                each(this.availableLocations, (location, key) => {
+                    options.push({
+                        id: key,
+                        value: location.country,
+                    });
+
+                    each(location.cities, (city) => {
+                        options.push({
+                            id: key +'-'+ city,
+                            value: ' - '+ city,
+                        });
+                    });
+                });
+
+                return options;
+            },
+
+            locationParts() {
+                const countryCity = {
+                    country: null,
+                    city: null,
+                };
+
+                if (!this.selectedLocation) {
+                    return countryCity;
+                }
+
+                const locationParts = this.selectedLocation.split('-');
+
+                return {
+                    country: locationParts[0],
+                    city: locationParts[1],
+                };
+            },
+        },
+
+        async mounted() {
+            this.mapLoader = new Loader({
+                apiKey: this.apiKey,
+                version: "weekly",
+                libraries: ["geometry", "drawing", "places"],
+            });
+
+            await this.mapLoader.load();
+
+            this.map = new google.maps.Map(this.mapDiv, {
+                center: {
+                    lat: this.initPosition.latitude,
+                    lng: this.initPosition.longitude,
+                },
+                zoom: 4,
+                draggable: this.isDraggable,
+                streetViewControl: false,
+                styles: drawMapStyle,
+                maxZoom: 17,
+                minZoom: 3,
+            });
+
+            this.markerClusterer= new MarkerClusterer({
+                map: this.map,
+            });
+
+            this.getLocationOptions((results) => {
+                if (keys(results).includes(this.userCountryCode)) {
+                    this.selectedLocation = this.userCountryCode;
+
+                    const foundedCity = find(
+                        get(results, this.userCountryCode+'.cities', []),
+                        (city) => city == this.userCity
+                    );
+
+                    if (foundedCity) {
+                        this.selectedLocation += "-" + foundedCity;
+                    }
+                }
+
+                this.getEvents();
+            });
+        },
+
+        methods: {
+            getEvents(url) {
+                let currentUrl = url ?? this.urls.getEvents;
+
+                this.queryParams.country = this.locationParts.country;
+                this.queryParams.city = this.locationParts.city;
+
+                this.onStartLoadingOverlay();
+
+                axios
+                    .get(currentUrl, {params: this.queryParams})
+                    .then((response) => {
+                        this.events = response.data.pagination;
+                        this.mapData = response.data.map;
+
+                        this.markerClusterer.clearMarkers(true);
+
+                        this.map.setZoom(response.data.map.zoom);
+
+                        this.map.setCenter({
+                            lat: parseFloat(response.data.map.center.lat),
+                            lng: parseFloat(response.data.map.center.long),
+                        })
+
+                        this.markers = response.data.pagination.data.map((event) => {
+                            return new google.maps.Marker({
+                                position: {
+                                    lat: event.location.latitude,
+                                    lng: event.location.longitude
+                                },
+                            });
+                        });
+
+                        this.markerClusterer.addMarkers(this.markers, true);
+                    })
+                    .then(() => {
+                        this.onEndLoadingOverlay();
+                    });
+            },
+
+            getLocationOptions(after) {
+                axios
+                    .get(this.urls.getLocationOptions)
+                    .then((response) => {
+                        this.availableLocations = response.data;
+
+                        if (after) {
+                            after(this.availableLocations);
+                        }
+                    });
+            },
+        },
+    };
+</script>
