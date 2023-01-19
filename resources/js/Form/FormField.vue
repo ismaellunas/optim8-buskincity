4f8ad7c0eee7f530b26cebb70b05528816a2a377
Naufal <naufal@sdbagency.com>
2023-01-19 16:17:06 +0700

    Fix custom field layout on frontend

---
+++ resources/js/Form/FormField.vue
@@ -0,0 +1,162 @@
+<template>
+    <form
+        v-if="form"
+        :key="formFieldKey"
+        @submit.prevent="submit"
+    >
+        <field-group
+            v-for="(group, index) in formField.fieldGroups"
+            :key="index"
+            :ref="'field_group__'+index"
+            v-model="form"
+            :group="group"
+            :selected-locale="selectedLocale"
+        />
+
+        <slot name="buttons">
+            <div class="field">
+                <biz-button class="is-medium is-primary">
+                    <span class="has-text-weight-bold">
+                        {{ buttonText }}
+                    </span>
+                </biz-button>
+            </div>
+        </slot>
+    </form>
+</template>
+
+<script>
+    import MixinHasLoader from '@/Mixins/HasLoader';
+    import BizButton from '@/Biz/Button';
+    import FieldGroup from './FieldGroup';
+    import { useForm, usePage } from '@inertiajs/inertia-vue3';
+    import { isEmpty, forOwn, forEach } from 'lodash';
+    import { success as successAlert, oops as oopsAlert } from '@/Libs/alert';
+
+    export default {
+        name: 'FormField',
+
+        components: {
+            BizButton,
+            FieldGroup,
+        },
+
+        mixins: [
+            MixinHasLoader,
+        ],
+
+        props: {
+            entityId: {type: Number, default: null},
+            formField: { type: Object, required: true },
+            localeOptions: { type: Object, default: () => {} },
+            routeName: { type: String, required: true },
+            routeSave: { type: String, default: 'forms.save' },
+            selectedLocale: { type: String, required: true },
+        },
+
+        emits: [
+            'on-success-submit'
+        ],
+
+        data() {
+            return {
+                form: null,
+                formFieldKey: 0,
+            };
+        },
+
+        computed: {
+            buttonText() {
+                return this.formField?.button?.text ?? 'Submit';
+            },
+        },
+
+        mounted() {
+            this.form = this.createForm();
+        },
+
+        methods: {
+            createForm() {
+                let fieldValue = null;
+                const form = {
+                    id: this.entityId,
+                    _token: usePage().props.value.csrfToken,
+                };
+
+                forOwn(this.formField.fieldGroups, (groupField, key) => {
+
+                    if (!isEmpty(groupField.fields)) {
+
+                        forOwn(groupField.fields, (field, key) => {
+                            if (typeof field.value === 'undefined') {
+                                form[ key ] = undefined;
+                            } else {
+                                form[ key ] = field.value;
+
+                                if (field.is_translated && field.value.length == 0) {
+                                    fieldValue = {};
+
+                                    this.localeOptions.forEach(function(locale) {
+                                        fieldValue[ locale.id ] = null
+                                    })
+
+                                    form[ key ] = fieldValue;
+                                }
+                            }
+                        });
+                    }
+                });
+
+                return useForm(form);
+            },
+
+            submit() {
+                const self = this;
+
+                this
+                    .form
+                    .transform((data) => ({
+                        ...data,
+                        key: self.formField.key,
+                        route_name: self.routeName,
+                    }))
+                    .post(
+                        route(self.routeSave),
+                        {
+                            preserveScroll: true,
+                            onStart: () => {
+                                self.onStartLoadingOverlay();
+                            },
+                            onSuccess: (page) => {
+                                successAlert(page.props.flash.message);
+
+                                self.$emit('on-success-submit');
+
+                                self.resetFields();
+
+                            },
+                            onError: errors => {
+                                oopsAlert({isScrollToTop: false});
+                            },
+                            onFinish: (visit) => {
+                                self.onEndLoadingOverlay();
+
+                                self.formFieldKey += 1;
+                            },
+                        }
+                    );
+            },
+
+
+            resetFields() {
+                forEach(this.$refs, (fieldGroup, fieldGroupKey) => {
+                    forEach(fieldGroup.$refs, (field, fieldKey) => {
+                        if (field.reset) {
+                            field.reset();
+                        }
+                    });
+                });
+            },
+        },
+    }
+</script>
