7db53a07b31aea9266a40431a792baedbe7d5e42
Naufal <naufal@sdbagency.com>
2023-01-31 16:05:14 +0700

    Remove setting seeder

--- database/seeders/DatabaseBuskincitySeeder.php
+++ database/seeders/DatabaseBuskincitySeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Database\Seeders\Buskincity\SettingSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseBuskincitySeeder extends Seeder
@@ -15,7 +14,6 @@ class DatabaseBuskincitySeeder extends Seeder
     public function run()
     {
         $this->call([
-            SettingSeeder::class,
         ]);
     }
 }
