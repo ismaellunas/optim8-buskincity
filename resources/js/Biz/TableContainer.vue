15a294677366ee8bef3e73beb519980da8e46e66
Naufal <naufal@sdbagency.com>
2023-02-08 14:04:04 +0700

    Add table container component

---
+++ resources/js/Biz/TableContainer.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <div class="table-container">
+            <biz-table-info
+                class="mb-2"
+                :records="records"
+            />
+
+            <biz-table
+                :class="tableClass"
+                is-fullwidth
+            >
+                <thead v-if="hasTableHeadSlot">
+                    <slot name="thead" />
+                </thead>
+
+                <tbody>
+                    <slot />
+                </tbody>
+
+                <tfoot v-if="hasTableFootSlot">
+                    <slot name="tfoot" />
+                </tfoot>
+            </biz-table>
+        </div>
+
+        <biz-pagination
+            :is-ajax="isAjaxPagination"
+            :links="records.links"
+            :query-params="queryParams"
+            @on-clicked-pagination="onClickedPagination"
+        />
+    </div>
+</template>
+
+<script>
+    import BizPagination from '@/Biz/Pagination';
+    import BizTable from '@/Biz/Table';
+    import BizTableInfo from '@/Biz/TableInfo';
+
+    export default {
+        name: 'BizTableContainer',
+
+        components: {
+            BizPagination,
+            BizTable,
+            BizTableInfo,
+        },
+
+        props: {
+            isAjaxPagination: { type: Boolean, default: false },
+            queryParams: { type: Object, default: () => {} },
+            records: { type: Object, required: true },
+            tableClass: { type: [Object, String, Array], default: 'is-striped is-hoverable' },
+        },
+
+        emits: [
+            'on-clicked-pagination'
+        ],
+
+        computed: {
+            hasTableHeadSlot() {
+                return !!this.$slots.thead
+            },
+
+            hasTableFootSlot() {
+                return !!this.$slots.tfoot
+            },
+        },
+
+        methods: {
+            onClickedPagination(url) {
+                this.$emit('on-clicked-pagination', url);
+            },
+        },
+    }
+</script>
