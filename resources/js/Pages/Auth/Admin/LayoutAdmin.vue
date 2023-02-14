ed80c28385cd65c98dc14375a6cea668768f9567
Naufal <naufal@sdbagency.com>
2023-02-14 15:15:32 +0700

    Update layout admin

--- resources/js/Pages/Auth/Admin/LayoutAdmin.vue
+++ resources/js/Pages/Auth/Admin/LayoutAdmin.vue
@@ -4,32 +4,26 @@
 
         <section class="hero is-fullheight">
             <div class="hero-body">
-                <div class="container has-text-centered">
-                    <div class="columns">
-                        <div class="column is-three-fifths has-text-left">
-                            <div class="level">
-                                <div class="level-left">
-                                    <div class="level-item">
-                                        <slot name="back" />
-                                    </div>
-                                </div>
+                <div class="container">
+                    <div class="has-text-centered mb-3">
+                        <biz-image
+                            :src="appLogoImageUrl"
+                            :style="{ width: '200px' }"
+                            :has-position="true"
+                        />
+                    </div>
+
+                    <div class="columns is-centered">
+                        <div class="column is-two-fifths">
+                            <div class="box">
+                                <slot />
                             </div>
-                            <section class="section">
-                                <div class="columns">
-                                    <div class="column is-9 is-offset-1">
-                                        <slot />
-                                    </div>
-                                </div>
-                            </section>
                         </div>
-                        <div class="column is-two-fifths has-text-left">
-                            <div class="card">
-                                <div class="card-image">
-                                    <figure class="image is-3by4">
-                                        <img src="/storage/images/default-auth-card.png">
-                                    </figure>
-                                </div>
-                            </div>
+                    </div>
+
+                    <div class="columns is-centered mt-4">
+                        <div class="column is-two-fifths">
+                            <slot name="back" />
                         </div>
                     </div>
                 </div>
@@ -40,12 +34,15 @@
 
 <script>
     import LayoutBackendBlank from '@/Layouts/BackendBlank';
-    import { Head } from '@inertiajs/inertia-vue3';
+    import BizImage from '@/Biz/Image';
+    import { computed } from 'vue';
+    import { Head, usePage } from '@inertiajs/inertia-vue3';
 
     export default {
         name: 'LayoutAdmin',
 
         components: {
+            BizImage,
             Head,
         },
 
@@ -53,6 +50,14 @@
 
         props: {
             title: { type: String, required: true },
+        },
+
+        setup() {
+            const appLogoImageUrl = computed(() => usePage().props.value.appLogoUrl);
+
+            return {
+                appLogoImageUrl,
+            };
         },
     }
 </script>
