5b5b7947459f2ad74b925dc269ff67aca6fe5952
Bayu <bayu@biz752.com>
2023-02-16 19:38:24 +0700

    Update HasPageErrors mixin to handle multiple field names

--- resources/js/Mixins/HasPageErrors.js
+++ resources/js/Mixins/HasPageErrors.js
@@ -5,6 +5,8 @@ import { isArray } from 'lodash';
 export default {
     methods: {
         error(field, errorBag = 'default', errors = null) {
+            const self = this;
+
             if (!errors) {
                 errors = usePage().props.value.errors;
             }
@@ -21,18 +23,36 @@ export default {
                 errorContainer = errors[errorBag];
             }
 
-            if (errorContainer && errorContainer.hasOwnProperty(field)) {
-                if (
-                    isArray(errorContainer[field])
-                    && errorContainer[field].length == 1
-                ) {
-                    return errorContainer[field][0];
-                }
-
-                return errorContainer[field];
+            if (! errorContainer) {
+                return null;
             }
 
-            return null;
-        }
+            let messages = [];
+
+            if (isArray(field)) {
+                field.forEach(function (currentField) {
+                    messages.push(
+                        self.getErrorFromContainer(errorContainer, currentField)
+                    );
+                });
+            } else {
+                messages.push(
+                    this.getErrorFromContainer(errorContainer, field)
+                );
+            }
+
+            return messages;
+        },
+
+        getErrorFromContainer(errorContainer, field) {
+            if (
+                isArray(errorContainer[field])
+                && errorContainer[field].length == 1
+            ) {
+                return errorContainer[field][0];
+            }
+
+            return errorContainer[field];
+        },
     }
 }
