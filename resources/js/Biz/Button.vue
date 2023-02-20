1e47949e56f9f61c5a6c5b6529b32c1c421bcbf3
Bayu <bayu@biz752.com>
2023-02-20 12:44:24 +0700

    Update BizButton to have dynamic tag

--- resources/js/Biz/Button.vue
+++ resources/js/Biz/Button.vue
@@ -1,20 +1,19 @@
 <template>
-    <button
+    <component
+        :is="tag"
         class="button"
         :type="type"
-        >
-        <slot></slot>
-    </button>
+    >
+        <slot />
+    </component>
 </template>
 
 <script>
     export default {
         name: 'BizButton',
         props: {
-            type: {
-                type: String,
-                default: 'submit',
-            },
+            type: { type: String, default: 'submit' },
+            tag: { type: String, default: 'button'},
         }
     }
 </script>
