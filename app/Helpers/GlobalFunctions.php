ff46c5554117960d2f79c43929b525ed027f7b46
Naufal <naufal@sdbagency.com>
2023-03-08 17:49:24 +0700

    Add global function to get default media library instructions

--- app/Helpers/GlobalFunctions.php
+++ app/Helpers/GlobalFunctions.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Helpers\HumanReadable;
 use App\Services\TranslationService;
+
 
 if (! function_exists('defaultLocale')) {
     function defaultLocale()
@@ -13,5 +15,21 @@ if (! function_exists('currentLocale')) {
     function currentLocale()
     {
         return TranslationService::currentLanguage();
+    }
+}
+
+if (! function_exists('defaultMediaLibraryInstructions')) {
+    function defaultMediaLibraryInstructions()
+    {
+        return [
+            __('Accepted file extensions: :extensions.', [
+                'extensions' => implode(', ', config('constants.extensions.image'))
+            ]),
+            __('Max file size: :filesize.', [
+                'filesize' => HumanReadable::bytesToHuman(
+                    (50 * config('constants.one_megabyte')) * 1024
+                )
+            ]),
+        ];
     }
 }
