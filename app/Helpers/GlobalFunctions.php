dbd5bd188f79afb2f10e591a4a9667c1ef2a734b
Naufal <naufal@sdbagency.com>
2023-05-12 10:47:20 +0700

    Remove global function to put, get, and forget the session

--- app/Helpers/GlobalFunctions.php
+++ app/Helpers/GlobalFunctions.php
@@ -15,32 +15,3 @@ if (! function_exists('currentLocale')) {
         return TranslationService::currentLanguage();
     }
 }
-
-if (! function_exists('putSession')) {
-    function putSession(string $key, mixed $value): void
-    {
-        session()->put($key, $value);
-    }
-}
-
-if (! function_exists('getSession')) {
-    function getSession(string $key, mixed $defaultValue = null): mixed
-    {
-        $value = session()->get($key);
-
-        if (! $value && $defaultValue) {
-            putSession($key, $defaultValue);
-
-            $value = $defaultValue;
-        }
-
-        return $value;
-    }
-}
-
-if (! function_exists('forgetSession')) {
-    function forgetSession(string $key): void
-    {
-        session()->forget($key);
-    }
-}
