7aee643ff618d5c6e3beb929d99b2aff0eaee960
Bayu <bayu@biz752.com>
2023-05-24 14:05:11 +0700

    Add async attribute on recaptcha/api.js script tag

--- themes/biz/views/components/recaptcha.blade.php
+++ themes/biz/views/components/recaptcha.blade.php
@@ -1,12 +1,3 @@
-@if (!empty($recaptchaSiteKey))
-    <div
-        class="g-recaptcha"
-        data-sitekey="{{ $recaptchaSiteKey }}"
-        data-size="invisible"
-        data-error-callback="recaptchaError"
-    ></div>
-@endif
-
 <span
     id="recaptcha-error-message"
     class="help has-text-danger is-hidden"
@@ -14,22 +5,25 @@
     Please check the reCAPTCHA!
 </span>
 
-@push('scripts')
-    <script>
-        function recaptchaError() {
-            document.getElementById('recaptcha-error-message').classList.remove('is-hidden');
-        }
-    </script>
-@endpush
+@if (!empty($recaptchaSiteKey))
+    <div
+        class="g-recaptcha"
+        data-sitekey="{{ $recaptchaSiteKey }}"
+        data-size="invisible"
+        data-error-callback="recaptchaError"
+    ></div>
 
-@push('bottom_scripts')
-    @if (!empty($recaptchaSiteKey))
+    @push('bottom_scripts')
         <script>
+            function recaptchaError() {
+                document.getElementById('recaptcha-error-message').classList.remove('is-hidden');
+            };
+
             var onloadCallback = function() {
                 grecaptcha.execute();
             };
         </script>
 
-        <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback" defer></script>
-    @endif
-@endpush
+        <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback" async defer></script>
+    @endpush
+@endif
