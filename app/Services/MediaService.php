594db76d58012d20e3ecaa34e80b19c66d2c4558
Naufal Rivaldi <naufal@biz752.com>
2023-05-25 12:01:23 +0700

    Add media detail translation

--- app/Services/MediaService.php
+++ app/Services/MediaService.php
@@ -518,6 +518,7 @@ class MediaService
     {
         return [
             'search' => __('Search'),
+            'media_detail' => __('Media detail'),
             'filter' => __('Filter'),
             'file_name' => __('File name'),
             'alternative_text' => __('Alternative text'),
