e8dc61642c9d54d6bc9d3c6ea291f899865daba6
Naufal Rivaldi <naufal@biz752.com>
2023-05-30 15:58:01 +0700

    Split the recaptcha score to specific method

--- app/Http/Middleware/RecaptchaForgotPasswordPage.php
+++ app/Http/Middleware/RecaptchaForgotPasswordPage.php
@@ -6,9 +6,6 @@ class RecaptchaForgotPasswordPage extends Recaptcha
 {
     protected function getRecaptchaScore(): float
     {
-        $recaptchaScores = $this->settingService->getRecaptchaScores();
-
-        return (float)$recaptchaScores['recaptcha_score_forgot_password']
-            ?? config('constants.settings.recaptcha.score');
+        return $this->settingService->getForgotPasswordRecaptchaScore();
     }
 }
