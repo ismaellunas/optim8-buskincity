f400cb8c77491836e0e3857a87afe04d0062a47b
Naufal <naufal@biz752.com>
2023-05-30 07:58:18 +0700

    Update recaptcha using eventListener to execute from submit form

--- themes/biz/views/components/recaptcha.blade.php
+++ themes/biz/views/components/recaptcha.blade.php
@@ -1,26 +1,43 @@
-@if (!empty($recaptchaSiteKey))
-    <input id="g-recaptcha-response" type="hidden" name="g-recaptcha-response" value="">
+<input id="g-recaptcha-response" type="hidden" name="g-recaptcha-response" value="">
 
+@if (! empty($recaptchaSiteKey))
     <span
         id="recaptcha-error-message"
         class="help has-text-danger is-hidden"
     >
         {{ __('Please check the reCAPTCHA!') }}
     </span>
-
-    @push('bottom_scripts')
-        <script src="https://www.google.com/recaptcha/api.js?render={{ $recaptchaSiteKey }}"></script>
-        <script>
-            grecaptcha.ready(function() {
-                try {
-                    grecaptcha.execute('{{ $recaptchaSiteKey }}', { action: '{{ $action }}' })
-                        .then((response) => {
-                            document.getElementById('g-recaptcha-response').value = response;
-                        });
-                } catch (error) {
-                    document.getElementById('recaptcha-error-message').classList.remove('is-hidden');
-                }
-            });
-        </script>
-    @endpush
 @endif
+
+@push('bottom_scripts')
+    @if (! empty($recaptchaSiteKey))
+        <script src="https://www.google.com/recaptcha/api.js?render={{ $recaptchaSiteKey }}"></script>
+    @endif
+
+    <script>
+        window.addEventListener('load', function () {
+            document.getElementById("{{ $formId }}").addEventListener('submit', function(event) {
+                event.preventDefault();
+
+                @if (! empty($recaptchaSiteKey))
+                    grecaptcha.ready(function() {
+                        try {
+                            grecaptcha.execute('{{ $recaptchaSiteKey }}', { action: '{{ $action }}' })
+                                .then((response) => {
+                                    document.getElementById('g-recaptcha-response').value = response;
+
+                                    event.target.submit();
+                                });
+                        } catch (error) {
+                            document.getElementById('recaptcha-error-message').classList.remove('is-hidden');
+
+                            event.target.submit();
+                        }
+                    });
+                @else
+                    event.target.submit();
+                @endif
+            });
+        }, false);
+    </script>
+@endpush
