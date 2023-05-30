ce248aa052647e2586c19865155d7784b6a34242
Naufal Rivaldi <naufal@biz752.com>
2023-05-30 15:57:11 +0700

    Update error message for error recaptcha

--- app/Http/Middleware/Recaptcha.php
+++ app/Http/Middleware/Recaptcha.php
@@ -66,13 +66,13 @@ class Recaptcha
             return redirect()
                 ->back()
                 ->withErrors([
-                    'recaptcha' => __('Recaptcha verification failed. Please try again.'),
+                    'recaptcha' => __('The process could not be completed due to a connection problem. Please try again.'),
                 ]);
         }
 
         return response([
             'success' => false,
-            'message' => __('Recaptcha verification failed. Please try again.'),
+            'message' => __('The process could not be completed due to a connection problem. Please try again.'),
         ]);
     }
 }
