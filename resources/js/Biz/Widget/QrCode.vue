5f97c66649e8f178820e9927333e76ceede9fcf2
Naufal <naufal@biz752.com>
2023-06-13 11:35:55 +0700

    Update mobile grid on qrcode widget component

--- resources/js/Biz/Widget/QrCode.vue
+++ resources/js/Biz/Widget/QrCode.vue
@@ -4,8 +4,8 @@
             {{ title }}
         </h2>
         <div class="box is-shadowless">
-            <div class="columns is-mobile">
-                <div class="column is-narrow">
+            <div class="columns is-multiline is-mobile">
+                <div class="column is-4-desktop is-12-tablet is-12-mobile">
                     <biz-qr-code
                         :height="128"
                         :width="128"
@@ -16,7 +16,7 @@
                     />
                 </div>
 
-                <div class="column">
+                <div class="column is-8-desktop is-12-tablet is-12-mobile">
                     <p>
                         {{ data.description }}
                     </p>
