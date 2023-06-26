22aee6bad57b944f4d6f644a785c1b67a3e06a27
Bayu <bayu@biz752.com>
2023-06-26 11:01:56 +0700

    Update vue apps on layout page blade components to dynamic import (lazy load js files)

--- resources/js/frontend.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import { components as defaultComponents } from './frontend-bootstrap';
-
-const app = createApp({
-    components: defaultComponents
-});
-
-app.mount('#app');
--- themes/biz/js/donation.js
+++ themes/biz/js/donation.js
@@ -1,13 +1,13 @@
 import './bootstrap';
 
-import FormDonation from './components/FormDonation.vue';
-import { LoadingPlugin } from 'vue-loading-overlay';
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 
 const profilePerformerDonation = createApp({
     components: {
-        FormDonation,
+        FormDonation: defineAsyncComponent(() =>
+            import('./components/FormDonation.vue')
+        ),
     },
 });
-profilePerformerDonation.use(LoadingPlugin, {color: '#3280bf', loader: 'dots', opacity: 0.3, zIndex: 8000});
+
 profilePerformerDonation.mount("#donation");
