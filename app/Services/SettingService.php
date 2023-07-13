1c2a497462aa45f80b27e0124ee062a6b45a3300
Naufal <naufal@biz752.com>
2023-07-13 10:59:46 +0700

    Add Open Graph image on Theme SEO

--- app/Services/SettingService.php
+++ app/Services/SettingService.php
@@ -670,6 +670,11 @@ class SettingService
         $this->saveMedia('post_thumbnail_media_id', $mediaId, 'theme_seo');
     }
 
+    public function saveOpenGraph(?int $mediaId = null): void
+    {
+        $this->saveMedia('open_graph_media_id', $mediaId, 'theme_seo');
+    }
+
     private function transformMedia(Media $media): void
     {
         $media->append(['is_image', 'thumbnail_url', 'display_file_name']);
@@ -822,4 +827,16 @@ class SettingService
 
         return $media;
     }
+
+    public function getOpenGraphMedia(): ?Media
+    {
+        $media = $this->getMediaFromSetting('open_graph_media_id');
+
+        if ($media) {
+            $this->transformMedia($media);
+        }
+
+        return $media;
+    }
+
 }
