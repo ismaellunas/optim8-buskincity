cabed607e0d6d5c5749ac8f43afc15dae26b038a
Naufal <naufal@biz752.com>
2023-07-27 14:00:27 +0700

    Remove duplicate tag for .modal-content class

--- resources/js/Components/Builder/Content/Gallery.vue
+++ resources/js/Components/Builder/Content/Gallery.vue
@@ -17,45 +17,43 @@
             v-if="isActive && activeMedium"
             @close="isActive = false"
         >
-            <div class="modal-content">
-                <div class="card">
-                    <div class="card-image">
-                        <figure class="image is-3by2">
-                            <img
-                                v-if="activeMedium.file_type == 'image'"
-                                class="image-source"
-                                :src="activeMedium.optimized_url ?? activeMedium.file_url"
-                            >
-                            <iframe
-                                v-if="activeMedium.file_type == 'video'"
-                                class="has-ratio"
-                                frameborder="0"
-                                :src="activeMedium.file_url"
-                                allowfullscreen
-                            />
-                        </figure>
-                    </div>
-                    <footer class="card-footer">
-                        <div class="column">
-                            <div class="is-pulled-left">
-                                <a @click.prevent="prev()">
-                                    <biz-icon
-                                        class="is-medium"
-                                        icon="fas fa-lg fa-chevron-circle-left"
-                                    />
-                                </a>
-                            </div>
-                            <div class="is-pulled-right has-text-primary">
-                                <a @click.prevent="next()">
-                                    <biz-icon
-                                        class="is-medium"
-                                        icon="fas fa-lg fa-chevron-circle-right"
-                                    />
-                                </a>
-                            </div>
-                        </div>
-                    </footer>
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image is-3by2">
+                        <img
+                            v-if="activeMedium.file_type == 'image'"
+                            class="image-source"
+                            :src="activeMedium.optimized_url ?? activeMedium.file_url"
+                        >
+                        <iframe
+                            v-if="activeMedium.file_type == 'video'"
+                            class="has-ratio"
+                            frameborder="0"
+                            :src="activeMedium.file_url"
+                            allowfullscreen
+                        />
+                    </figure>
                 </div>
+                <footer class="card-footer">
+                    <div class="column">
+                        <div class="is-pulled-left">
+                            <a @click.prevent="prev()">
+                                <biz-icon
+                                    class="is-medium"
+                                    icon="fas fa-lg fa-chevron-circle-left"
+                                />
+                            </a>
+                        </div>
+                        <div class="is-pulled-right has-text-primary">
+                            <a @click.prevent="next()">
+                                <biz-icon
+                                    class="is-medium"
+                                    icon="fas fa-lg fa-chevron-circle-right"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                </footer>
             </div>
         </biz-modal>
     </div>
