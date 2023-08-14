4f764be946144b6a6a386fdcbbf1c57038767b79
Naufal Rivaldi <naufal@biz752.com>
2023-08-14 18:37:55 +0700

    Update social media driver using component on Buskincity theme

--- themes/buskincity/views/auth/register.blade.php
+++ themes/buskincity/views/auth/register.blade.php
@@ -72,27 +72,7 @@
                         {{ __('Please choose a method to continue.') }}
                     </p>
 
-                    @foreach ($availableSocialiteDrivers as $driver)
-                        <a href="{{ route('oauth.redirect', $driver) }}" @class([
-                            "button is-medium is-light is-fullwidth",
-                            "mt-6" => $loop->first,
-                            "mt-4" => !$loop->first,
-                        ])>
-                            <x-icon icon="fa-brands fa-{{ $driver }}" is-small />
-                            <span>
-                                {!! __('Sign up with :driver', ['driver' => '<span class="has-text-weight-bold">'.Str::title($driver).'</span>']) !!}
-                            </span>
-                        </a>
-                    @endforeach
-
-                    <div class="is-divider mt-6 mb-6 ml-5 mr-6" data-content="OR"></div>
-
-                    <a class="button is-medium is-light is-fullwidth mt-4" onclick="showForm()">
-                        <x-icon icon="fa-envelope" is-small />
-                        <span>
-                            {!! __('Sign up with :driver', ['driver' => '<span class="has-text-weight-bold">Email</span>']) !!}
-                        </span>
-                    </a>
+                    <x-social-driver />
                 </div>
 
                 <div
