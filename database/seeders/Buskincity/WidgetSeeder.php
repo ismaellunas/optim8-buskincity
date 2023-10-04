7a40653c675c239a353170843c56743ea858612d
Naufal <naufal@biz752.com>
2023-10-04 20:45:34 +0700

    [Buskincity] Add Street Performers You Might Like widget on seeder

--- database/seeders/Buskincity/WidgetSeeder.php
+++ database/seeders/Buskincity/WidgetSeeder.php
@@ -17,13 +17,12 @@ class WidgetSeeder extends Seeder
      */
     public function run()
     {
-        $pageId = PageTranslation::where('slug', 'performer-application')
-            ->first()
-            ->page_id ?? null;
-
-        $formId = Form::where('key', 'performer_application')
-            ->first()
-            ->id ?? null;
+        $streetPerformerPage = PageTranslation::firstWhere('slug', 'street-performers');
+        $streetPerformerPageUrl = $streetPerformerPage
+            ? route('frontend.pages.show', [
+                'page_translation' => $streetPerformerPage->slug,
+            ])
+            : null;
 
         $settings = [
             [
@@ -42,14 +41,39 @@ class WidgetSeeder extends Seeder
                             'mobile' => 12,
                         ],
                         'setting' => [
-                            'page_id' => $pageId,
-                            'form_id' => $formId,
+                            'page_id' => PageTranslation::firstWhere('slug', 'performer-application')
+                                ->page_id ?? null,
+                            'form_id' => Form::firstWhere('key', 'performer_application')
+                                ->id ?? null,
                         ],
                         'i18n' => [
                             'description' => 'Apply to become a performer on BuskinCity. It will allow you to have your page, receive donations, private bookings, and build a bigger audience.',
                             'under_review' => 'Our team is reviewing your application. Usually takes from 3-5 days to get approved. We will contact you by email with our decision. Have questions or comments?',
                             'button_apply' => 'Apply now',
                             'button_get_in_touch' => 'Get in touch',
+                        ],
+                        'is_enabled' => true,
+                    ],
+                    [
+                        'id' => Str::uuid(),
+                        'widget' => "App\Entities\Widgets\DefaultWidget",
+                        'module' => null,
+                        'title' => 'Street Performers You Might Like',
+                        'order' => 5,
+                        'grid' => [
+                            'desktop' => 6,
+                            'tablet' => 12,
+                            'mobile' => 12,
+                        ],
+                        'setting' => [
+                            'url' => $streetPerformerPageUrl,
+                            'visibility' => [
+                                'role' => [],
+                            ]
+                        ],
+                        'i18n' => [
+                            'description' => 'Find and follow your favorite street performers on BuskinCity.',
+                            'button_url' => 'Find out',
                         ],
                         'is_enabled' => true,
                     ],
