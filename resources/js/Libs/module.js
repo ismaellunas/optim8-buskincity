b1541272c52aa5a60ef4f179cab1a7463aa1c303
Bayu <bayu@biz752.com>
2023-10-12 19:45:48 +0700

    Make sure isModuleActive in module.js will return boolean

--- resources/js/Libs/module.js
+++ resources/js/Libs/module.js
@@ -2,7 +2,7 @@ import { find } from 'lodash';
 import { usePage } from '@inertiajs/vue3';
 
 export function isModuleActive(moduleName) {
-    return find(usePage().props.modules, (module) => module == moduleName);
+    return !!find(usePage().props.modules, (module) => module == moduleName);
 }
 
 export function isEcommerceModuleActive() {
