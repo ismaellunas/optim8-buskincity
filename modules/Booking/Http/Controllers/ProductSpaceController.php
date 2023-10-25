da5e58b58092af08cf6b7d26003a7be186e28eec
Naufal <naufal@biz752.com>
2023-10-25 11:03:09 +0700

    Remove unused service

--- modules/Booking/Http/Controllers/ProductSpaceController.php
+++ modules/Booking/Http/Controllers/ProductSpaceController.php
@@ -5,15 +5,11 @@ namespace Modules\Booking\Http\Controllers;
 use App\Http\Controllers\CrudController;
 use Illuminate\Support\Arr;
 use Modules\Booking\Http\Requests\ProductSpaceRequest;
-use Modules\Booking\Services\ProductSpaceService;
 use Modules\Ecommerce\Entities\Product;
 
 class ProductSpaceController extends CrudController
 {
     protected $title = 'Product Space';
-
-    public function __construct(private ProductSpaceService $productSpaceService)
-    {}
 
     public function update(ProductSpaceRequest $request, Product $product)
     {
