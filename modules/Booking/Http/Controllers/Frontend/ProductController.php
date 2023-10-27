e774ca98aacd93b7305b84ca52d1b0b21eddef41
Bayu <bayu@biz752.com>
2023-10-27 20:57:04 +0700

    Update Booking button on Booking Index page to make it more readable

--- modules/Booking/Http/Controllers/Frontend/ProductController.php
+++ modules/Booking/Http/Controllers/Frontend/ProductController.php
@@ -56,6 +56,9 @@ class ProductController extends CrudController
             ),
             'countryOptions' => $this->productEventService->getFrontendCountryOptions(),
             'cityOptions' => $this->productEventService->getFrontendCityOptions(),
+            'i18n' => [
+                'book_now' => __('Book now'),
+            ],
         ]));
     }
 
