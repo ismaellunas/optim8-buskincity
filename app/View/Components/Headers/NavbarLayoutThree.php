582f037e3a9b0639939699ec7d7b64349426ed33
Naufal <naufal@biz752.com>
2023-10-29 22:44:27 +0700

    Use layout one when selecting layout two and three

--- app/View/Components/Headers/NavbarLayoutThree.php
+++ app/View/Components/Headers/NavbarLayoutThree.php
@@ -2,18 +2,5 @@
 
 namespace App\View\Components\Headers;
 
-use App\Services\MenuService;
-
-class NavbarLayoutThree extends BaseNavbarLayout
-{
-    public $layoutName = 'navbar-layout-one';
-    public $socialMediaMenus;
-
-    public function __construct($menus, $currentLanguage, $logoUrl, $languageOptions)
-    {
-        parent::__construct($menus, $currentLanguage, $logoUrl, $languageOptions);
-        $menuService = app(MenuService::class);
-        $this->socialMediaMenus = $menuService->getSocialMediaMenus();
-
-    }
-}
+class NavbarLayoutThree extends NavbarLayoutOne
+{}
--- app/View/Components/Headers/NavbarLayoutTwo.php
+++ app/View/Components/Headers/NavbarLayoutTwo.php
@@ -2,20 +2,5 @@
 
 namespace App\View\Components\Headers;
 
-class NavbarLayoutTwo extends BaseNavbarLayout
-{
-    public $menuChunks;
-    public $layoutName = 'navbar-layout-one';
-
-    public function __construct($menus, $currentLanguage, $logoUrl, $languageOptions)
-    {
-        parent::__construct($menus, $currentLanguage, $logoUrl, $languageOptions);
-        $this->menuChunks = $this->splitMenu();
-    }
-
-    private function splitMenu()
-    {
-        $half = ceil(count($this->menus) / 2);
-        return collect($this->menus)->chunk($half);
-    }
-}
+class NavbarLayoutTwo extends NavbarLayoutOne
+{}
