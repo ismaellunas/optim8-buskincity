757a46d013e52735557167f61bb367e2b242e8d1
dwiff <dwiff.baconguis@gmail.com>
2025-05-08 21:29:50 +0800

    dev: Updates GlobalOptionSeeder

--- database/seeders/GlobalOptionSeeder.php
+++ database/seeders/GlobalOptionSeeder.php
@@ -14,100 +14,36 @@ class GlobalOptionSeeder extends Seeder
      */
     public function run()
     {
-        $this->populateDisciplineOptions();
+        $this->populateSpaceAndDisciplineOptions();
     }
 
-    private function populateDisciplineOptions()
+    private function populateSpaceAndDisciplineOptions()
     {
-        $disciplines = [
-            [
-                "default_value" => "Acrobat",
-                "name" => "Acrobat",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "BalanceAct",
-                "name" => "BalanceAct",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Clown",
-                "name" => "Clown",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Acrobatic",
-                "name" => "Acrobatic",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Dance",
-                "name" => "Dance",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Escapologist",
-                "name" => "Escapologist",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Juggler",
-                "name" => "Juggler",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Magician",
-                "name" => "Magician",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Multidisciplinary Circus",
-                "name" => "Multidisciplinary Circus",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Musician",
-                "name" => "Musician",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Music-Clown",
-                "name" => "Music-Clown",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Music-Acrobat",
-                "name" => "Music-Acrobat",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Performance",
-                "name" => "Performance",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Pupeteer",
-                "name" => "Pupeteer",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Stiltwalkers",
-                "name" => "Stiltwalkers",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Visual Comedy",
-                "name" => "Visual Comedy",
-                "type" => "discipline",
-            ],
-            [
-                "default_value" => "Other",
-                "name" => "Other",
-                "type" => "discipline",
-            ],
+        $spaceAndDisciplinesData = [
+            ["name" => "Country", "type" => "space", "default_value" => null],
+            ["name" => "Pitch", "type" => "space", "default_value" => null],
+            ["name" => "City", "type" => "space", "default_value" => null],
+            ["name" => "Acrobat", "type" => "discipline", "default_value" => "Acrobat"],
+            ["name" => "BalanceAct", "type" => "discipline", "default_value" => "BalanceAct"],
+            ["name" => "Clown", "type" => "discipline", "default_value" => "Clown"],
+            ["name" => "Acrobatic", "type" => "discipline", "default_value" => "Acrobatic"],
+            ["name" => "Escapologist", "type" => "discipline", "default_value" => "Escapologist"],
+            ["name" => "Juggler", "type" => "discipline", "default_value" => "Juggler"],
+            ["name" => "Magician", "type" => "discipline", "default_value" => "Magician"],
+            ["name" => "Music-Clown", "type" => "discipline", "default_value" => "Music-Clown"],
+            ["name" => "Music-Acrobat", "type" => "discipline", "default_value" => "Music-Acrobat"],
+            ["name" => "Performance", "type" => "discipline", "default_value" => "Performance"],
+            ["name" => "Pupeteer", "type" => "discipline", "default_value" => "Pupeteer"],
+            ["name" => "Visual Comedy", "type" => "discipline", "default_value" => "Visual Comedy"],
+            ["name" => "Other", "type" => "discipline", "default_value" => "Other"],
+            ["name" => "Dance", "type" => "discipline", "default_value" => "Dance"],
+            ["name" => "Multidisciplinary Circus", "type" => "discipline", "default_value" => "Multidisciplinary Circus"],
+            ["name" => "Musician", "type" => "discipline", "default_value" => "Musician"],
+            ["name" => "Stiltwalkers", "type" => "discipline", "default_value" => "Stiltwalkers"],
+            ["name" => "Special Events / Festivals", "type" => "space", "default_value" => null],
         ];
 
-        $this->createData($disciplines);
+        $this->createData($spaceAndDisciplinesData);
     }
 
     private function createData($options)
